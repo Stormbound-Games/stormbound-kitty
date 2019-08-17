bca1e16254e5222cd82e73dd2195b99b711093e9
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2019-08-17 18:11:37 +0200

    Make it possible to reach existing cards in card builder from their name/id

--- src/helpers/getInitialCardData.js
+++ src/helpers/getInitialCardData.js
@@ -1,8 +1,27 @@
 import { deserialiseCard } from './deserialise'
+import { serialiseCardFromCollection } from './serialise'
+import cards from '../data/cards'
+
+const getCardFromSlug = slug =>
+  cards.find(
+    card => slugify(card.name) === slug.toLowerCase() || card.id === slug
+  )
+
+const slugify = name =>
+  name
+    .replace(/\s/g, '_')
+    .replace(/’',/g, '')
+    .toLowerCase()
 
 export default card => {
   if (!card) {
     return {}
+  }
+
+  const isExistingCard = getCardFromSlug(card)
+
+  if (isExistingCard) {
+    return deserialiseCard(serialiseCardFromCollection(isExistingCard.id))
   }
 
   const decodedData = decodeURIComponent(card)
