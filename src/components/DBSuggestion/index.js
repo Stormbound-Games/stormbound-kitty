b41831ca9bc8df814e33ee04a021c281169c6f2b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2019-08-23 10:20:14 +0200

    Add a rarity display bar to deck suggestions

--- src/components/DBSuggestion/index.js
+++ src/components/DBSuggestion/index.js
@@ -2,29 +2,38 @@ import React from 'react'
 import { Link } from '@reach/router'
 import Deck from '../Deck'
 import Tags from '../Tags'
+import RarityBar from '../RarityBar'
 import { CATEGORIES } from '../../constants/decks'
 import { deserialiseDeck } from '../../helpers/deserialise'
+import getRawCardData from '../../helpers/getRawCardData'
 import './index.css'
 
-const DBSuggestion = props => (
-  <div className="DBSuggestion">
-    <Deck
-      deck={deserialiseDeck(props.id)}
-      orientation="horizontal"
-      onClick={props.zoom}
-      onClickLabel="Display card"
-    />
-    <span className="DBSuggestion__name">
-      <Link to={`/deck/${props.id}`}>{props.name}</Link>
-    </span>
-    <span className="DBSuggestion__author">
-      {CATEGORIES[props.category]} deck by {props.author}
-    </span>
-    <span className="DBSuggestion__tags">
-      {props.tags.length > 0 && ' · '}
-      <Tags tags={props.tags} />
-    </span>
-  </div>
-)
+const DBSuggestion = props => {
+  const deck = deserialiseDeck(props.id)
+
+  return (
+    <div className="DBSuggestion">
+      <Deck
+        deck={deck}
+        orientation="horizontal"
+        onClick={props.zoom}
+        onClickLabel="Display card"
+      />
+      <div className="DBSuggestion__rarity-bar">
+        <RarityBar deck={deck.map(({ id }) => getRawCardData(id))} />
+      </div>
+      <span className="DBSuggestion__name">
+        <Link to={`/deck/${props.id}`}>{props.name}</Link>
+      </span>
+      <span className="DBSuggestion__author">
+        {CATEGORIES[props.category]} deck by {props.author}
+      </span>
+      <span className="DBSuggestion__tags">
+        {props.tags.length > 0 && ' · '}
+        <Tags tags={props.tags} />
+      </span>
+    </div>
+  )
+}
 
 export default DBSuggestion
---
+++ src/components/RarityBar/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import './index.css'
+
+const RarityBar = props => {
+  const getRarityCount = rarity =>
+    props.deck.filter(card => card.rarity === rarity).length
+  const commons = getRarityCount('common')
+  const rares = getRarityCount('rare')
+  const epics = getRarityCount('epic')
+  const legendaries = getRarityCount('legendary')
+
+  return (
+    <div className="RarityBar" aria-hidden>
+      {commons > 0 && (
+        <div
+          className="RarityBar__slice"
+          style={{
+            '--color': 'rgb(151, 146, 123)',
+            '--count': commons
+          }}
+        >
+          <span className="RarityBar__count" title={commons}>
+            {commons}
+          </span>
+        </div>
+      )}
+      {rares > 0 && (
+        <div
+          className="RarityBar__slice"
+          style={{
+            '--color': 'rgb(31, 174, 224)',
+            '--count': rares
+          }}
+        >
+          <span className="RarityBar__count" title={rares}>
+            {rares}
+          </span>
+        </div>
+      )}
+      {epics > 0 && (
+        <div
+          className="RarityBar__slice"
+          style={{
+            '--color': 'rgb(196, 93, 230)',
+            '--count': epics
+          }}
+        >
+          <span className="RarityBar__count" title={epics}>
+            {epics}
+          </span>
+        </div>
+      )}
+      {legendaries > 0 && (
+        <div
+          className="RarityBar__slice"
+          style={{
+            '--color': 'rgb(232, 137, 49)',
+            '--count': legendaries
+          }}
+        >
+          <span className="RarityBar__count" title={legendaries}>
+            {legendaries}
+          </span>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default RarityBar
