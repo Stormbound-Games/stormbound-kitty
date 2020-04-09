be673fb24e14acbe604e46e90471f6bb7c2efc2f
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-09 09:03:20 +0200

    Fix the collection cost not being accurate

--- src/helpers/getCollectionCost.js
+++ src/helpers/getCollectionCost.js
@@ -7,18 +7,36 @@ export default collection => {
   return collection
     .map(card => {
       const { rarity } = resolveCardForLevel(card)
-      const { copies, stonesForMissing } = RARITY_COPIES[rarity]
+      const {
+        copies: levelCopies,
+        stonesForMissing: craftingCost,
+        stonesPerMissingCopy: stonesPerCopy,
+        stonesPerExtraCopy
+      } = RARITY_COPIES[rarity]
 
-      return [
-        stonesForMissing,
-        card.level === 2 && copies[0] * stonesForMissing,
-        card.level === 3 && copies[1] * stonesForMissing,
-        card.level === 4 && copies[2] * stonesForMissing,
-        card.level === 5 && copies[3] * stonesForMissing,
-        card.copies * stonesForMissing
-      ]
-        .filter(Boolean)
-        .reduce(sum, 0)
+      // If the card is missing, it has no value.
+      if (card.missing) return 0
+
+      switch (card.level) {
+        // If the card is level 1, its total value is its crafting cost + the
+        // amount of copies times the value of a copy.
+        case 1:
+          return craftingCost + card.copies * stonesPerCopy
+        // If the card is level 2, 3 or 4, its total value is its crafting cost
+        // + the amount of copies to get to that level times the value of a copy
+        // + the amount of extra copies times the value of a copy.
+        case 2:
+        case 3:
+        case 4:
+          return craftingCost + levelCopies[card.level - 2] * stonesPerCopy + card.copies * stonesPerCopy
+        // If the card is level 5, its total value is its crafting cost + the
+        // amount of copies to get to level 5 times the value of a copy + the
+        // amount of extra copies times the value of an extra copy past level 5.
+        case 5:
+          return craftingCost + levelCopies[3] * stonesPerCopy + card.copies * stonesPerExtraCopy
+        default:
+          return 0
+      }
     })
     .reduce(sum, 0)
 }
