dac79c7ceb1aba3569cf867c468bd13bb079aa74
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-11 10:33:13 +0200

    Replace ‘react-loadable’ with ‘@loadable/component’

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,179 +1,80 @@
 import React from 'react'
-import Loadable from 'react-loadable'
+import loadable from '@loadable/component'
 import { Router, Redirect } from '@reach/router'
-import PageLoading from '../PageLoading'
+import Loader from '../Loader'
 
-const Home = Loadable({
-  loader: () => import('../Home' /* webpackChunkName: "Home" */),
-  loading: PageLoading,
-})
-const FAQ = Loadable({
-  loader: () => import('../FAQ' /* webpackChunkName: "FAQ" */),
-  loading: PageLoading,
-})
-const CardBuilder = Loadable({
-  loader: () => import('../CBRoot' /* webpackChunkName: "CBRoot" */),
-  loading: PageLoading,
-})
-const CardContest = Loadable({
-  loader: () => import('../CBContest' /* webpackChunkName: "CBContest" */),
-  loading: PageLoading,
-})
-const CardDisplay = Loadable({
-  loader: () => import('../CBDisplay' /* webpackChunkName: "CBDisplay" */),
-  loading: PageLoading,
-})
-const DeckBuilder = Loadable({
-  loader: () => import('../DBRoot' /* webpackChunkName: "DBRoot" */),
-  loading: PageLoading,
-})
-const QuestBuilder = Loadable({
-  loader: () => import('../QBRoot' /* webpackChunkName: "QBRoot" */),
-  loading: PageLoading,
-})
-const Puzzles = Loadable({
-  loader: () => import('../BSPuzzles' /* webpackChunkName: "BSPuzzles" */),
-  loading: PageLoading,
-})
-const BattleSim = Loadable({
-  loader: () => import('../BSRoot' /* webpackChunkName: "BSRoot" */),
-  loading: PageLoading,
-})
-const BattleDisplay = Loadable({
-  loader: () => import('../BSDisplay' /* webpackChunkName: "BSDisplay" */),
-  loading: PageLoading,
-})
-const Editor = Loadable({
-  loader: () =>
-    import('../DBEditorView' /* webpackChunkName: "DBEditorView" */),
-  loading: PageLoading,
-})
-const Collection = Loadable({
-  loader: () =>
-    import('../DBCollection' /* webpackChunkName: "DBCollection" */),
-  loading: PageLoading,
-})
-const DeckGuide = Loadable({
-  loader: () => import('../DeckGuide' /* webpackChunkName: "DeckGuide" */),
-  loading: PageLoading,
-})
-const CompleteGuide = Loadable({
-  loader: () =>
-    import('../CompleteGuide' /* webpackChunkName: "CompleteGuide" */),
-  loading: PageLoading,
-})
-const WinterGuide = Loadable({
-  loader: () => import('../WinterGuide' /* webpackChunkName: "WinterGuide" */),
-  loading: PageLoading,
-})
-const PirateGuide = Loadable({
-  loader: () => import('../PirateGuide' /* webpackChunkName: "PirateGuide" */),
-  loading: PageLoading,
-})
-const Guides = Loadable({
-  loader: () => import('../Guides' /* webpackChunkName: "Guides" */),
-  loading: PageLoading,
-})
-const Suggestions = Loadable({
-  loader: () =>
-    import('../DBSuggestions' /* webpackChunkName: "DBSuggestions" */),
-  loading: PageLoading,
-})
-const Detail = Loadable({
-  loader: () =>
-    import('../DBDetailView' /* webpackChunkName: "DBDetailView" */),
-  loading: PageLoading,
-})
-const DryRun = Loadable({
-  loader: () =>
-    import('../DBDryRunView' /* webpackChunkName: "DBDryRunView" */),
-  loading: PageLoading,
-})
-const Tracker = Loadable({
-  loader: () =>
-    import('../DBTrackerView' /* webpackChunkName: "DBTrackerView" */),
-  loading: PageLoading,
-})
-const Lexicon = Loadable({
-  loader: () => import('../Lexicon' /* webpackChunkName: "Lexicon" */),
-  loading: PageLoading,
-})
-const Stories = Loadable({
-  loader: () => import('../Stories' /* webpackChunkName: "Stories" */),
-  loading: PageLoading,
-})
-const StoriesCategory = Loadable({
-  loader: () =>
-    import('../StoriesCategory' /* webpackChunkName: "StoriesCategory" */),
-  loading: PageLoading,
-})
-const Story = Loadable({
-  loader: () => import('../Story' /* webpackChunkName: "Story" */),
-  loading: PageLoading,
-})
-const TierList = Loadable({
-  loader: () =>
-    import('../TLBEditorView' /* webpackChunkName: "TLBEditorView" */),
-  loading: PageLoading,
-})
-const TierDisplay = Loadable({
-  loader: () =>
-    import('../TLBDisplayView' /* webpackChunkName: "TLBDisplayView" */),
-  loading: PageLoading,
+const Page = loadable(props => import(`../${props.Component}`), {
+  fallback: <Loader />,
 })
 
 const AppRouter = props => (
   <Router primary={false}>
-    <Home path='/' />
-    <FAQ path='/faq' />
+    <Page Component='Home' path='/' />
+    <Page Component='FAQ' path='/faq' />
 
-    <BattleSim path='/sim' />
-    <Puzzles path='/sim/puzzles' />
-    <BattleSim path='/sim/:simId' />
-    <BattleDisplay path='/sim/:simId/display' />
+    <Page Component='BSRoot' path='/sim' />
+    <Page Component='BSPuzzles' path='/sim/puzzles' />
+    <Page Component='BSRoot' path='/sim/:simId' />
+    <Page Component='BSDisplay' path='/sim/:simId/display' />
 
-    <CardBuilder path='/card' />
-    <CardContest path='/card/contest' />
-    <CardBuilder path='/card/:cardId' />
-    <CardDisplay path='/card/:cardId/display' />
+    <Page Component='CBRoot' path='/card' />
+    <Page Component='CBContest' path='/card/contest' />
+    <Page Component='CBRoot' path='/card/:cardId' />
+    <Page Component='CBDisplay' path='/card/:cardId/display' />
 
-    <DeckBuilder path='/deck'>
-      <Editor path='/' />
-    </DeckBuilder>
-    <Suggestions path='/deck/suggestions' />
-    <Collection path='/deck/collection' />
+    <Page Component='DBRoot' path='/deck'>
+      <Page Component='DBEditorView' path='/' />
+    </Page>
+    <Page Component='DBSuggestions' path='/deck/suggestions' />
+    <Page Component='DBCollection' path='/deck/collection' />
 
     <Redirect noThrow from='/deck/:deckId/stats' to='/deck/:deckId/detail' />
-    <DeckBuilder path='/deck/:deckId'>
-      <Editor path='/' />
-      <Detail path='/detail' />
-      <DryRun path='/dry-run' />
-      <Tracker path='/tracker' />
-    </DeckBuilder>
+    <Page Component='DBRoot' path='/deck/:deckId'>
+      <Page Component='DBEditorView' path='/' />
+      <Page Component='DBDetailView' path='/detail' />
+      <Page Component='DBDryRunView' path='/dry-run' />
+      <Page Component='DBTrackerView' path='/tracker' />
+    </Page>
 
-    <QuestBuilder path='/quest' />
-    <QuestBuilder path='/quest/:questId' />
+    <Page Component='QBRoot' path='/quest' />
+    <Page Component='QBRoot' path='/quest/:questId' />
 
-    <Stories path='/stories' />
-    <StoriesCategory path='/stories/neutral' category='neutral' />
-    <StoriesCategory path='/stories/ironclad' category='ironclad' />
-    <StoriesCategory path='/stories/winter' category='winter' />
-    <StoriesCategory path='/stories/shadowfen' category='shadowfen' />
-    <StoriesCategory path='/stories/swarm' category='swarm' />
-    <StoriesCategory path='/stories/lore' category='lore' />
-    <Story path='/stories/:storyId' />
+    <Page Component='Stories' path='/stories' />
+    <Page
+      Component='StoriesCategory'
+      path='/stories/neutral'
+      category='neutral'
+    />
+    <Page
+      Component='StoriesCategory'
+      path='/stories/ironclad'
+      category='ironclad'
+    />
+    <Page
+      Component='StoriesCategory'
+      path='/stories/winter'
+      category='winter'
+    />
+    <Page
+      Component='StoriesCategory'
+      path='/stories/shadowfen'
+      category='shadowfen'
+    />
+    <Page Component='StoriesCategory' path='/stories/swarm' category='swarm' />
+    <Page Component='StoriesCategory' path='/stories/lore' category='lore' />
+    <Page Component='Story' path='/stories/:storyId' />
 
-    <Guides path='/guides' />
-    <CompleteGuide path='/guides/complete' />
-    <DeckGuide path='/guides/deck' />
-    <WinterGuide path='/guides/winter' />
-    <PirateGuide path='/guides/pirate' />
-    <Lexicon path='/guides/lexicon' />
+    <Page Component='Guides' path='/guides' />
+    <Page Component='CompleteGuide' path='/guides/complete' />
+    <Page Component='DeckGuide' path='/guides/deck' />
+    <Page Component='WinterGuide' path='/guides/winter' />
+    <Page Component='PirateGuide' path='/guides/pirate' />
+    <Page Component='Lexicon' path='/guides/lexicon' />
     <Redirect noThrow from='/deck/guide' to='/guides/deck' />
 
-    <TierList path='/list' />
-    <TierList path='/list/:listId' />
-    <TierDisplay path='/list/:listId/display' />
+    <Page Component='TLBEditorView' path='/list' />
+    <Page Component='TLBEditorView' path='/list/:listId' />
+    <Page Component='TLBDisplayView' path='/list/:listId/display' />
   </Router>
 )
 
