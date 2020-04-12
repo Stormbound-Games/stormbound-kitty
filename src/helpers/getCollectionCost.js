744010ec096d428b2a750a51717ed28eaabc30eb
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-12 10:24:44 +0200

    Fix the collection cost not taking intermediary levels into account

--- src/helpers/getCollectionCost.js
+++ src/helpers/getCollectionCost.js
@@ -3,48 +3,70 @@ import resolveCardForLevel from './resolveCardForLevel'
 
 const sum = (a, b) => a + b
 
+export const getCardCost = card => {
+  const { rarity } = resolveCardForLevel(card)
+  const {
+    copies: levelCopies,
+    stonesForMissing: craftingCost,
+    stonesPerMissingCopy: stonesPerCopy,
+    stonesPerExtraCopy,
+  } = RARITY_COPIES[rarity]
+
+  // If the card is missing, it has no value.
+  if (card.missing) return 0
+
+  switch (card.level) {
+    // If the card is level 1, its total value is its crafting cost + the
+    // amount of copies times the value of a copy.
+    case 1: {
+      return craftingCost + card.copies * stonesPerCopy
+    }
+
+    // If the card is level 2, its total value is its crafting cost + the amount
+    // of copies to get to level 2 times the value of a copy + the amount of
+    // extra copies times the value of a copy.
+    case 2: {
+      const copies = levelCopies[0] + card.copies
+
+      return craftingCost + copies * stonesPerCopy
+    }
+
+    // If the card is level 3, its total value is its crafting cost + the amount
+    // of copies to get to level 3 + the amount of extra copies times the value
+    // of a copy.
+    case 3: {
+      const copies = levelCopies[0] + levelCopies[1] + card.copies
+
+      return craftingCost + copies * stonesPerCopy
+    }
+
+    // If the card is level 4, its total value is its crafting cost + the amount
+    // of copies to get to level 4 + the amount of extra copies times the value
+    // of a copy.
+    case 4: {
+      const copies =
+        levelCopies[0] + levelCopies[1] + levelCopies[2] + card.copies
+
+      return craftingCost + copies * stonesPerCopy
+    }
+
+    // If the card is level 5, its total value is its crafting cost + the
+    // amount of copies to get to level 5 times the value of a copy + the
+    // amount of extra copies times the value of an extra copy past level 5.
+    case 5: {
+      const copies =
+        levelCopies[0] + levelCopies[1] + levelCopies[2] + levelCopies[3]
+
+      return (
+        craftingCost + copies * stonesPerCopy + card.copies * stonesPerExtraCopy
+      )
+    }
+
+    default:
+      return 0
+  }
+}
+
 export default collection => {
-  return collection
-    .map(card => {
-      const { rarity } = resolveCardForLevel(card)
-      const {
-        copies: levelCopies,
-        stonesForMissing: craftingCost,
-        stonesPerMissingCopy: stonesPerCopy,
-        stonesPerExtraCopy,
-      } = RARITY_COPIES[rarity]
-
-      // If the card is missing, it has no value.
-      if (card.missing) return 0
-
-      switch (card.level) {
-        // If the card is level 1, its total value is its crafting cost + the
-        // amount of copies times the value of a copy.
-        case 1:
-          return craftingCost + card.copies * stonesPerCopy
-        // If the card is level 2, 3 or 4, its total value is its crafting cost
-        // + the amount of copies to get to that level times the value of a copy
-        // + the amount of extra copies times the value of a copy.
-        case 2:
-        case 3:
-        case 4:
-          return (
-            craftingCost +
-            levelCopies[card.level - 2] * stonesPerCopy +
-            card.copies * stonesPerCopy
-          )
-        // If the card is level 5, its total value is its crafting cost + the
-        // amount of copies to get to level 5 times the value of a copy + the
-        // amount of extra copies times the value of an extra copy past level 5.
-        case 5:
-          return (
-            craftingCost +
-            levelCopies[3] * stonesPerCopy +
-            card.copies * stonesPerExtraCopy
-          )
-        default:
-          return 0
-      }
-    })
-    .reduce(sum, 0)
+  return collection.map(getCardCost).reduce(sum, 0)
 }
