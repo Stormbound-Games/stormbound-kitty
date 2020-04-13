b8dd938f299b598e85c59b9576933d1663fac3ff
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-13 22:44:19 +0200

    Improve the calculator

--- src/components/BooksCalculator/index.js
+++ src/components/BooksCalculator/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import cards from '../../data/cards'
 import { RARITIES } from '../../constants/game'
 import Column from '../Column'
-import CTA from '../CTA'
 import PageMeta from '../PageMeta'
 import Row from '../Row'
 import Title from '../Title'
 import shuffle from '../../helpers/shuffle'
 import capitalise from '../../helpers/capitalise'
+import { getRarityImage } from '../../helpers/getRarity'
 import './index.css'
 
 const sum = (a, b) => a + b
@@ -15,6 +15,8 @@ const sum = (a, b) => a + b
 const CARD_COUNTS = Object.keys(RARITIES).map(
   rarity => cards.filter(card => card.rarity === rarity).length
 )
+
+console.log(CARD_COUNTS)
 
 const BOOKS = {
   NOBLE: { percentiles: [70, 25, 4, 1], draws: 3 },
@@ -24,8 +26,9 @@ const BOOKS = {
   MYTHIC: { percentiles: [0, 0, 70, 30], draws: 6 },
 }
 
-const getDrawingChance = bookType => {
+const isExpectedCard = (bookType, target) => {
   const { percentiles, draws } = BOOKS[bookType]
+  const expectedRarity = Object.keys(RARITIES).indexOf(target.toLowerCase())
   const chances = percentiles.map((_, index, array) =>
     array.slice(0, index + 1).reduce(sum, 0)
   )
@@ -36,26 +39,60 @@ const getDrawingChance = bookType => {
   for (let i = 0; i < draws; i++) {
     const random = Math.random() * 100
     const rarity = chances.findIndex(chance => chance > random)
+
+    // If looking for a card of specific rarity, and the draw did not yield the
+    // expected rarity, then no need to even check whether the card is the
+    // expected one.
+    if (target !== 'FUSION_STONES' && rarity !== expectedRarity) {
+      return false
+    }
+
     if (pools[rarity].pop() === 0) return true
   }
 
   return false
 }
 
-const getDrawingChances = (bookType, amount) => {
+const getDrawingChances = (bookType, target, amount) => {
   let found = 0
-  for (let i = 0; i < amount; i++) found += Number(getDrawingChance(bookType))
+  for (let i = 0; i < amount; i++)
+    found += Number(isExpectedCard(bookType, target))
 
   return (found / amount).toFixed(4)
 }
 
+const Image = props => {
+  return props.target === 'FUSION_STONES' ? (
+    <img
+      className='BookCalculator__image'
+      src='/assets/images/stones.png'
+      alt='Fusion Stones'
+    />
+  ) : (
+    <img
+      className='BookCalculator__image'
+      src={getRarityImage(props.target.toLowerCase())}
+      alt={props.target.toLowerCase()}
+    />
+  )
+}
+
 const BooksCalculator = props => {
   const [book, setBook] = React.useState('MYTHIC')
-  const [amount, setAmount] = React.useState(50000)
-  const chances = React.useMemo(() => getDrawingChances(book, amount), [
+  const [amount, setAmount] = React.useState(2500)
+  const [target, setTarget] = React.useState('FUSION_STONES')
+  const chances = React.useMemo(() => getDrawingChances(book, target, amount), [
     amount,
     book,
+    target,
   ])
+  const options = {
+    FUSION_STONES: 'Fusion stones',
+    COMMON: 'A specific common card',
+    RARE: 'A specific rare card',
+    EPIC: 'A specific epic card',
+    LEGENDARY: 'A specific legendary card',
+  }
 
   return (
     <>
@@ -63,7 +100,7 @@ const BooksCalculator = props => {
 
       <Row desktopOnly wideGutter>
         <Column width={33}>
-          <Title>Configuration</Title>
+          <Title element='h2'>Configuration</Title>
           <form>
             <Row>
               <Column>
@@ -82,13 +119,13 @@ const BooksCalculator = props => {
                 </select>
               </Column>
               <Column>
-                <label htmlFor='amount'>Amount</label>
+                <label htmlFor='amount'>Amount of books</label>
                 <input
                   type='number'
                   name='amount'
                   id='amount'
                   min={100}
-                  max={100000}
+                  max={50000}
                   step={100}
                   value={amount}
                   onChange={event => setAmount(event.target.value)}
@@ -98,20 +135,51 @@ const BooksCalculator = props => {
             </Row>
             <Row>
               <Column>
-                <CTA type='button' onClick={() => window.location.reload()}>
-                  Recompute chances
-                </CTA>
+                <label htmlFor='target'>Looking for</label>
+                <select
+                  id='target'
+                  name='target'
+                  value={target}
+                  onChange={event => setTarget(event.target.value)}
+                >
+                  {Object.keys(options).map(option => (
+                    <option key={option} value={option}>
+                      {options[option]}
+                    </option>
+                  ))}
+                </select>
               </Column>
             </Row>
           </form>
         </Column>
         <Column width={66}>
-          <Title>Outcome</Title>
-          <p className='BookCalculator__outcome'>
-            Given {amount} book openings, the estimated chances to find{' '}
-            <strong>Fusion Stones</strong> in a {capitalise(book.toLowerCase())}{' '}
-            book are about {chances}%. That is, not a lot. Good luck.
-          </p>
+          <Title element='h2'>Outcome</Title>
+
+          {!amount ? (
+            <p className='BookCalculator__outcome'>
+              Open at least a book to compute drawing chances.
+            </p>
+          ) : (
+            <>
+              {Number(chances) === 0 ? (
+                <p className='BookCalculator__outcome'>
+                  <Image target={target} />
+                  Given {amount} book openings, it is unlikely to find{' '}
+                  <strong>{options[target].toLowerCase()}</strong> at all —
+                  either because this rarity does not appear in this type of
+                  book, or because it’s simply is too low to be significant.
+                </p>
+              ) : (
+                <p className='BookCalculator__outcome'>
+                  <Image target={target} />
+                  Given {amount} book openings, the estimated chances to find{' '}
+                  <strong>{options[target]}</strong> in a{' '}
+                  {capitalise(book.toLowerCase())} book are about {chances}%.
+                  That is, not a lot. Good luck.
+                </p>
+              )}
+            </>
+          )}
         </Column>
       </Row>
 
