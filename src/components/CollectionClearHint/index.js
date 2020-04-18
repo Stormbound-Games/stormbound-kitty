2b8cac30e1cdecf90e6018014d32120ba38d2b11
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-18 16:56:05 +0200

    Refine button as links

--- src/components/CollectionClearHint/index.js
+++ src/components/CollectionClearHint/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { CollectionContext } from '../CollectionProvider'
 import Icon from '../Icon'
-import './index.css'
 
 const CollectionClearHint = props => {
   const { hasDefaultCollection, resetCollection } = React.useContext(
@@ -14,11 +13,7 @@ const CollectionClearHint = props => {
     <p className='CollectionClearHint'>
       <Icon icon='books' /> A locally saved collection was found and loaded. If
       you would like to remove it and start fresh, you can{' '}
-      <button
-        type='button'
-        onClick={resetCollection}
-        className='CollectionClearHint__button ButtonAsLink'
-      >
+      <button type='button' onClick={resetCollection} className='ButtonAsLink'>
         clear the local data
       </button>{' '}
       (you will be asked to confirm).
