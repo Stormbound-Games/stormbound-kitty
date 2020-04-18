2c85fde76eff0ca80105c37a657120d0cce42faf
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-18 09:47:07 +0200

    Improve collection filters layout on mobile

--- src/components/CollectionFilters/index.js
+++ src/components/CollectionFilters/index.js
@@ -13,105 +13,118 @@ const CollectionFilters = props => (
     onSubmit={event => event.preventDefault()}
   >
     <Row desktopOnly>
-      <Column width={25}>
-        <FactionSelect
-          value={props.faction}
-          onChange={event => props.setFaction(event.target.value)}
-          withNeutral
-          withAny
-          data-testid='faction-select'
-        />
+      <Column>
+        <Row>
+          <Column>
+            <FactionSelect
+              value={props.faction}
+              onChange={event => props.setFaction(event.target.value)}
+              withNeutral
+              withAny
+              data-testid='faction-select'
+            />
+          </Column>
+          <Column>
+            <label htmlFor='status'>Status</label>
+            <select
+              name='status'
+              id='status'
+              value={props.status}
+              onChange={event => props.setStatus(event.target.value)}
+              data-testid='status-select'
+            >
+              <option value='*'>Any</option>
+              <option value='MISSING'>Missing</option>
+              <option value='UPGRADABLE'>Upgradable</option>
+              <option value='EXCESS'>Excess</option>
+            </select>
+          </Column>
+        </Row>
       </Column>
-
-      <Column width={25}>
-        <label htmlFor='status'>Status</label>
-        <select
-          name='status'
-          id='status'
-          value={props.status}
-          onChange={event => props.setStatus(event.target.value)}
-          data-testid='status-select'
-        >
-          <option value='*'>Any</option>
-          <option value='MISSING'>Missing</option>
-          <option value='UPGRADABLE'>Upgradable</option>
-          <option value='EXCESS'>Excess</option>
-        </select>
-      </Column>
-
-      <Column width={25}>
-        <label htmlFor='level'>Level</label>
-        <select
-          name='level'
-          id='level'
-          value={props.level}
-          onChange={event => props.setLevel(event.target.value)}
-          data-testid='level-select'
-        >
-          <option value='*'>Any</option>
-          <option value='1'>1</option>
-          <option value='2'>2</option>
-          <option value='3'>3</option>
-          <option value='4'>4</option>
-          <option value='5'>5</option>
-        </select>
-      </Column>
-
-      <Column width={25}>
-        <label htmlFor='rarity'>Rarity</label>
-        <select
-          name='rarity'
-          id='rarity'
-          value={props.rarity}
-          onChange={event => props.setRarity(event.target.value)}
-          data-testid='rarity-select'
-        >
-          <option value='*'>Any</option>
-          {Object.keys(RARITIES).map(rarity => (
-            <option value={rarity} key={rarity}>
-              {capitalise(rarity)}
-            </option>
-          ))}
-        </select>
+      <Column>
+        <Row>
+          <Column>
+            <label htmlFor='level'>Level</label>
+            <select
+              name='level'
+              id='level'
+              value={props.level}
+              onChange={event => props.setLevel(event.target.value)}
+              data-testid='level-select'
+            >
+              <option value='*'>Any</option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value='4'>4</option>
+              <option value='5'>5</option>
+            </select>
+          </Column>
+          <Column>
+            <label htmlFor='rarity'>Rarity</label>
+            <select
+              name='rarity'
+              id='rarity'
+              value={props.rarity}
+              onChange={event => props.setRarity(event.target.value)}
+              data-testid='rarity-select'
+            >
+              <option value='*'>Any</option>
+              {Object.keys(RARITIES).map(rarity => (
+                <option value={rarity} key={rarity}>
+                  {capitalise(rarity)}
+                </option>
+              ))}
+            </select>
+          </Column>
+        </Row>
       </Column>
     </Row>
 
     <Row desktopOnly>
-      <Column width={25}>
-        <label htmlFor='text'>Name</label>
-        <input
-          type='search'
-          name='text'
-          id='text'
-          value={props.text}
-          onChange={event => props.setText(event.target.value)}
-          placeholder='e.g. Faun'
-          data-testid='name-input'
-        />
+      <Column>
+        <Row>
+          <Column>
+            <label htmlFor='text'>Name</label>
+            <input
+              type='search'
+              name='text'
+              id='text'
+              value={props.text}
+              onChange={event => props.setText(event.target.value)}
+              placeholder='e.g. Faun'
+              data-testid='name-input'
+            />
+          </Column>
+          <Column>
+            <label htmlFor='order'>Order</label>
+            <select
+              name='order'
+              id='order'
+              value={props.order}
+              onChange={event => props.setOrder(event.target.value)}
+              data-testid='order-select'
+            >
+              <option value='NATURAL'>Natural</option>
+              <option value='VALUE'>Value</option>
+            </select>
+          </Column>
+        </Row>
       </Column>
-      <Column width={25}>
-        <label htmlFor='order'>Order</label>
-        <select
-          name='order'
-          id='order'
-          value={props.order}
-          onChange={event => props.setOrder(event.target.value)}
-          data-testid='order-select'
-        >
-          <option value='NATURAL'>Natural</option>
-          <option value='VALUE'>Value</option>
-        </select>
-      </Column>
-      <Column width={25} />
-      <Column width={25}>
-        <CTA
-          onClick={props.resetFilters}
-          type='button'
-          className='CollectionFilters__reset'
-          data-testid='reset-btn'
-        >
-          Reset filters
-        </CTA>
+      <Column>
+        <Row>
+          <Column />
+          <Column>
+            <CTA
+              onClick={props.resetFilters}
+              type='button'
+              className='CollectionFilters__reset'
+              data-testid='reset-btn'
+            >
+              Reset filters
+            </CTA>
+          </Column>
+        </Row>
       </Column>
     </Row>
   </form>
