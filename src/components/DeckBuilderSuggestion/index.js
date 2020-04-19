07ec506581a9dbdd8365176fa46c3cea8c384cda
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-19 16:03:43 +0200

    Display distance to maxed deck in a tooltip

--- src/components/DeckBuilderSuggestion/index.js
+++ src/components/DeckBuilderSuggestion/index.js
@@ -3,12 +3,27 @@ import { Link } from 'react-router-dom'
 import { CATEGORIES } from '../../constants/decks'
 import { CollectionContext } from '../CollectionProvider'
 import Deck from '../Deck'
+import QuestionMark from '../QuestionMark'
+import Only from '../Only'
 import RarityBar from '../RarityBar'
+import ResourceIcon from '../ResourceIcon'
+import Tooltip from '../Tooltip'
 import { deserialiseDeck } from '../../helpers/deserialise'
 import { serialiseDeck } from '../../helpers/serialise'
+import getDeckDistanceToMax from '../../helpers/getDeckDistanceToMax'
 import getRawCardData from '../../helpers/getRawCardData'
 import resolveCollection from '../../helpers/resolveCollection'
 import './index.css'
+
+const tooltipStyles = {
+  backgroundColor: 'var(--dark-blue)',
+  color: 'var(--white)',
+  borderRadius: '2px',
+  border: '1px solid var(--dark-beige)',
+  boxShadow: '0 0 0 2px var(--dark-blue)',
+  maxWidth: '15em',
+  whiteSpace: 'normal',
+}
 
 const DeckBuilderSuggestion = props => {
   const { hasDefaultCollection, collection } = React.useContext(
@@ -28,6 +43,8 @@ const DeckBuilderSuggestion = props => {
         level: resolvedCollection[card.id].level,
         missing: resolvedCollection[card.id].missing,
       }))
+  const id = serialiseDeck(deck)
+  const distance = getDeckDistanceToMax(resolvedCollection)({ id })
 
   return (
     <div className='DeckBuilderSuggestion'>
@@ -41,7 +58,26 @@ const DeckBuilderSuggestion = props => {
         <RarityBar deck={deck.map(({ id }) => getRawCardData(id))} />
       </div>
       <span className='DeckBuilderSuggestion__name'>
-        <Link to={`/deck/${serialiseDeck(deck)}`}>{props.name}</Link>
+        <Link to={`/deck/${id}`}>{props.name}</Link>
+        <Only.CustomCollection>
+          <Only.Desktop>
+            <Tooltip
+              style={tooltipStyles}
+              label={
+                distance === Infinity ? (
+                  'You are missing some cards from this deck'
+                ) : (
+                  <>
+                    Distance to maxed out deck:{' '}
+                    <ResourceIcon resource='STONE' /> {distance} fusion stones
+                  </>
+                )
+              }
+            >
+              {trigger => <QuestionMark {...trigger} />}
+            </Tooltip>
+          </Only.Desktop>
+        </Only.CustomCollection>
       </span>
       <span className='DeckBuilderSuggestion__author'>
         <Link to={`/deck/suggestions?category=${props.category}`}>
