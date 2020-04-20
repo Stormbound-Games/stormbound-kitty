8d12e23aaf6b53e485c6ddc7e1fdd51cbbda2624
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-20 21:07:05 +0200

    Render guides list from JSON file

--- src/components/Guides/index.js
+++ src/components/Guides/index.js
@@ -3,62 +3,30 @@ import { Link } from 'react-router-dom'
 import Banner from '../Banner'
 import InfoHint from '../InfoHint'
 import PageMeta from '../PageMeta'
+import guides from '../../data/guides'
 
 const Guides = props => (
   <>
     <h1 className='visually-hidden'>Guides</h1>
 
-    <Banner
-      faction='neutral'
-      title='Complete Guide'
-      subline='By Arikrat'
-      copy='Learn the basics of Stormbound and more with this in-depth guide by Arikrat. From battle placement to upgrade strategies, you’ll learn everything you’ll need to know to make your way towards Diamond!'
-      cta={{
-        'aria-label': 'Read complete guide by Arikrat',
-        to: '/guides/complete',
-        children: 'Read guide',
-      }}
-      image='/assets/images/cards/gifted_recruits.png'
-    />
-
-    <Banner
-      faction='shadowfen'
-      title='Deck Building Guide'
-      subline='By Zemeu'
-      copy='In this primer guide to building efficient decks, Zemeu goes through fundamental concepts such as mana curve, need for cheap units, and winning strategies. If you’re looking to making your own deck, you’re at the right place.'
-      cta={{
-        'aria-label': 'Read deck building guide by Zemeu',
-        to: '/guides/deck',
-        children: 'Read guide',
-      }}
-      image='/assets/images/cards/archdruid_earyn.png'
-    />
-
-    <Banner
-      faction='winter'
-      title='Winter Guide'
-      subline='By WinterBoii'
-      copy='In this deep-dive, WinterBoii tells us how to play, well, Winter Pact. From creating a deck to using niche abilities like Broken Earth Drakes’ and Rockworkers’, you should learn everything you need to know. Snow problem.'
-      cta={{
-        'aria-label': 'Read winter guide by WinterBoii',
-        to: '/guides/winter',
-        children: 'Read guide',
-      }}
-      image='/assets/images/cards/frozen_core.png'
-    />
-
-    <Banner
-      faction='neutral'
-      title='Pirate Guide'
-      subline='By MooreFunn'
-      copy='In this comprehensive guide, MooreFunn tells us how to conquer the seas with pirate cards! From composing an efficient deck to countering specific situations, you should learn everything you need to know to master pirate gameplay.'
-      cta={{
-        'aria-label': 'Read pirate guide by MooreFunn',
-        to: '/guides/pirate',
-        children: 'Read guide',
-      }}
-      image='/assets/images/cards/lucky_charmers.png'
-    />
+    {guides.map(guide => (
+      <Banner
+        faction={guide.faction}
+        title={guide.name}
+        subline={
+          <>
+            By <Link to={'/member/' + guide.author}>{guide.author}</Link>
+          </>
+        }
+        copy={guide.excerpt}
+        cta={{
+          'aria-label': `Read ${guide.name} by ${guide.author}`,
+          to: guide.link,
+          children: 'Read guide',
+        }}
+        image={guide.image}
+      />
+    ))}
 
     <Banner
       faction='ironclad'
