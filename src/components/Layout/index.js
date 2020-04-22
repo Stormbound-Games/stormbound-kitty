26bba4b1359438d40e309a0ace9e437e92bbdc25
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-22 17:54:14 +0200

    Add fade-in page transitions

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import Footer from '../Footer'
 import Header from '../Header'
 import './index.css'
@@ -6,7 +7,17 @@ import './index.css'
 const Layout = props => (
   <div className='Layout'>
     <Header active={props.active} />
-    <main className='Layout__body'>{props.children}</main>
+    <main className='Layout__body'>
+      <motion.div
+        key='layout'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: 'easeOut', duration: 0.8 }}
+      >
+        {props.children}
+      </motion.div>
+    </main>
     <Footer />
   </div>
 )
