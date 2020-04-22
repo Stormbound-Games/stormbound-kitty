bea6da98f8537cf24678c2d72217965852d684d1
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-22 11:53:29 +0200

    Enforce a minimum loading time to ease transitions

--- src/helpers/load.js
+++ src/helpers/load.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import pMinDelay from 'p-min-delay'
 import loadable from '@loadable/component'
 import Loader from '../components/Loader'
 
 const options = { fallback: <Loader /> }
-const load = name => loadable(() => import('../components/' + name), options)
+const load = name =>
+  loadable(() => pMinDelay(import('../components/' + name), 300), options)
 
 export default load
