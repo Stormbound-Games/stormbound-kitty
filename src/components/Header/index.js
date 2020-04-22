04594f0363503c38b3dfb7df411f0770742037e5
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-22 17:06:07 +0200

    Avoid header flickering caused by mobile layout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import NavBattleSim from '../NavBattleSim'
 import NavCardBuilder from '../NavCardBuilder'
 import NavCollection from '../NavCollection'
@@ -38,89 +38,94 @@ const SubNav = props => {
   }
 }
 
-const Header = props => {
+const Wrapper = props => {
   const viewportWidth = useViewportWidth()
-  const { pathname } = useLocation()
   const [isExpanded, expand] = React.useState(false)
 
-  React.useEffect(() => expand(false), [pathname])
+  if (viewportWidth > 700) {
+    return props.children
+  }
 
   return (
-    <header role='banner' className='Header'>
-      <TogglableContent
-        id='navigation'
-        isExpanded={viewportWidth > 700 ? true : isExpanded}
-        renderToggle={toggleProps =>
-          viewportWidth > 700 ? null : (
-            <>
-              <button
-                {...toggleProps}
-                type='button'
-                onClick={() => expand(s => !s)}
-                className='Header__toggle'
-                title={isExpanded ? 'Close menu' : 'Open menu'}
-                aria-label={isExpanded ? 'Close menu' : 'Open menu'}
-              >
-                ☰
-              </button>
-              <Link to='/' className='Header__title'>
-                Stormbound Kitty
-              </Link>
-            </>
-          )
-        }
-      >
-        <nav className='Header__nav'>
-          <ul className='Header__list'>
-            <Only.Desktop>
-              <li className='Header__item'>
-                <NavLink to='/' active={props.active === 'HOME'}>
-                  <Icon icon='home' /> Home
-                </NavLink>
-              </li>
-            </Only.Desktop>
-            <li className='Header__item'>
-              <NavLink to='/sim' active={props.active === 'BATTLE_SIM'}>
-                <Icon icon='sword' /> Battle Sim
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/deck' active={props.active === 'DECK_BUILDER'}>
-                <Icon icon='stack' /> Deck builder
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/collection' active={props.active === 'COLLECTION'}>
-                <Icon icon='books' /> Collection
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/card' active={props.active === 'CARD_BUILDER'}>
-                <Icon icon='wand' /> Card builder
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/list' active={props.active === 'LIST_BUILDER'}>
-                <Icon icon='template' /> List Builder
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/stories' active={props.active === 'STORIES'}>
-                <Icon icon='quill' /> Stories
-              </NavLink>
-            </li>
-            <li className='Header__item'>
-              <NavLink to='/guides' active={props.active === 'GUIDES'}>
-                <Icon icon='compass' /> Guides
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-
-        {Boolean(props.active) && <SubNav active={props.active} />}
-      </TogglableContent>
-    </header>
+    <TogglableContent
+      id='navigation'
+      isExpanded={isExpanded}
+      renderToggle={toggleProps => (
+        <>
+          <button
+            {...toggleProps}
+            type='button'
+            onClick={() => expand(s => !s)}
+            className='Header__toggle'
+            title={isExpanded ? 'Close menu' : 'Open menu'}
+            aria-label={isExpanded ? 'Close menu' : 'Open menu'}
+          >
+            ☰
+          </button>
+          <Link to='/' className='Header__title'>
+            Stormbound Kitty
+          </Link>
+        </>
+      )}
+    >
+      {props.children}
+    </TogglableContent>
   )
 }
+
+const Header = props => (
+  <header role='banner' className='Header'>
+    <Wrapper>
+      <nav className='Header__nav'>
+        <ul className='Header__list'>
+          <Only.Desktop>
+            <li className='Header__item'>
+              <NavLink to='/' active={props.active === 'HOME'}>
+                <Icon icon='home' /> Home
+              </NavLink>
+            </li>
+          </Only.Desktop>
+          <li className='Header__item'>
+            <NavLink to='/sim' active={props.active === 'BATTLE_SIM'}>
+              <Icon icon='sword' /> Battle Sim
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/deck' active={props.active === 'DECK_BUILDER'}>
+              <Icon icon='stack' /> Deck builder
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/collection' active={props.active === 'COLLECTION'}>
+              <Icon icon='books' /> Collection
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/card' active={props.active === 'CARD_BUILDER'}>
+              <Icon icon='wand' /> Card builder
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/list' active={props.active === 'LIST_BUILDER'}>
+              <Icon icon='template' /> List Builder
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/stories' active={props.active === 'STORIES'}>
+              <Icon icon='quill' /> Stories
+            </NavLink>
+          </li>
+          <li className='Header__item'>
+            <NavLink to='/guides' active={props.active === 'GUIDES'}>
+              <Icon icon='compass' /> Guides
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      {Boolean(props.active) && <SubNav active={props.active} />}
+    </Wrapper>
+  </header>
+)
 
 export default Header
