6cc577ffce49e50ac42aae6329d2947196212d24
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-23 18:11:39 +0200

    Add download button to all images from the fan-kit page

--- src/components/FanKit/index.js
+++ src/components/FanKit/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import LazyLoad from 'react-lazyload'
 import Column from '../Column'
+import CTA from '../CTA'
+import Dialog from '../Dialog'
+import Icon from '../Icon'
 import Image from '../Image'
 import Loader from '../Loader'
 import PageMeta from '../PageMeta'
@@ -11,7 +14,59 @@ import sortCards from '../../helpers/sortCards'
 import useViewportWidth from '../../hooks/useViewportWidth'
 import './index.css'
 
+const Download = props => (
+  <button
+    className='FanKit__download'
+    onClick={() => props.setActive(props.id)}
+    title={'Download asset ' + props.name}
+  >
+    <Icon icon='download' />
+  </button>
+)
+
+const DownloadDialog = props => {
+  const { name, image } = props.activeCard || {}
+
+  return (
+    <Dialog
+      id='dialog'
+      dialogRef={props.dialogRef}
+      close={props.close}
+      title={name || 'Download image'}
+      image={image}
+    >
+      {image ? (
+        <>
+          <p>
+            <a href={image} target='_blank' rel='noopener noreferrer'>
+              Open image in new tab
+            </a>{' '}
+            or download it as:
+          </p>
+          <Row>
+            <Column>
+              <CTA href={image} download>
+                PNG
+              </CTA>
+            </Column>
+            <Column>
+              <CTA href={image.replace('.png', '.webp')} download>
+                WebP
+              </CTA>
+            </Column>
+          </Row>
+        </>
+      ) : (
+        <p>An error has occurred.</p>
+      )}
+    </Dialog>
+  )
+}
+
 const FanKit = props => {
+  const dialogRef = React.useRef(null)
+  const [active, setActive] = React.useState(null)
+  const activeCard = active ? cards.find(card => card.id === active) : null
   const viewportWidth = useViewportWidth()
   const items = React.useMemo(
     () =>
@@ -25,9 +80,24 @@ const FanKit = props => {
     []
   )
 
+  React.useEffect(() => {
+    if (!dialogRef.current) return
+    if (active) dialogRef.current.show()
+    else dialogRef.current.hide()
+  }, [active])
+
   return (
     <>
       <h1 className='VisuallyHidden'>Stormbound Fan-kit</h1>
+
+      <DownloadDialog
+        activeCard={activeCard}
+        dialogRef={instance => {
+          dialogRef.current = instance
+          dialogRef.current.on('hide', () => setActive(null))
+        }}
+        close={() => dialogRef.current.hide()}
+      />
 
       {items.map(([a, b, c]) => (
         <LazyLoad
@@ -39,16 +109,25 @@ const FanKit = props => {
         >
           <Row desktopOnly>
             <Column width={33}>
-              <Image src={a.image} alt={a.name} className='FanKit__image' />
+              <div className='FanKit__item'>
+                <Download {...a} setActive={setActive} />
+                <Image src={a.image} alt={a.name} className='FanKit__image' />
+              </div>
             </Column>
             <Column width={33}>
               {b && (
-                <Image src={b.image} alt={b.name} className='FanKit__image' />
+                <div className='FanKit__item'>
+                  <Download {...b} setActive={setActive} />
+                  <Image src={b.image} alt={b.name} className='FanKit__image' />
+                </div>
               )}
             </Column>
             <Column width={33}>
               {c && (
-                <Image src={c.image} alt={c.name} className='FanKit__image' />
+                <div className='FanKit__item'>
+                  <Download {...c} setActive={setActive} />
+                  <Image src={c.image} alt={c.name} className='FanKit__image' />
+                </div>
               )}
             </Column>
           </Row>
