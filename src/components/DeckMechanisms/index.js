1a9f187ce1dc1d7cca92ee92fa0b22e030708f89
Neicigam <neicigam95@gmail.com>
2020-04-23 17:43:56 +0300

    Removed Unhealthy Hysteria fix

--- src/components/DeckMechanisms/index.js
+++ src/components/DeckMechanisms/index.js
@@ -449,7 +449,7 @@ export default class DeckMechanisms extends React.Component {
     }
 
     if (this.state.turn === 1) {
-      const unplayableSpells = ['W1', 'S10', 'N15', 'N63']
+      const unplayableSpells = ['W1', 'S10', 'N15']
 
       if (this.state.specifics.noUnitsOnFirstTurn) {
         unplayableSpells.push('F4')
