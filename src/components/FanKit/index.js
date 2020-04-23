07828e58123e54274a976017ab8e05ba4c9079c3
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-23 17:32:01 +0200

    Add lazy loading to the fan-kit page

--- src/components/FanKit/index.js
+++ src/components/FanKit/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import LazyLoad from 'react-lazyload'
 import Column from '../Column'
 import Image from '../Image'
+import Loader from '../Loader'
 import PageMeta from '../PageMeta'
 import Row from '../Row'
 import cards from '../../data/cards'
 import chunk from '../../helpers/chunk'
 import sortCards from '../../helpers/sortCards'
+import useViewportWidth from '../../hooks/useViewportWidth'
 import './index.css'
 
 const FanKit = props => {
+  const viewportWidth = useViewportWidth()
   const items = React.useMemo(
     () =>
       chunk(
@@ -26,21 +30,29 @@ const FanKit = props => {
       <h1 className='VisuallyHidden'>Stormbound Fan-kit</h1>
 
       {items.map(([a, b, c]) => (
-        <Row desktopOnly>
-          <Column width={33}>
-            <Image src={a.image} alt={a.name} className='FanKit__image' />
-          </Column>
-          <Column width={33}>
-            {b && (
-              <Image src={b.image} alt={b.name} className='FanKit__image' />
-            )}
-          </Column>
-          <Column width={33}>
-            {c && (
-              <Image src={c.image} alt={c.name} className='FanKit__image' />
-            )}
-          </Column>
-        </Row>
+        <LazyLoad
+          offset={150}
+          resize
+          placeholder={<Loader hideLabel />}
+          height={viewportWidth > 700 ? 300 : 900}
+          key={a.id}
+        >
+          <Row desktopOnly>
+            <Column width={33}>
+              <Image src={a.image} alt={a.name} className='FanKit__image' />
+            </Column>
+            <Column width={33}>
+              {b && (
+                <Image src={b.image} alt={b.name} className='FanKit__image' />
+              )}
+            </Column>
+            <Column width={33}>
+              {c && (
+                <Image src={c.image} alt={c.name} className='FanKit__image' />
+              )}
+            </Column>
+          </Row>
+        </LazyLoad>
       ))}
 
       <PageMeta
