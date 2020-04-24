4dc38b3b902e230966959a116bd0a89750c904a8
Neicigam <neicigam95@gmail.com>
2020-04-24 14:41:00 +0300

    Changed objects to arrays

--- src/components/DeckMechanisms/index.js
+++ src/components/DeckMechanisms/index.js
@@ -22,37 +22,13 @@ export const FRIENDLY_CHANCES = {
 
 const FROZEN_ENEMIES_AFTER = {
   // Frosthexers
-  W2: {
-    0: 1,
-    1: 2,
-    2: 3,
-    3: 3,
-    4: 4,
-  },
+  W2: [1, 2, 3, 3, 4],
   // Moment's Peace
-  W6: {
-    0: 3,
-    1: 3,
-    2: 3,
-    3: 4,
-    4: 4,
-  },
+  W6: [3, 3, 3, 4, 4],
   // Midwinter Chaos
-  W11: {
-    0: 3,
-    1: 3,
-    2: 3,
-    3: 2,
-    4: 1,
-  },
+  W11: [3, 3, 3, 2, 1],
   // Wisp Cloud
-  W4: {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 1,
-    4: 1,
-  },
+  W4: [0, 0, 0, 1, 1],
 }
 
 const getDefaultState = props => ({
