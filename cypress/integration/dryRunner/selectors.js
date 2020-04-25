cceaefdd6ed81740717b7e55e57cd0a1d53ff425
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-25 11:14:56 +0200

    Reorganise dry-runner tests

---
+++ cypress/integration/dryRunner/selectors.js
@@ -0,0 +1,9 @@
+export default {
+  MANA: '[data-testid="mana-pool"]',
+  CHANCES_CHECKBOX: '[data-testid="display-chance"]',
+  DECK_CARD: '.Deck__card',
+  CARD: '[data-testid="hand"] [data-testid="card"]',
+  CYCLE_BTN: '[data-testid="cycle-btn"]',
+  PLAY_BTN: '[data-testid="play-btn"]',
+  END_TURN_BTN: '[data-testid="end-turn-btn"]',
+}
