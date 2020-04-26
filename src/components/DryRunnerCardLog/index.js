e5fd90a87b7f9f30bcdab6c2860c7fade665f5cd
Neicigam <neicigam95@gmail.com>
2020-04-26 22:38:05 +0200

    Added more CSS styling

--- src/components/DryRunnerCardLog/index.js
+++ src/components/DryRunnerCardLog/index.js
@@ -5,27 +5,26 @@ import Row from '../Row'
 import arrayPad from '../../helpers/arrayPad'
 import './index.css'
 
-const DryRunnerCardLog = props =>
-  React.useMemo(() => {
-    const cards = arrayPad(props.cards.slice(0, 6), 6, null, +1)
-    return (
-      <div className='DryRunnerCardLog'>
-        <Row>
-          {cards.map((card, index) => (
-            <Column width='1/6' key={(card ? card.id : '') + index}>
-              {card && (
-                <Image
-                  wrapperClassName='DryRunnerCardLog__image-wrapper'
-                  className='DryRunnerCardLog__image'
-                  src={card.image}
-                  alt={card.name}
-                />
-              )}
-            </Column>
-          ))}
-        </Row>
-      </div>
-    )
-  }, [props.cards])
+const DryRunnerCardLog = React.memo(props => {
+  const cards = arrayPad(props.cards.slice(0, 6), 6, null, +1)
+  return (
+    <div className='DryRunnerCardLog'>
+      <Row>
+        {cards.map((card, index) => (
+          <Column width='1/6' key={(card ? card.id : '') + index}>
+            {card && (
+              <Image
+                wrapperClassName='DryRunnerCardLog__image-wrapper'
+                className='DryRunnerCardLog__image'
+                src={card.image}
+                alt={card.name}
+              />
+            )}
+          </Column>
+        ))}
+      </Row>
+    </div>
+  )
+})
 
 export default DryRunnerCardLog
