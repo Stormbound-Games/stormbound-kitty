a4c61468144b2f2728032e51f3d378bfc4030833
Neicigam <neicigam95@gmail.com>
2020-04-26 22:54:39 +0200

    Add opacity change and title

--- src/components/DryRunnerCardLog/index.js
+++ src/components/DryRunnerCardLog/index.js
@@ -8,22 +8,29 @@ import './index.css'
 const DryRunnerCardLog = React.memo(props => {
   const cards = arrayPad(props.cards.slice(0, 6), 6, null, +1)
   return (
-    <div className='DryRunnerCardLog'>
-      <Row>
-        {cards.map((card, index) => (
-          <Column width='1/6' key={(card ? card.id : '') + index}>
-            {card && (
-              <Image
-                wrapperClassName='DryRunnerCardLog__image-wrapper'
-                className='DryRunnerCardLog__image'
-                src={card.image}
-                alt={card.name}
-              />
-            )}
-          </Column>
-        ))}
-      </Row>
-    </div>
+    <>
+      <h2 className='DryRunnerCardLog__title'>Last played cards</h2>
+      <div className='DryRunnerCardLog__container'>
+        <Row>
+          {cards.map((card, index) => (
+            <Column
+              style={{ opacity: 1 - index / 8 }}
+              width='1/6'
+              key={(card ? card.id : '') + index}
+            >
+              {card && (
+                <Image
+                  wrapperClassName='DryRunnerCardLog__image-wrapper'
+                  className='DryRunnerCardLog__image'
+                  src={card.image}
+                  alt={card.name}
+                />
+              )}
+            </Column>
+          ))}
+        </Row>
+      </div>
+    </>
   )
 })
 
