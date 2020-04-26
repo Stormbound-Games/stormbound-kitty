3e637613fd0ceab024441be8f9f3bf427f9bf746
Neicigam <neicigam95@gmail.com>
2020-04-26 22:13:37 +0200

    Fix image size

--- src/components/DryRunnerCardLog/index.js
+++ src/components/DryRunnerCardLog/index.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import Column from '../Column'
 import Image from '../Image'
+import Row from '../Row'
+import arrayPad from '../../helpers/arrayPad'
 import './index.css'
 
 const DryRunnerCardLog = props => {
   return (
     <div className='DryRunnerCardLog'>
-      {props.cards.slice(0, 6).map((card, index) => (
-        <Column
-          width='1/6'
-          key={card.id + ' #' + index}
-          style={{ flexGrow: 0 }}
-        >
-          <Image
-            className='DryRunnerCardLog__image'
-            src={card.image}
-            alt={card.name}
-          />
-        </Column>
-      ))}
+      <Row>
+        {arrayPad(props.cards.slice(0, 6), 6, null, +1).map((card, index) => (
+          <Column width='1/6' key={(card ? card.id : '') + index}>
+            {card && (
+              <Image
+                wrapperClassName='DryRunnerCardLog__image-wrapper'
+                className='DryRunnerCardLog__image'
+                src={card.image}
+                alt={card.name}
+              />
+            )}
+          </Column>
+        ))}
+      </Row>
     </div>
   )
 }
