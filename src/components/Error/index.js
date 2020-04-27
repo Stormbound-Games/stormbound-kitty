0eabd8ac89bdd5acbccdcdbe308aba6198a9bb05
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-27 18:14:14 +0200

    Provide a better error in case of failed chunk

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -15,23 +15,32 @@ export default React.memo(function Error(props) {
 
         <Image src='/assets/images/cards/sweetcap_kittens.png' alt='' />
 
-        <p>
-          It looks like something went wrong… It’s definitely not your fault
-          though, don’t worry!
-        </p>
-        <p>
-          What you can do is report it to Kitty#1909 on the{' '}
-          <a
-            href='https://discord.gg/stormbound'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Stormbound Discord
-          </a>
-          , along with the URL of this page and the following error:
-        </p>
+        {props.error === 'ChunkLoadError' ? (
+          <p>
+            You must have changed page during a re-deployment of the site. Very
+            sorry about this! Please refresh the page or press “Retry”.
+          </p>
+        ) : (
+          <>
+            <p>
+              It looks like something went wrong… It’s definitely not your fault
+              though, don’t worry!
+            </p>
+            <p>
+              What you can do is report it to Kitty#1909 on the{' '}
+              <a
+                href='https://discord.gg/stormbound'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Stormbound Discord
+              </a>
+              , along with the URL of this page and the following error:
+            </p>
 
-        <pre className='Error__pre'>{props.error}</pre>
+            <pre className='Error__pre'>{props.error}</pre>
+          </>
+        )}
 
         {props.retry && <CTA onClick={props.retry}>Retry</CTA>}
       </div>
--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -11,13 +11,39 @@ export default class ErrorBoundary extends React.Component {
     return { hasError: true, error: error.message }
   }
 
+  handleError = event => {
+    // The `ChunkLoadError` happens when navigating during a deployment. The
+    // current chunks have been wiped from the `build` folder and replaced with
+    // new ones, yielding a loading error. Only a reload can fix it.
+    if (event.error.name === 'ChunkLoadError') {
+      this.setState({ hasError: true, error: event.error.name })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('error', this.handleError)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('error', this.handleError)
+  }
+
   componentDidCatch(error, info) {
     console.error({ error, info })
   }
 
   render() {
     if (this.state.hasError) {
-      return <Error error={this.state.error} />
+      return (
+        <Error
+          error={this.state.error}
+          retry={
+            this.state.error === 'ChunkLoadError'
+              ? () => window.location.reload()
+              : undefined
+          }
+        />
+      )
     }
 
     return this.props.children
