52118d3fb5e8a382476b57e8cf89c2a303536552
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-27 13:30:15 +0200

    Add banner about guides

---
+++ src/components/EyeCatcher/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './index.css'
+
+const hasBeenShownYet = key => {
+  try {
+    console.log(localStorage.getItem(key))
+    return JSON.parse(localStorage.getItem(key))
+  } catch (error) {
+    return false
+  }
+}
+
+const EyeCatcher = props => {
+  const key = 'sb.eye_catcher.' + props.id
+  const [isVisible, setIsVisible] = React.useState(!hasBeenShownYet(key))
+
+  React.useEffect(() => {
+    localStorage.setItem(key, !isVisible)
+  }, [key, isVisible])
+
+  if (!isVisible) {
+    return null
+  }
+
+  return (
+    <p className='EyeCatcher'>
+      <span role='img' aria-label='Sparkles'>
+        ✨
+      </span>{' '}
+      {props.children}
+      <button
+        className='ButtonAsLink EyeCatcher__close'
+        type='button'
+        onClick={() => setIsVisible(false)}
+      >
+        (Dismiss)
+      </button>
+    </p>
+  )
+}
+
+export default EyeCatcher
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
+import EyeCatcher from '../EyeCatcher'
 import Footer from '../Footer'
 import Header from '../Header'
+import Only from '../Only'
 import './index.css'
 
 const Layout = React.memo(props => (
   <div className='Layout'>
+    <Only.Desktop>
+      <EyeCatcher id='guides_authors'>
+        Fellow travelers, Stormbound-Kitty is looking for{' '}
+        <Link to='/faq#adding-a-guide'>guides authors</Link>!
+      </EyeCatcher>
+    </Only.Desktop>
+
     <Header active={props.active} />
+
     <main className='Layout__body'>
       <motion.div
         key='layout'
@@ -18,6 +29,7 @@ const Layout = React.memo(props => (
         {props.children}
       </motion.div>
     </main>
+
     <Footer />
   </div>
 ))
