9ea16852cbef63b2d3e29b83c23c415c6ef5d715
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-04-30 22:08:29 +0200

    Improve header of guides

--- src/components/Guide/index.js
+++ src/components/Guide/index.js
@@ -1,20 +1,29 @@
 import React from 'react'
+import HeaderBanner from '../HeaderBanner'
+import Only from '../Only'
 import PageMeta from '../PageMeta'
 import Title from '../Title'
 import './index.css'
 
 export default React.memo(function Guide(props) {
   return (
-    <div className={`Guide ${props.className}`}>
-      <Title element='h1'>{props.title}</Title>
-      <p className='Guide__author'>
-        This guide has been authored by {props.author} and is published here
-        with their consent. All credits to the original author.
-      </p>
+    <>
+      <Only.Desktop>
+        <HeaderBanner title={props.name} background={props.background} />
+      </Only.Desktop>
+      <div className={['Guide', props.className].filter(Boolean).join(' ')}>
+        <Only.Mobile>
+          <Title>{props.name}</Title>
+        </Only.Mobile>
+        <p className='Guide__author'>
+          This guide has been authored by {props.author} and is published here
+          with their consent. All credits to the original author.
+        </p>
 
-      {props.children}
+        {props.children}
 
-      <PageMeta title={props.title} description={props.description} />
-    </div>
+        <PageMeta title={props.title} description={props.description} />
+      </div>
+    </>
   )
 })
