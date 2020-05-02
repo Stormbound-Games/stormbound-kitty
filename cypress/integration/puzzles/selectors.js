c58d866f4fb1c0f44f3f34eddbf1ea8be342c3b6
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-02 10:23:47 +0200

    Add tests for puzzles

---
+++ cypress/integration/puzzles/selectors.js
@@ -0,0 +1,12 @@
+export default {
+  DIFFICULTY_SELECT: '[data-testid="difficulty-select"]',
+  TYPE_SELECT: '[data-testid="type-select"]',
+  NAME_INPUT: '[data-testid="name-input"]',
+  PUZZLE: '[data-testid="puzzle"]',
+  PUZZLE_DIFFICULTY: '[data-testid="puzzle-difficulty"]',
+  PUZZLE_TYPE: '[data-testid="puzzle-type"]',
+  PUZZLE_RESTRICTIONS: '[data-testid^="puzzle-restrictions"]',
+  PUZZLE_LINK: '[data-testid="puzzle-link"]',
+  RESTRICTION_TOGGLE: '[aria-controls="puzzles-filters-target"]',
+  RESTRICTION_CHECKBOXES: '[data-testid="restriction-checkbox"]',
+}
--- src/components/BattleSimPuzzle/index.js
+++ src/components/BattleSimPuzzle/index.js
@@ -21,20 +21,22 @@ export default React.memo(function BattleSimPuzzle(props) {
       )}
       <div className='BattleSimPuzzle__content'>
         <h3 className='BattleSimPuzzle__name'>
-          <Link to={`/sim/${props.board}/display`}>{props.name}</Link>
+          <Link to={`/sim/${props.board}/display`} data-testid='puzzle-link'>
+            {props.name}
+          </Link>
         </h3>
         <p className='BattleSimPuzzle__author'>by {props.author}</p>
         <dl className='BattleSimPuzzle__details'>
-          <dt>{props.type.toLowerCase()}:</dt>
+          <dt data-testid='puzzle-type'>{props.type.toLowerCase()}:</dt>
           <dd>
             {props.description || TYPES[props.type]} {props.note}
           </dd>
 
           <dt>Difficulty:</dt>
-          <dd>{props.difficulty}/3</dd>
+          <dd data-testid='puzzle-difficulty'>{props.difficulty}/3</dd>
 
           <dt>Restrictions:</dt>
-          <dd>
+          <dd data-testid={'puzzle-restrictions'}>
             {props.restrictions.length > 0
               ? [...props.restrictions].sort().map((restriction, index) => (
                   <React.Fragment key={index}>
