34bb85f87960ea5f5ce2eedae29973900add68e0
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-03 13:03:42 +0200

    Fetch full story from JSON file in ‘Story’

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link, useRouteMatch } from 'react-router-dom'
-import stories from '../../data/stories'
 import Article from '../Article'
 import Error from '../Error'
 import InfoHint from '../InfoHint'
+import Loader from '../Loader'
 import MicroMarkdown from '../MicroMarkdown'
 import PageMeta from '../PageMeta'
 import Stories from '../Stories'
@@ -11,52 +11,48 @@ import Title from '../Title'
 import getRawCardData from '../../helpers/getRawCardData'
 import getExcerpt from '../../helpers/getExcerpt'
 import getReadingTime from '../../helpers/getReadingTime'
+import useFetch from '../../hooks/useFetch'
 
-const getStoriesFromAuthor = author =>
-  stories.filter(story => story.author === author)
+const getStoriesFromAuthor = (stories = [], story) =>
+  story ? stories.filter(s => s.author === story.author) : []
 
 export default function Story(props) {
   const match = useRouteMatch()
   const { storyId: id } = match.params
-
-  let story = null
-  let storiesByAuthor = []
-
-  try {
-    const decoded = decodeURIComponent(window.atob(id))
-    const [title, author] = decoded.split('-')
-    storiesByAuthor = getStoriesFromAuthor(author)
-    story = storiesByAuthor.find(story => story.title === title)
-
-    if (!story) throw new Error('STORY_NOT_FOUND')
-  } catch (error) {
-    return <Error error={'Story ' + id + ' not found'} />
-  }
-
-  const card = getRawCardData(story.cardId)
+  const path = '/stories/' + id + '.json'
+  const { data: story, loading } = useFetch(path)
+  const { data: stories = [] } = useFetch('/stories.json')
+  const card = story ? getRawCardData(story.cardId) : {}
+  const storiesFromAuthor = getStoriesFromAuthor(stories, story)
 
   return (
     <div className='Story'>
-      <Article
-        title={story.title}
-        author={story.author}
-        readingTime={getReadingTime(story.content)}
-        backLink={{
-          to: '/stories',
-          children: 'Back to stories',
-        }}
-      >
-        <MicroMarkdown content={story.content} />
-      </Article>
+      {loading ? (
+        <Loader />
+      ) : story ? (
+        <Article
+          title={story.title}
+          author={story.author}
+          readingTime={getReadingTime(story.content)}
+          backLink={{
+            to: '/stories',
+            children: 'Back to stories',
+          }}
+        >
+          <MicroMarkdown content={story.content} />
+        </Article>
+      ) : (
+        <Error error='Error fetching stories.' />
+      )}
 
-      {storiesByAuthor.length > 1 && (
+      {storiesFromAuthor.length > 1 && (
         <>
           <Title>
             Other stories by{' '}
             <Link to={`/member/${story.author}`}>{story.author}</Link>
           </Title>
           <Stories
-            stories={storiesByAuthor.filter(s => story.title !== s.title)}
+            stories={storiesFromAuthor.filter(s => story.title !== s.title)}
             columns={3}
           />
         </>
@@ -67,12 +63,14 @@ export default function Story(props) {
         <Link to='/faq#adding-a-story'>Have your own story published</Link>.
       </InfoHint>
 
-      <PageMeta
-        title={story.title}
-        author={story.author}
-        image={card.image}
-        description={getExcerpt(story.content.replace('---', ''), 160)}
-      />
+      {story && (
+        <PageMeta
+          title={story.title}
+          author={story.author}
+          image={card.image}
+          description={getExcerpt(story.content.replace('---', ''), 160)}
+        />
+      )}
     </div>
   )
 }
