35dbb2370faef5612c8fe74c266f0156fdb60498
Neicigam <neicigam95@gmail.com>
2020-05-03 21:36:09 +0200

    Small fix in collection value calculation

--- src/helpers/getCollectionCost.js
+++ src/helpers/getCollectionCost.js
@@ -4,67 +4,25 @@ import resolveCardForLevel from './resolveCardForLevel'
 const sum = (a, b) => a + b
 
 export const getCardCost = card => {
+  // If the card is missing, it has no value.
+  if (card.missing) return 0
   const { rarity } = resolveCardForLevel(card)
   const {
     copies: levelCopies,
     stonesForMissing: craftingCost,
     stonesPerMissingCopy: stonesPerCopy,
-    stonesPerExtraCopy,
   } = RARITY_COPIES[rarity]
+  // If the card is level 3 (for example), it represents copies equal to the
+  // current unused copies added to those needed to bring it to level 3,
+  // stored in levelCopies[0] (1 to 2) and levelCopies[1] (3 to 3)
+  const conditionalSum = (acc, levelCop, index) =>
+    index < card.level - 1 ? acc + levelCop : acc
+  const copies = card.copies + levelCopies.reduce(conditionalSum, 0)
+  // Copies that could be exchanged for coins do not count towards the
+  // fusion stones value of the card
+  const maxCopies = levelCopies.reduce(sum, 0)
 
-  // If the card is missing, it has no value.
-  if (card.missing) return 0
-
-  switch (card.level) {
-    // If the card is level 1, its total value is its crafting cost + the
-    // amount of copies times the value of a copy.
-    case 1: {
-      return craftingCost + card.copies * stonesPerCopy
-    }
-
-    // If the card is level 2, its total value is its crafting cost + the amount
-    // of copies to get to level 2 times the value of a copy + the amount of
-    // extra copies times the value of a copy.
-    case 2: {
-      const copies = levelCopies[0] + card.copies
-
-      return craftingCost + copies * stonesPerCopy
-    }
-
-    // If the card is level 3, its total value is its crafting cost + the amount
-    // of copies to get to level 3 + the amount of extra copies times the value
-    // of a copy.
-    case 3: {
-      const copies = levelCopies[0] + levelCopies[1] + card.copies
-
-      return craftingCost + copies * stonesPerCopy
-    }
-
-    // If the card is level 4, its total value is its crafting cost + the amount
-    // of copies to get to level 4 + the amount of extra copies times the value
-    // of a copy.
-    case 4: {
-      const copies =
-        levelCopies[0] + levelCopies[1] + levelCopies[2] + card.copies
-
-      return craftingCost + copies * stonesPerCopy
-    }
-
-    // If the card is level 5, its total value is its crafting cost + the
-    // amount of copies to get to level 5 times the value of a copy + the
-    // amount of extra copies times the value of an extra copy past level 5.
-    case 5: {
-      const copies =
-        levelCopies[0] + levelCopies[1] + levelCopies[2] + levelCopies[3]
-
-      return (
-        craftingCost + copies * stonesPerCopy + card.copies * stonesPerExtraCopy
-      )
-    }
-
-    default:
-      return 0
-  }
+  return craftingCost + Math.min(copies, maxCopies) * stonesPerCopy
 }
 
 export default collection => {
