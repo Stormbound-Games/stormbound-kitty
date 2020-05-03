6df3fa525f0eb92b136cb015585c2631e5f29e85
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-03 22:15:23 +0200

    Add tests for ‘unfoldValue’

---
+++ cypress/integration/helpers/unfoldValue.js
@@ -0,0 +1,9 @@
+import unfoldValue from '../../../src/helpers/unfoldValue'
+
+describe('The `unfoldValue` helper', () => {
+  it('should stringify and split on slashes', () => {
+    expect(unfoldValue('kitty')).to.deep.equal(['kitty'])
+    expect(unfoldValue('k/i/t/t/y')).to.deep.equal(['k', 'i', 't', 't', 'y'])
+    expect(unfoldValue(1)).to.deep.equal(['1'])
+  })
+})
