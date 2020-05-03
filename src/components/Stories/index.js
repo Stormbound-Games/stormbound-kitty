d9c01d81389afa1abb6c9e47e70d2739549a8682
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-03 13:01:40 +0200

    Make ‘Stories’ more resilient to lack of stories

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -5,7 +5,7 @@ import StoryTeaser from '../StoryTeaser'
 import chunk from '../../helpers/chunk'
 
 export default React.memo(function Stories(props) {
-  return chunk(props.stories, props.columns).map((row, index) => (
+  return chunk(props.stories || [], props.columns).map((row, index) => (
     <Row key={index} desktopOnly wideGutter>
       {Array.from({ length: props.columns }, (_, index) => (
         <Column
