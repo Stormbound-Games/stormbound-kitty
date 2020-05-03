c66c4ebae6a3e13aed44824a22ebbd0e7213819b
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-03 18:20:06 +0200

    Remove reading time from story teasers

--- src/components/StoryTeaser/index.js
+++ src/components/StoryTeaser/index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Teaser from '../Teaser'
 import getExcerpt from '../../helpers/getExcerpt'
-import getReadingTime from '../../helpers/getReadingTime'
 import './index.css'
 
 const StoryMeta = props => (
   <>
+    Story by{' '}
     <Link className='StoryTeaser__author' to={'/member/' + props.author}>
       {props.author}
     </Link>{' '}
-    · {getReadingTime(props.content)}
   </>
 )
 
