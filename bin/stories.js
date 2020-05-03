39e9b5f5716344695ca7c6811a851ece543a8860
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-03 13:02:05 +0200

    Add script to generate stories manifest

---
+++ bin/stories.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+const manifest = fs
+  .readdirSync(path.resolve('public/stories'))
+  .map(fileName => {
+    const story = require(path.resolve('public/stories/' + fileName))
+
+    return {
+      id: fileName.replace('.json', ''),
+      title: story.title,
+      author: story.author,
+      content: story.content.slice(0, 160) + '…',
+      category: story.category,
+      cardId: story.cardId,
+    }
+  })
+
+fs.writeFileSync(
+  path.resolve('public/stories.json'),
+  JSON.stringify(manifest, null, 2),
+  'utf8'
+)
+
+console.log(manifest)
