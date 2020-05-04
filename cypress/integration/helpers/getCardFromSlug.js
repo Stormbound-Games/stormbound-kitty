f40c5ae86b380d7677a44fd4147e6a2e3337adb3
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-04 09:27:59 +0200

    Extract ‘getCardFromSlug’ in a helper

---
+++ cypress/integration/helpers/getCardFromSlug.js
@@ -0,0 +1,19 @@
+import getCardFromSlug from '../../../src/helpers/getCardFromSlug'
+
+describe('The `getCardFromSlug` helper', () => {
+  it('should find a card from its ID', () => {
+    expect(getCardFromSlug('N1').name).to.equal('Green Prototypes')
+  })
+
+  it('should from its slugified name', () => {
+    expect(getCardFromSlug('green_prototypes').id).to.equal('N1')
+  })
+
+  it('should take commas into account', () => {
+    expect(getCardFromSlug('xuri_lord_of_life').id).to.equal('S19')
+  })
+
+  it('should take quotes into account', () => {
+    expect(getCardFromSlug('heralds_hymn').id).to.equal('S20')
+  })
+})
