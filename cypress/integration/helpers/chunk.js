727c918488ec598931415c0f04089d35ca6080de
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-05 17:22:47 +0200

    Make the ‘chunk’ helper more resilient

--- cypress/integration/helpers/chunk.js
+++ cypress/integration/helpers/chunk.js
@@ -1,6 +1,14 @@
 import chunk from '../../../src/helpers/chunk'
 
 describe('The `chunk` helper', () => {
+  it('should leave an empty array untouched', () => {
+    expect(chunk([], 2)).to.deep.equal([])
+  })
+
+  it('should leave the array untouched if the size is 1', () => {
+    expect(chunk([0, 1, 2, 3], 1)).to.deep.equal([0, 1, 2, 3])
+  })
+
   it('should split array in chunks', () => {
     expect(chunk([0, 1, 2, 3], 2)).to.deep.equal([
       [0, 1],
--- src/helpers/chunk.js
+++ src/helpers/chunk.js
@@ -1,5 +1,7 @@
-export default (array, size) => {
+export default (array = [], size = 1) => {
   const clone = [...array]
+
+  if (array.length === 0 || size === 1) return array
 
   return new Array(Math.ceil(clone.length / size))
     .fill()
