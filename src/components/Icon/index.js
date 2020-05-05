943c5ab71e8067a11f220c77b8670af917a0f9fe
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-05 13:34:03 +0200

    Make it possible to pass a class name to ‘Icon’

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -4,7 +4,9 @@ import './index.css'
 export default React.memo(function Icon(props) {
   return (
     <svg
-      className={`icon icon-${props.icon}`}
+      className={['icon', `icon-${props.icon}`, props.className]
+        .filter(Boolean)
+        .join(' ')}
       aria-hidden
       focusable='false'
       viewBox='0 0 32 32'
