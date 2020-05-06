1d4ee06db747f8075749fc93ecfae22ccd953200
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-06 10:36:08 +0200

    Add an explanatory comment

--- src/helpers/serialisation.deck.js
+++ src/helpers/serialisation.deck.js
@@ -16,6 +16,12 @@ const deserialiseDeck = hash => {
   try {
     const string = window.atob(hash)
 
+    // The card serialisation system operates within the base64 range, which
+    // means it is technically possible to base64 decode a deck string without
+    // an error. Therefore, we check if the base64 decoded string contains only
+    // numbers, faction indicators and comma (for old decks). If it doesn’t, it
+    // means the input was not actually a base64 hash but deck string to be
+    // deserialised.
     if (!/^[NSFWIT\d,]+$/.test(string)) {
       return serialisation.cards.deserialise(hash.toUpperCase())
     }
