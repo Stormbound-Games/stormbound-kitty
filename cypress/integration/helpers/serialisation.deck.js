99b4ebfa2b8bbf645e945f46cb469b9ec78e3871
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-06 09:29:32 +0200

    Remove base64 encoding from deck serialisation

--- cypress/integration/helpers/serialisation.deck.js
+++ cypress/integration/helpers/serialisation.deck.js
@@ -2,40 +2,32 @@ import serialisation from '../../../src/helpers/serialisation'
 
 describe('The `serialisation.deck.serialise` helper', () => {
   it('should serialise some cards', () => {
-    expect(serialisation.cards.serialise([{ level: 1, id: 'N1' }])).to.equal(
-      '1N1'
+    expect(serialisation.deck.serialise([{ level: 1, id: 'N1' }])).to.equal(
+      '1n1'
     )
   })
 
   it('should handle tokens with a forced padding', () => {
     expect(
-      serialisation.cards.serialise([
+      serialisation.deck.serialise([
         { level: 1, id: 'T1', token: true },
         { level: 10, id: 'T2', token: true },
       ])
-    ).to.equal('01T110T2')
+    ).to.equal('01t110t2')
   })
 
   it('should strip out empty cards', () => {
     expect(
-      serialisation.cards.serialise([{ level: 1, id: 'N1' }, null, {}])
-    ).to.equal('1N1')
-  })
-
-  it('should be a btoa wrapper around `serialisation.cards.serialise`', () => {
-    const cards = [{ level: 1, id: 'N1' }]
-
-    expect(serialisation.deck.serialise(cards)).to.equal(
-      btoa(serialisation.cards.serialise(cards))
-    )
+      serialisation.deck.serialise([{ level: 1, id: 'N1' }, null, {}])
+    ).to.equal('1n1')
   })
 })
 
 describe('The `serialisation.deck.deserialise` helper', () => {
   it('should handle decks serialised with the old system', () => {
     expect(
-      serialisation.cards.deserialise(
-        '5N1,5N2,5F2,5F3,5N3,5F5,5N12,5N16,5F14,5F15,5N30,5N57'
+      serialisation.deck.deserialise(
+        window.btoa('5N1,5N2,5F2,5F3,5N3,5F5,5N12,5N16,5F14,5F15,5N30,5N57')
       )
     ).to.deep.equal([
       { level: 5, id: 'N1' },
@@ -55,8 +47,8 @@ describe('The `serialisation.deck.deserialise` helper', () => {
 
   it('should deserialise a deck', () => {
     expect(
-      serialisation.cards.deserialise(
-        '5N15N25F25F35N35F55N125N165F145F155N305N57'
+      serialisation.deck.deserialise(
+        '5n15n25f25f35n35f55n125n165f145f155n305n57'
       )
     ).to.deep.equal([
       { level: 5, id: 'N1' },
@@ -75,7 +67,7 @@ describe('The `serialisation.deck.deserialise` helper', () => {
   })
 
   it('should deserialise a series of cards smaller than 12', () => {
-    expect(serialisation.cards.deserialise('5N15N25F25F3')).to.deep.equal([
+    expect(serialisation.deck.deserialise('5n15n25f25f3')).to.deep.equal([
       { level: 5, id: 'N1' },
       { level: 5, id: 'N2' },
       { level: 5, id: 'F2' },
@@ -84,19 +76,11 @@ describe('The `serialisation.deck.deserialise` helper', () => {
   })
 
   it('should handle tokens', () => {
-    expect(serialisation.cards.deserialise('01T105T210T399T4')).to.deep.equal([
+    expect(serialisation.deck.deserialise('01t105t210t399t4')).to.deep.equal([
       { level: 1, id: 'T1' },
       { level: 5, id: 'T2' },
       { level: 10, id: 'T3' },
       { level: 99, id: 'T4' },
     ])
-  })
-
-  it('should be a atob wrapper around `serialisation.cards.deserialise`', () => {
-    const cards = '01T105T210T399T4'
-
-    expect(serialisation.deck.deserialise(btoa(cards))).to.deep.equal(
-      serialisation.cards.deserialise(cards)
-    )
   })
 })
--- src/helpers/serialisation.deck.js
+++ src/helpers/serialisation.deck.js
@@ -1,6 +1,33 @@
 import serialisation from './serialisation'
 
+const serialiseDeck = cards => {
+  // Do not en/decode to base64 as the hash ends up being longer than the
+  // original data. While it might make sense for sims or cards, it is
+  // unnecessary for decks which are highly compressible to begin with.
+  return serialisation.cards.serialise(cards).toLowerCase()
+}
+
+/**
+ * Deserialise a deck into an array of cards
+ * @param {String} hash - Either base64 hash (old) or card string (new)
+ * @return {Object[]} cards
+ */
+const deserialiseDeck = hash => {
+  try {
+    const string = window.atob(hash)
+
+    if (!/^[NSFWIT\d,]+$/.test(string)) {
+      return serialisation.cards.deserialise(hash.toUpperCase())
+    }
+
+    // Maintain backward compability with decks serialised in base64.
+    return serialisation.cards.deserialise(string)
+  } catch (error) {
+    return serialisation.cards.deserialise(hash.toUpperCase())
+  }
+}
+
 export default {
-  serialise: cards => window.btoa(serialisation.cards.serialise(cards)),
-  deserialise: hash => serialisation.cards.deserialise(window.atob(hash)),
+  serialise: serialiseDeck,
+  deserialise: deserialiseDeck,
 }
