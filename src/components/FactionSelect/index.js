1d03b79ff10e960e848dd68e63487132a4707db5
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-08 17:33:29 +0200

    Make ‘FactionSelect’ a bit more flexible

--- src/components/FactionSelect/index.js
+++ src/components/FactionSelect/index.js
@@ -5,8 +5,11 @@ import capitalise from '../../helpers/capitalise'
 export default React.memo(function FactionSelect(props) {
   return (
     <>
-      <label htmlFor={props.id || 'faction'}>Faction</label>
+      <label htmlFor={props.id || 'faction'} className={props.labelClassName}>
+        {props.label || 'Faction'}
+      </label>
       <select
+        form={props.form}
         name={props.name || 'faction'}
         id={props.id || 'faction'}
         required={props.required}
@@ -15,6 +18,7 @@ export default React.memo(function FactionSelect(props) {
         data-testid={props['data-testid']}
       >
         {props.withAny && <option value='*'>Any</option>}
+        {props.withEmpty && <option value=''>Pick a faction</option>}
         {Object.keys(FACTIONS)
           .filter(faction => props.withNeutral || faction !== 'neutral')
           .map(faction => (
