fd4ec0a1f5f7c8e32f4497e3e97fb49674b6bc01
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-14 09:02:38 +0200

    Hide recommended decks section if no decks

--- src/components/BrawlRecommendedDecks/index.js
+++ src/components/BrawlRecommendedDecks/index.js
@@ -6,15 +6,16 @@ import Title from '../Title'
 
 export default React.memo(function BrawlRecommendedDecks(props) {
   const { id } = React.useContext(BrawlContext)
+  const brawlDecks = decks
+    .filter(deck => deck.brawl === id)
+    .slice(0, props.limit)
+
+  if (brawlDecks.length === 0) return null
 
   return (
     <>
       <Title>Recommended deck{props.limit === 1 ? '' : 's'}</Title>
-      <Decks
-        showUpgrades
-        columns={props.columns}
-        decks={decks.filter(deck => deck.brawl === id).slice(0, props.limit)}
-      />
+      <Decks showUpgrades columns={props.columns} decks={brawlDecks} />
     </>
   )
 })
