3ce045e12f8c77d8b7ae11ab0e5d3fd9d775e081
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-29 16:58:09 +0200

    Take Gift of the Wise and Rimelings into account for the deck chart

--- src/components/DeckStatsChart/index.js
+++ src/components/DeckStatsChart/index.js
@@ -40,44 +40,64 @@ const getSequences = () => {
 
 const SEQUENCES = getSequences()
 
-const getManaSequences = ([A, B, C, D]) => [
-  A,
-  A + B,
-  A + C,
-  A + D,
-  A + B + C,
-  A + B + D,
-  A + C + D,
-  A + B + C + D,
-  B,
-  B + C,
-  B + D,
-  B + C + D,
-  C,
-  C + D,
-  D,
-]
+const getEffectiveManaCost = mana => card => {
+  // If the card is GotW and it can be played, reduced its mana cost by the
+  // amount of mana given by its ability.
+  if (card.id === 'W19' && mana >= 7)
+    return 7 - ([9, 10, 11, 12, 13][card.level] - 7)
+
+  // If the card is Rimelings and it can be played, reduced its mana cost by
+  // the amount of mana given by its ability.
+  if (card.id === 'W12' && mana >= 5) return 2
+
+  return card.mana
+}
+
+const getManaSequences = mana => cards => {
+  const [A, B, C, D] = cards.map(getEffectiveManaCost(mana))
+
+  return [
+    A,
+    A + B,
+    A + C,
+    A + D,
+    A + B + C,
+    A + B + D,
+    A + C + D,
+    A + B + C + D,
+    B,
+    B + C,
+    B + D,
+    B + C + D,
+    C,
+    C + D,
+    D,
+  ]
+}
 
 const sum = sequence => sequence.reduce((acc, value) => acc + value, 0)
 
 const computeDeckChances = (deck, mana) => {
+  const getCardAtIndex = index => deck[index]
+
   // Compute all the possible unique hand sequences that can happen for the
   // given deck, and resolve them to host mana instead of card index.
-  const sequences = SEQUENCES.map(sequence =>
-    sequence.map(index => deck[index].mana)
-  )
   // Find all the hand sequences using all the available mana.
-  const sequencesUsingAllMana = sequences
-    .map(getManaSequences)
+  const sequencesUsingAllMana = SEQUENCES.map(sequence =>
+    sequence.map(getCardAtIndex)
+  )
+    .map(getManaSequences(mana))
     .filter(sequence => sequence.includes(mana))
   // Find all the hand sequences being fully playable within the available mana.
-  const sequencesUsingAllCards = sequences
+  const sequencesUsingAllCards = SEQUENCES.map(sequence =>
+    sequence.map(getCardAtIndex).map(getEffectiveManaCost(mana))
+  )
     .map(sum)
     .filter(manaCost => manaCost <= mana)
 
   return {
-    usingAllMana: (sequencesUsingAllMana.length / sequences.length) * 100,
-    playingAllCards: (sequencesUsingAllCards.length / sequences.length) * 100,
+    usingAllMana: (sequencesUsingAllMana.length / SEQUENCES.length) * 100,
+    playingAllCards: (sequencesUsingAllCards.length / SEQUENCES.length) * 100,
   }
 }
 
