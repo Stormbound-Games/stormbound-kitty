61c667cd2acdd5e002774cfc098367003e077430
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-05-30 12:46:00 +0200

    Add a bookodds command to the bot

---
+++ src/bot/commands/bookodds/index.js
@@ -0,0 +1,97 @@
+import { RARITIES, BOOKS, PRE_MADE_EXPECTATIONS } from '../../../constants/game'
+import capitalise from '../../../helpers/capitalise'
+import getDrawingProbability from '../../../helpers/getDrawingProbability'
+import getCardsForSearch from '../../../helpers/getCardsForSearch'
+
+const getRarityOdds = book => rarity => {
+  const anyKey = 'ANY_' + rarity.toUpperCase()
+  const specificKey = 'SPECIFIC_' + rarity.toUpperCase()
+  const anyLabel = PRE_MADE_EXPECTATIONS[anyKey].label
+  const anyOdds = getDrawingProbability(
+    book,
+    PRE_MADE_EXPECTATIONS[anyKey].expectations
+  ).toFixed(2)
+  const specificLabel = PRE_MADE_EXPECTATIONS[specificKey].label
+  const specificOdds = getDrawingProbability(
+    book,
+    PRE_MADE_EXPECTATIONS[specificKey].expectations
+  ).toFixed(2)
+
+  return `${anyOdds}% to draw **${anyLabel}**, ${specificOdds}% to draw **${specificLabel}**`
+}
+
+const parseSearch = search => {
+  let book = null
+  let target = null
+
+  search.split(/\s+/g).forEach(term => {
+    if (Object.keys(BOOKS).includes(term.toUpperCase())) {
+      book = term.toUpperCase()
+    } else if (Object.keys(RARITIES).includes(term.toLowerCase())) {
+      target = term.toUpperCase()
+    } else if (term.toLowerCase() === 'fs') {
+      target = 'FUSION_STONES'
+    } else {
+      const [card] = getCardsForSearch(term)
+      target = card
+    }
+  })
+  return { book, target }
+}
+
+export default {
+  command: 'bookodds',
+  name: 'Book Drawing Odds',
+  description:
+    'Get the odds of a drawing a certain card of Fusion stones from a certain book',
+  example: 'mythic rof',
+  icon: '📕',
+  handler: function (search) {
+    const { book, target } = parseSearch(search)
+
+    // The book argument should be mandatory and there is no way to compute
+    // anything if it’s not provided.
+    if (!book) return
+
+    const bookName = capitalise(book.toLowerCase())
+    const intro = `A **${bookName} book** has:`
+    const fsOdds = getDrawingProbability(
+      book,
+      PRE_MADE_EXPECTATIONS.FUSION_STONES.expectations
+    )
+    const fsLine = `- ${fsOdds.toFixed(2)}% chances to draw **Fusion stones**`
+
+    if (target === 'FUSION_STONES') {
+      return `${intro.slice(0, -1)}${fsLine.slice(1)}.`
+    }
+
+    // If no specific target is provided, return all the odds for the given book
+    // starting with fusion stones, and then going through rarities.
+    if (!target) {
+      return (
+        [
+          intro,
+          fsLine,
+          ...Object.keys(RARITIES)
+            .map(getRarityOdds(book))
+            .map(line => '- ' + line),
+        ].join('\n') + '.'
+      )
+    }
+
+    // If the target happes to be a card, compute the odds to draw a specific
+    // card of the card’s rarity, and provide a custom answer.
+    if (target.id) {
+      const odds = getDrawingProbability(
+        book,
+        PRE_MADE_EXPECTATIONS['SPECIFIC_' + target.rarity.toUpperCase()]
+          .expectations
+      )
+      return `${intro.slice(0, -1)} ${odds.toFixed(2)}% chances to draw **${
+        target.name
+      }**.`
+    }
+
+    return `${intro.slice(0, -1)} ${getRarityOdds(book)(target)}.`
+  },
+}
---
+++ src/bot/commands/bookodds/spec.js
@@ -0,0 +1,48 @@
+import command from './'
+const bookodds = command.handler
+
+describe('Bot — !bookodds', () => {
+  it('should return nothing for a missing term', () => {
+    expect(bookodds('')).to.equal(undefined)
+    expect(bookodds('  ')).to.equal(undefined)
+  })
+
+  it('should return nothing for a missing book', () => {
+    expect(bookodds('rare')).to.equal(undefined)
+    expect(bookodds('rof')).to.equal(undefined)
+  })
+
+  it('should return odds for fusion stones', () => {
+    expect(bookodds('mythic fs')).to.contain('Fusion stones')
+    expect(bookodds('classic fs')).to.contain('Fusion stones')
+    expect(bookodds('heroic fs')).to.contain('Fusion stones')
+    expect(bookodds('noble fs')).to.contain('Fusion stones')
+    expect(bookodds('humble fs')).to.contain('Fusion stones')
+  })
+
+  it('should return odds for a rarity', () => {
+    expect(bookodds('mythic common')).to.contain('any common card')
+    expect(bookodds('mythic common')).to.contain('specific common card')
+    expect(bookodds('mythic rare')).to.contain('any rare card')
+    expect(bookodds('mythic rare')).to.contain('specific rare card')
+    expect(bookodds('mythic epic')).to.contain('any epic card')
+    expect(bookodds('mythic epic')).to.contain('specific epic card')
+    expect(bookodds('mythic legendary')).to.contain('any legendary card')
+    expect(bookodds('mythic legendary')).to.contain('specific legendary card')
+  })
+
+  it('should return odds for a specific card', () => {
+    expect(bookodds('mythic N1')).to.contain('Green Prototypes')
+    expect(bookodds('mythic gifted')).to.contain('Gifted Recruits')
+    expect(bookodds('mythic rof')).to.contain('Rain of Frogs')
+    expect(bookodds('mythic QoH')).to.contain('Queen of Herds')
+  })
+
+  it('should ignore casing', () => {
+    expect(bookodds('MytHiC fS')).to.contain('Fusion stones')
+  })
+
+  it('should ignore order', () => {
+    expect(bookodds('fs classic')).to.contain('Fusion stones')
+  })
+})
