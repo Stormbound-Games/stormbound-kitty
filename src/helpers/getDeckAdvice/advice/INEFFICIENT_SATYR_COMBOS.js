f07102b9fb7c054007f18e51a02d0ce4e7c94a41
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-01 10:28:39 +0200

    fixed hasAny function

--- src/helpers/getDeckAdvice/advice/INEFFICIENT_SATYR_COMBOS.js
+++ src/helpers/getDeckAdvice/advice/INEFFICIENT_SATYR_COMBOS.js
@@ -4,14 +4,14 @@ import getRawCardData from '../../getRawCardData/'
 const getSatyrs = cards => cards.filter(card => card.race === 'satyr')
 
 export default cards => {
-  const hasSatyrSynergist = hasAny(cards, ['S5', 'S7', 'S9'])
+  const satyrConsumers = ['S5', 'S7', 'S9']
   const satyrs = getSatyrs(cards)
+  const hasCard = id => cards.map(card => card.id).includes(id)
+  const hasAny = ids => ids.some(hasCard)
+  const hasSatyrSynergist = hasAny(satyrConsumers)
 
   if (!hasSatyrSynergist || satyrs.length > 5) return null
 
-  const hasCard = id => cards.map(card => card.id).includes(id)
-  const hasAny = ids => ids.some(hasCard)
-  const satyrConsumers = ['S5', 'S7', 'S9']
   const listOfNames = toSentence(
     satyrConsumers
       .filter(hasCard)
