b503b0253e0849303e9585ee24c293bd988b56de
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-04 12:56:57 +0200

    updated inefficient satyr combo deck advice to include head start

--- src/helpers/getDeckAdvice/advice/INEFFICIENT_SATYR_COMBOS.js
+++ src/helpers/getDeckAdvice/advice/INEFFICIENT_SATYR_COMBOS.js
@@ -8,9 +8,11 @@ export default cards => {
   const satyrs = getSatyrs(cards)
   const hasCard = id => cards.map(card => card.id).includes(id)
   const hasAny = ids => ids.some(hasCard)
+  const hasHeadStart = hasCard('S24')
+  const numSatyrs = hasHeadStart ? satyrs.length + 1 : satyrs.length
   const hasSatyrSynergist = hasAny(satyrConsumers)
 
-  if (!hasSatyrSynergist || satyrs.length > 5) return null
+  if (!hasSatyrSynergist || numSatyrs > 4) return null
 
   const listOfNames = toSentence(
     satyrConsumers
@@ -23,7 +25,7 @@ export default cards => {
   return {
     id: 'INEFFICIENT_SATYR_COMBOS',
     name: 'Inefficient Satyr Combos',
-    description: `This deck includes ${listOfNames}, but doesn’t include enough satyrs to provide good synergy. Consider including more satyrs.`,
-    highlight: ['S5', 'S7', 'S9', ...satyrs],
+    description: `This deck includes ${listOfNames}, but doesn’t include enough satyrs to provide good synergy. Consider including more satyrs or Head Start.`,
+    highlight: ['S5', 'S7', 'S9', 'S24', ...satyrs],
   }
 }
