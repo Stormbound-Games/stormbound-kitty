05db7ba737c081a2538d7f84a8088b7c752f3926
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-06-05 12:38:38 +0200

    Provide a way to change Brawl modifier in deck details

--- src/components/DeckDetailView/index.js
+++ src/components/DeckDetailView/index.js
@@ -9,10 +9,35 @@ import Row from '../Row'
 import ShareButton from '../DeckShareButton'
 import Stats from '../DeckStats'
 import Title from '../Title'
+import { NotificationContext } from '../NotificationProvider'
 import getDeckBuilderMetaTags from '../../helpers/getDeckBuilderMetaTags'
+import modifyDeck from '../../helpers/modifyDeck'
+import getDeckPresets from '../../helpers/getDeckPresets'
+import { BRAWLS } from '../../constants/brawl'
+
+const getDefaultBrawlModifier = deck => {
+  const presets = getDeckPresets(deck)
+
+  return presets.modifier.includes('MANA') ? presets.modifier : 'NONE'
+}
 
 export default React.memo(function DeckDetailView(props) {
+  const { notify } = React.useContext(NotificationContext)
   const history = useHistory()
+  const defaultModifier = getDefaultBrawlModifier(props.deck)
+  const [modifier, setModifier] = React.useState(defaultModifier)
+  const deck = modifyDeck(props.deck, modifier)
+  const sendNotification = React.useCallback(
+    message => notify({ icon: 'stack', children: message }),
+    [notify]
+  )
+
+  React.useEffect(() => {
+    if (defaultModifier !== 'NONE') {
+      const brawlLabel = BRAWLS.find(({ id }) => id === defaultModifier).label
+      sendNotification(`Brawl deck found. Loaded with modifier ${brawlLabel}.`)
+    }
+  }, [defaultModifier, sendNotification])
 
   return (
     <>
@@ -23,7 +48,7 @@ export default React.memo(function DeckDetailView(props) {
           <Title>Your deck</Title>
           <Deck
             id='deck'
-            deck={props.deck}
+            deck={deck}
             highlightedCards={props.highlightedCards}
             onClick={card => history.push('/card/' + card.id + '/display')}
             onClickLabel='Open card in card builder'
@@ -37,12 +62,16 @@ export default React.memo(function DeckDetailView(props) {
         </Column>
 
         <Column width='1/3'>
-          <Stats deck={props.deck} highlight={props.highlight} />
-          <DeckStatsChart deck={props.deck} />
+          <Stats deck={deck} highlight={props.highlight} />
+          <DeckStatsChart
+            deck={deck}
+            modifier={modifier}
+            setModifier={setModifier}
+          />
         </Column>
 
         <Column width='1/3'>
-          <Advice deck={props.deck} highlight={props.highlight} />
+          <Advice deck={deck} highlight={props.highlight} />
         </Column>
       </Row>
 
--- src/components/DeckStatsChart/index.js
+++ src/components/DeckStatsChart/index.js
@@ -8,15 +8,18 @@ import {
   XAxis,
   YAxis,
 } from 'recharts'
-import getResolvedCardData from '../../helpers/getResolvedCardData'
 import computeDeckChances from '../../helpers/computeDeckChances'
 import { TOOLTIP_STYLES } from '../../constants/stats'
+import { BRAWLS } from '../../constants/brawl'
+import './index.css'
 
 export default React.memo(function DeckStatsChart(props) {
-  const deck = props.deck.map(getResolvedCardData)
   const data = Array.from({ length: 15 }, (_, index) => {
     const mana = index + 3
-    const { usingAllMana, playingAllCards } = computeDeckChances(deck, mana)
+    const { usingAllMana, playingAllCards } = computeDeckChances(
+      props.deck,
+      mana
+    )
 
     return {
       mana,
@@ -26,28 +29,51 @@ export default React.memo(function DeckStatsChart(props) {
   })
 
   return (
-    <ResponsiveContainer width='100%' height={300}>
-      <LineChart
-        data={data}
-        margin={{ top: 5, right: 0, left: -28, bottom: 5 }}
-      >
-        <XAxis dataKey='mana' />
-        <YAxis />
-        <Tooltip {...TOOLTIP_STYLES} labelFormatter={mana => 'Mana ' + mana} />
-        <Legend align='left' wrapperStyle={{ paddingLeft: '40px' }} />
-        <Line
-          name='Chances to spend all mana'
-          type='monotone'
-          dataKey='usingAllMana'
-          stroke='#8884d8'
-        />
-        <Line
-          name='Chances to play all cards'
-          type='monotone'
-          dataKey='playingAllCards'
-          stroke='#82ca9d'
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <>
+      <ResponsiveContainer width='100%' height={300}>
+        <LineChart
+          data={data}
+          margin={{ top: 5, right: 0, left: -28, bottom: 5 }}
+        >
+          <XAxis dataKey='mana' />
+          <YAxis />
+          <Tooltip
+            {...TOOLTIP_STYLES}
+            labelFormatter={mana => 'Mana ' + mana}
+          />
+          <Legend align='left' wrapperStyle={{ paddingLeft: '40px' }} />
+          <Line
+            name='Chances to spend all mana'
+            type='monotone'
+            dataKey='usingAllMana'
+            stroke='#8884d8'
+          />
+          <Line
+            name='Chances to play all cards'
+            type='monotone'
+            dataKey='playingAllCards'
+            stroke='#82ca9d'
+          />
+        </LineChart>
+      </ResponsiveContainer>
+
+      <div className='DeckStatsChart__select'>
+        <label htmlFor='brawl-modifier'>Brawl modifier</label>
+        <select
+          name='brawl-modifier'
+          id='brawl-modifier'
+          data-testid='brawl-modifier'
+          value={props.modifier}
+          onChange={event => props.setModifier(event.target.value)}
+        >
+          <option value='NONE'>None</option>
+          {BRAWLS.filter(brawl => brawl.id.includes('MANA')).map(brawl => (
+            <option key={brawl.id} value={brawl.id}>
+              {brawl.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
   )
 })
