c5265afb37156db8d34331116bb406ff9a6802c5
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-06-06 09:15:22 +0200

    Display a message when no suggestions could be found

--- src/components/DeckAdvice/index.js
+++ src/components/DeckAdvice/index.js
@@ -8,31 +8,45 @@ export default function DeckAdvice(props) {
   const cards = props.deck.map(getResolvedCardData)
   const suggestions = getDeckAdvice(cards, props.modifier)
 
-  if (suggestions.length === 0) {
-    return null
-  }
-
   return (
     <div className='DeckAdvice'>
       <Title>
         Suggestions <LearnMoreIcon anchor='#incorrect-deck-suggestions' />
       </Title>
 
-      {suggestions.map(suggestion => (
-        <p
-          key={suggestion.name}
-          onMouseOver={
-            suggestion.highlight
-              ? () => props.highlight(suggestion.highlight)
-              : undefined
-          }
-          onMouseOut={() => props.highlight([])}
-          id={suggestion.id}
-        >
-          <strong className='Highlight'>{suggestion.name}:</strong>{' '}
-          {suggestion.description}
-        </p>
-      ))}
+      {suggestions.length > 0 ? (
+        suggestions.map(suggestion => (
+          <p
+            key={suggestion.name}
+            onMouseOver={
+              suggestion.highlight
+                ? () => props.highlight(suggestion.highlight)
+                : undefined
+            }
+            onMouseOut={() => props.highlight([])}
+            id={suggestion.id}
+          >
+            <strong className='Highlight'>{suggestion.name}:</strong>{' '}
+            {suggestion.description}
+          </p>
+        ))
+      ) : (
+        <>
+          <p>
+            No particular suggestions could be found for that deck. It likely
+            means this is a solid and well balanced deck, so kudos and enjoy
+            playing it!{'  '}
+            <span role='img' aria-label='Party'>
+              🎉
+            </span>
+          </p>
+          <p>
+            If you think something should be said about it however, please get
+            in touch with <span className='Highlight'>Kitty#1909</span> on
+            Discord to discuss potential improvements.
+          </p>
+        </>
+      )}
     </div>
   )
 }
