1f6b54d717a8388442e38174e30abf9a5330f32a
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-06-06 09:39:03 +0200

    Adjust the Aeryn check based on her level

--- src/helpers/getDeckAdvice/advice/INEFFICIENT_EARYN.js
+++ src/helpers/getDeckAdvice/advice/INEFFICIENT_EARYN.js
@@ -1,15 +1,16 @@
 const getSpells = cards => cards.filter(c => c.type === 'spell')
 
 export default cards => {
-  const hasArchdruidEaryn = cards.map(card => card.id).includes('N48')
+  const archdruidEaryn = cards.find(card => card.id === 'N48')
   const spells = getSpells(cards)
+  const threshold = archdruidEaryn && archdruidEaryn.level >= 4 ? 4 : 3
 
-  if (!hasArchdruidEaryn || spells.length >= 2) return null
+  if (!archdruidEaryn || spells.length >= threshold) return null
 
   return {
     id: 'INEFFICIENT_EARYN',
     name: 'Undervalued Archdruid Earyn',
-    description: `This deck includes Archdruid Earyn but has only ${spells.length} spell, which is unusually low. Consider adding an extra spell to get the most out of Archdruid Earyn.`,
+    description: `This deck includes Archdruid Earyn (level ${archdruidEaryn.level}) but has only ${spells.length} spell, which is unusually low. Consider adding an extra spell to get the most out of Archdruid Earyn.`,
     highlight: ['N48', ...spells],
   }
 }
