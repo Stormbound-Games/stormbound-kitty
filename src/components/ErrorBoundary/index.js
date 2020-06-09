c1612d466dbfe41bf9c033cfa4cd4de9a5a501a6
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-06-09 09:58:08 +0200

    Try solving the chunk loading error differently

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -11,43 +11,18 @@ export default class ErrorBoundary extends React.Component {
     return { hasError: true, error: error.message }
   }
 
-  handleError = event => {
-    // The `ChunkLoadError` happens when navigating during a deployment. The
-    // current chunks have been wiped from the `build` folder and replaced with
-    // new ones, yielding a loading error. Only a reload can fix it.
-    if (
-      event.error.name === 'ChunkLoadError' ||
-      event.error.message.toLowerCase().includes('loading chunk')
-    ) {
-      this.setState({ hasError: true, error: 'ChunkLoadError' })
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('error', this.handleError)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('error', this.handleError)
-  }
-
   componentDidCatch(error, info) {
     // eslint-disable-next-line
     console.error({ error, info })
+
+    if (/Loading chunk [\d]+ failed/i.test(error.message)) {
+      window.location.reload()
+    }
   }
 
   render() {
     if (this.state.hasError) {
-      return (
-        <Error
-          error={this.state.error}
-          retry={
-            this.state.error === 'ChunkLoadError'
-              ? () => window.location.reload()
-              : undefined
-          }
-        />
-      )
+      return <Error error={this.state.error} />
     }
 
     return this.props.children
