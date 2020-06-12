e8de1ee90e8d6e816adae07eb95dda65d9e22773
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-12 11:44:40 +0200

    cleaned up code for Queen of Herds deck advice

--- src/helpers/getDeckAdvice/advice/QUEEN_OF_HERDS/index.js
+++ src/helpers/getDeckAdvice/advice/QUEEN_OF_HERDS/index.js
@@ -1,21 +1,14 @@
-import getRawCardData from '../../../getRawCardData'
 const getSatyrs = cards => cards.filter(card => card.race === 'satyr')
 
 export default cards => {
-  const hasCard = id => cards.map(card => card.id).includes(id)
-  const hasQueenOfHerds = hasCard('S21')
-  const queenOfHerdsLvl = hasQueenOfHerds
-    ? cards.find(card => card.id === 'S21').level
-    : 1
+  const queenOfHerds = cards.find(card => card.id === 'S21')
   const satyrs = getSatyrs(cards)
-  const SATYR_THRESHOLD = queenOfHerdsLvl > 3 ? 2 : 1
 
-  // If the deck has Queen of Herds, but not enough satyrs to
-  // be spawned, it is considered inefficient.
-  if (!hasQueenOfHerds || satyrs.length >= SATYR_THRESHOLD) return null
+  if (!queenOfHerds || satyrs.length >= (queenOfHerds.level >= 4 ? 2 : 1))
+    return null
 
   return {
-    name: 'Undervalued Queen of Herds',
+    name: 'Inefficient Queen of Herds',
     description: `This deck includes Queen of Herds, but doesn’t include enough satyrs to provide good synergy. Consider including more satyrs.`,
     highlight: ['S21', ...satyrs],
   }
