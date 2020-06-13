b6769653be183d8da0ba77611fdf27ee68bb3d01
Simon Castagna <simoncastagna@Simons-MacBook-Pro.local>
2020-06-13 23:28:18 +0200

    fixed kindred's grace deck advice to account for spawn cards

--- src/helpers/getDeckAdvice/advice/KINDREDS_GRACE/spec.js
+++ src/helpers/getDeckAdvice/advice/KINDREDS_GRACE/spec.js
@@ -1,7 +1,19 @@
 import advice from './'
 import getResolvedCardData from '../../../getResolvedCardData'
+import getRawCardData from '../../../getRawCardData'
 import serialisation from '../../../serialisation'
 import modifyDeck from '../../../modifyDeck'
+import { SPAWNS } from './index'
+
+const SPAWNS_ARRAY = Object.entries(SPAWNS)
+const BASE_DECKS = {
+  dragon: '4n14n34n43n63n103f273n173n243n254n283n40',
+  knight: '4n14n34n43n63n103f273n173n243n254n283n40',
+  satyr: '4n14n34n43n63n103f273n173n243n254n283n40',
+  construct: '4n14n34n43n63n103f273n173n243n254n283n40',
+  raven: '4n14n34n43n63n103f273n173n243n254n283n40',
+  toad: '4n14n34n43n63n103n243n254n283n404n523n53',
+}
 
 const getCards = (id, modifier = 'NONE') =>
   modifyDeck(serialisation.deck.deserialise(id), modifier).map(
@@ -14,14 +26,19 @@ describe('The `KINDREDS_GRACE` advice', () => {
     expect(advice(cards)).to.not.equal(null)
   })
 
-  it('should not be returned if the deck contains Azure Hatchers', () => {
-    const cards = getCards('1n11n21n31n41n51n621n631n81n111n131f101n40')
-    expect(advice(cards)).to.equal(null)
-  })
+  SPAWNS_ARRAY.forEach(spawnCard => {
+    const cardId = spawnCard[0]
+    const race = spawnCard[1][0]
+    const testDeck = BASE_DECKS[race] + '1' + cardId.toLowerCase()
 
-  it('should not be returned if the deck contains Rain of Frogs', () => {
-    const cards = getCards('1n11n21n31n41n51n621n631n81n111n131f81n40')
-    expect(advice(cards)).to.equal(null)
+    it(
+      'should not be returned if the deck contains ' +
+        getRawCardData(cardId).name,
+      () => {
+        const cards = getCards(testDeck)
+        expect(advice(cards)).to.equal(null)
+      }
+    )
   })
 
   it('should not be returned if the deck enough units of the same race represented', () => {
