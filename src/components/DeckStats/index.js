22bdc7b6f956e8c36f04b535f247cce1600e703a
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-06-24 08:17:48 +0200

    Fix Wild Saberpaws not counting as moving

--- src/components/DeckStats/index.js
+++ src/components/DeckStats/index.js
@@ -7,12 +7,23 @@ import canCardBePlayed from '../../helpers/canCardBePlayed'
 import canDeployUnits from '../../helpers/canDeployUnits'
 import './index.css'
 
-const getEffectiveSpeed = card =>
-  // Dreadfauns has an average speed of about 0.5. Out of the 16 slots it
-  // can be played, 2 of them always move the line (corners), 8 of them have
-  // 2 chances out of 3 to move the line (edges), and the 6 remaining ones
-  // have 1 chance out of 2 to move the line (middle).
-  card.id === 'S16' ? 0.625 : card.movement || 0
+const getEffectiveSpeed = card => {
+  switch (card.id) {
+    case 'S16':
+      // Dreadfauns has an average speed of about 0.5. Out of the 16 slots it
+      // can be played, 2 of them always move the line (corners), 8 of them have
+      // 2 chances out of 3 to move the line (edges), and the 6 remaining ones
+      // have 1 chance out of 2 to move the line (middle).
+      return 0.625
+    case 'N67':
+      // Wild Saberpaws can have 0, 1 or 2 speed based on whether they are
+      // played with bordering or surrounding units, so we consider an average
+      // speed of 1.
+      return 1
+    default:
+      return card.movement || 0
+  }
+}
 const sum = (a, b) => a + b
 const getAverageManaCost = cards =>
   (cards.map(c => c.mana).reduce(sum, 0) / cards.length).toFixed(2)
