5242c5b4e4b0f8a1aac007e519bb71742acb6a84
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-10 13:56:12 +0200

    Add route test for update page

--- cypress/integration/routes/changelog.js
+++ cypress/integration/routes/changelog.js
@@ -2,4 +2,8 @@ describe('Routes — Changelog', () => {
   it('it should render', () => {
     cy.visit('/changelog').get('main h1').should('exist')
   })
+
+  it('it should render July 2020 update', () => {
+    cy.visit('/changelog/07-2020').get('main h1').should('exist')
+  })
 })
