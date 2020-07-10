19f2e350f7b56fb3cda730f9189256f660f0abe4
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-10 08:05:42 +0200

    Remove unnecessary code from Tooltip

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -6,11 +6,6 @@ const OFFSET = 8
 
 // Center the tooltip, but collisions will win
 const centered = (triggerRect, tooltipRect) => {
-  const styles = {
-    left: `${triggerRect.left + window.scrollX}px`,
-    top: `${triggerRect.top + triggerRect.height + window.scrollY}px`,
-  }
-
   const collisions = {
     top: triggerRect.top - tooltipRect.height < 0,
     right: window.innerWidth < triggerRect.left + tooltipRect.width,
@@ -25,7 +20,6 @@ const centered = (triggerRect, tooltipRect) => {
   const maxLeft = window.innerWidth - tooltipRect.width - 2
 
   return {
-    ...styles,
     left: Math.min(Math.max(2, left), maxLeft) + window.scrollX,
     top: directionUp
       ? `${triggerRect.top - OFFSET - tooltipRect.height + window.scrollY}px`
