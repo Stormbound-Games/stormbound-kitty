a7c982bf28c896dd7cf8e45ab99deff1dff69b82
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-15 10:37:36 +0200

    Migrate ‘story’ command to embeds

--- src/bot/commands/story/index.js
+++ src/bot/commands/story/index.js
@@ -1,20 +1,45 @@
+import Discord from 'discord.js'
 import getStoriesForSearch from '../../../helpers/getStoriesForSearch'
+import getRawCardData from '../../../helpers/getRawCardData'
 import arrayRandom from '../../../helpers/arrayRandom'
 import stories from '../../../../public/stories'
+
+const getEmbedForStory = story => {
+  const embed = new Discord.MessageEmbed()
+
+  embed
+    .setColor('#D7598B')
+    .setTitle(story.title)
+    .setURL('https://stormbound-kitty.com/stories/' + story.id)
+    .addFields(
+      { name: 'Author', value: story.author, inline: true },
+      { name: 'Card', value: getRawCardData(story.cardId).name, inline: true }
+    )
+    .setDescription(story.content.replace('\n', ' '))
+
+  return embed
+}
 
 export default {
   command: 'story',
   help: function () {
-    return `📝  **Story Search:** Link a random story published on Stormbound-Kitty. It optionally accepts a card abbreviation, a Stormbound-Kitty ID, or otherwise performs a “fuzzy search” on the card name to find an associated story. For instance, \`!${this.command} mia\`.`
+    const embed = new Discord.MessageEmbed()
+
+    embed
+      .setColor('#D7598B')
+      .setTitle(`📝  Story help`)
+      .setURL('https://stormbound-kitty.com/stories')
+      .setDescription(
+        `Link a random story published on Stormbound-Kitty. It optionally accepts a card abbreviation, a Stormbound-Kitty ID, or otherwise performs a “fuzzy search” on the card name to find an associated story. For instance, \`!${this.command} mia\`.`
+      )
+
+    return embed
   },
   handler: function (message) {
     if (message === 'random' || message === '') {
-      return 'https://stormbound-kitty.com/stories/' + arrayRandom(stories).id
+      return getEmbedForStory(arrayRandom(stories))
     }
 
-    return getStoriesForSearch(message)
-      .slice(0, 2)
-      .map(story => 'https://stormbound-kitty.com/stories/' + story.id)
-      .join('\n')
+    return getEmbedForStory(getStoriesForSearch(message)[0])
   },
 }
