cc8c6d860f0bf2f7b2c2ff81c0a11e26a38794ef
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-15 09:56:45 +0200

    Migrate ‘donate’ command to embeds

--- src/bot/commands/donate/index.js
+++ src/bot/commands/donate/index.js
@@ -1,12 +1,29 @@
+import Discord from 'discord.js'
+
 const BASE_MESSAGE =
   'This Discord bot and <https://stormbound-kitty.com> are solely maintained by <@368097495605182483> and 100% free — no ads, no tracking, no paywall. If you enjoy them, please consider donating: <https://stormbound-kitty.com/donate>'
+
+const getEmbed = () => {
+  const embed = new Discord.MessageEmbed()
+
+  embed
+    .setTitle(`💸  Donate help`)
+    .setURL('https://stormbound-kitty.com/donate')
+    .setDescription(BASE_MESSAGE)
+
+  return embed
+}
 
 export default {
   command: 'donate',
   help: function () {
-    return `💸  **Donate:** ${BASE_MESSAGE}`
+    return getEmbed()
   },
   handler: function () {
-    return BASE_MESSAGE
+    const embed = getEmbed()
+
+    embed.setTitle('Donate')
+
+    return embed
   },
 }
