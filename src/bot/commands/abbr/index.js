09858df3753208b5e8fc016d92756a0fe5ba95c7
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-15 09:47:34 +0200

    Migrate the ‘abbr’ command to embeds

--- src/bot/commands/abbr/index.js
+++ src/bot/commands/abbr/index.js
@@ -1,3 +1,4 @@
+import Discord from 'discord.js'
 import getAbbreviations from '../../../helpers/getAbbreviations'
 import toSentence from '../../../helpers/toSentence'
 
@@ -8,16 +9,32 @@ const quotify = value => `“${value}”`
 export default {
   command: 'abbr',
   help: function () {
-    return `❔  **Abbreviations:** Get the meaning of a card or popular abbreviation (regardless of casing). For instance, \`!${this.command} rof\` or \`!${this.command} AoE\`.`
+    const embed = new Discord.MessageEmbed()
+    embed
+      .setColor('#D7598B')
+      .setTitle(`❔  Abbreviation help`)
+      .setURL('https://stormbound-kitty.com/guides/lexicon')
+      .setDescription(
+        `Get the meaning of a card or popular abbreviation (regardless of casing). For instance, \`!${this.command} rof\` or \`!${this.command} AoE\`.`
+      )
+
+    return embed
   },
   handler: function (message) {
     const matches = ABBREVIATIONS[message.toLowerCase()]
 
-    if (matches) {
-      return `“${message}” might mean ${toSentence(
-        matches.map(quotify),
-        'or'
-      )}.`
-    }
+    if (!matches) return
+
+    const embed = new Discord.MessageEmbed()
+
+    embed
+      .setColor('#D7598B')
+      .setTitle(`Abbreviation “${message}”`)
+      .setURL('https://stormbound-kitty.com/guides/lexicon')
+      .setDescription(
+        `“${message}” might mean ${toSentence(matches.map(quotify), 'or')}.`
+      )
+
+    return embed
   },
 }
--- src/bot/commands/abbr/spec.js
+++ src/bot/commands/abbr/spec.js
@@ -8,16 +8,16 @@ describe('Bot — !abbr', () => {
   })
 
   it('should handle an abbreviation with a single definition', () => {
-    expect(abbr('VotM')).to.contain('Victors of the Melee')
+    expect(abbr('VotM').description).to.contain('Victors of the Melee')
   })
 
   it('should discard casing entirely', () => {
-    expect(abbr('aoe')).to.contain('Area of Effect')
+    expect(abbr('aoe').description).to.contain('Area of Effect')
   })
 
   it('should handle an abbreviation with multiple definitions', () => {
-    expect(abbr('fs')).to.contain('Fusion Stones')
-    expect(abbr('fs')).to.contain('Forgotten Souls')
+    expect(abbr('fs').description).to.contain('Fusion Stones')
+    expect(abbr('fs').description).to.contain('Forgotten Souls')
   })
 
   it('should return nothing for a no-match', () => {
