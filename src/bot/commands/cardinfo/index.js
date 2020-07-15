49d9815f503284fc53a75801e65a26a5dff9bd46
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-15 10:24:35 +0200

    Migrate ‘cardinfo’ command to embeds

--- src/bot/commands/cardinfo/index.js
+++ src/bot/commands/cardinfo/index.js
@@ -1,9 +1,20 @@
+import Discord from 'discord.js'
 import searchCards from '../../../helpers/searchCards'
 
 export default {
   command: 'cardinfo',
   help: function () {
-    return `⚡️  **Card Info:** Get information about the card(s) matching the given search criteria (up to 3 results). It expects a card abbreviation, a Stormbound-Kitty ID, or otherwise performs a “fuzzy search” on the card name. For instance, \`!${this.command} rof\`, \`!${this.command} N1\` or \`!${this.command} souls\`.`
+    const embed = new Discord.MessageEmbed()
+
+    embed
+      .setColor('#D7598B')
+      .setTitle(`⚡️  Card Info help`)
+      .setURL('https://stormbound-kitty.com/card')
+      .setDescription(
+        `Get information about the card(s) matching the given search criteria (up to 3 results). It expects a card abbreviation, a Stormbound-Kitty ID, or otherwise performs a “fuzzy search” on the card name. For instance, \`!${this.command} rof\`, \`!${this.command} N1\` or \`!${this.command} souls\`.`
+      )
+
+    return embed
   },
   handler: function (message) {
     return (
