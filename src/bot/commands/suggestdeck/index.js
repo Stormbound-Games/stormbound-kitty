48e0b2b1f5ac715771e0b047b968684bc7540adf
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-15 11:39:42 +0200

    Migrate ‘suggestdeck’ command to embeds

--- src/bot/commands/suggestdeck/index.js
+++ src/bot/commands/suggestdeck/index.js
@@ -1,27 +1,52 @@
+import Discord from 'discord.js'
 import decks from '../../../data/decks'
 import arrayRandom from '../../../helpers/arrayRandom'
 import getIgnoredSearch from '../../../helpers/getIgnoredSearch'
 import serialisation from '../../../helpers/serialisation'
+import capitalise from '../../../helpers/capitalise'
+import getRawCardData from '../../../helpers/getRawCardData'
 import { parseMessage } from '../decks'
+import { CATEGORIES } from '../../../constants/decks'
 
 export default {
   command: 'suggestdeck',
   help: function () {
-    return `✅  **Deck Suggestion:** Suggest a deck matching the given search criteria. It optionally accepts a faction, category and card to include (regardless of order and casing). For instance, \`!${this.command} ic\`, \`!${this.command} wp d1\` or \`!${this.command} brawl kg\`.`
+    const embed = new Discord.MessageEmbed()
+
+    embed
+      .setColor('#D7598B')
+      .setTitle(`✅  Deck Suggestion help`)
+      .setURL('https://stormbound-kitty.com/guides/lexicon')
+      .setDescription(
+        `Suggest a deck matching the given search criteria. It optionally accepts a faction, category and card to include (regardless of order and casing). For instance, \`!${this.command} ic\`, \`!${this.command} wp d1\` or \`!${this.command} brawl kg\`.`
+      )
+
+    return embed
   },
   handler: function (message) {
     const { params, ignored } = parseMessage(message.toLowerCase())
 
+    const embed = new Discord.MessageEmbed()
+
+    embed.setColor('#D7598B').setTitle(`✅  Deck Suggestion`)
+
     if (Object.keys(params).length === 0) {
-      return (
-        'https://stormbound-kitty.com/deck/' +
-        arrayRandom(
-          // If the category is not provided, assume the expectation is to have
-          // a deck that works and is competitive under normal circumstances (so
-          // ranking and Diamond) and therefore discard any Brawl/Equals deck.
-          decks.filter(deck => !['BRAWL', 'EQUALS'].includes(deck.category))
-        ).id
+      const deck = arrayRandom(
+        // If the category is not provided, assume the expectation is to have
+        // a deck that works and is competitive under normal circumstances (so
+        // ranking and Diamond) and therefore discard any Brawl/Equals deck.
+        decks.filter(deck => !['BRAWL', 'EQUALS'].includes(deck.category))
       )
+
+      embed.setTitle(deck.name)
+      embed.setURL('https://stormbound-kitty.com/deck/' + deck.id)
+      embed.addFields(
+        { name: 'Author', value: deck.author, inline: true },
+        { name: 'Faction', value: capitalise(deck.faction), inline: true },
+        { name: 'Category', value: CATEGORIES[deck.category], inline: true }
+      )
+
+      return embed
     }
 
     const results = decks.filter(deck => {
@@ -51,13 +76,25 @@ export default {
       return true
     })
 
-    return results.length > 0
-      ? [
-          'https://stormbound-kitty.com/deck/' + arrayRandom(results).id,
-          getIgnoredSearch(message, ignored),
-        ]
-          .filter(Boolean)
-          .join('\n')
-      : undefined
+    if (results.length > 0) {
+      const deck = arrayRandom(results)
+
+      embed.setTitle(deck.name)
+      embed.setURL('https://stormbound-kitty.com/deck/' + deck.id)
+      embed.addFields(
+        { name: 'Author', value: deck.author, inline: true },
+        { name: 'Faction', value: capitalise(deck.faction), inline: true },
+        { name: 'Category', value: CATEGORIES[deck.category], inline: true }
+      )
+
+      if (ignored.length > 0) {
+        embed.addFields({
+          name: 'Ignored terms',
+          value: ignored.join(', '),
+        })
+      }
+
+      return embed
+    }
   },
 }
--- src/bot/commands/suggestdeck/spec.js
+++ src/bot/commands/suggestdeck/spec.js
@@ -7,44 +7,44 @@ const BASE_URL = 'https://stormbound-kitty.com/deck/'
 
 describe('Bot — !suggestdeck', () => {
   it('should return a suggested deck for an empty search', () => {
-    expect(suggestdeck('')).to.contain(BASE_URL)
+    expect(suggestdeck('').url).to.contain(BASE_URL)
   })
 
   it('should handle factions', () => {
-    expect(suggestdeck('ironclad').replace(BASE_URL, '')).to.contain('i')
-    expect(suggestdeck('swarm').replace(BASE_URL, '')).to.contain('s')
-    expect(suggestdeck('winter').replace(BASE_URL, '')).to.contain('w')
-    expect(suggestdeck('shadowfen').replace(BASE_URL, '')).to.contain('f')
+    expect(suggestdeck('ironclad').url.replace(BASE_URL, '')).to.contain('i')
+    expect(suggestdeck('swarm').url.replace(BASE_URL, '')).to.contain('s')
+    expect(suggestdeck('winter').url.replace(BASE_URL, '')).to.contain('w')
+    expect(suggestdeck('shadowfen').url.replace(BASE_URL, '')).to.contain('f')
   })
 
   it('should handle categories', () => {
     Object.keys(CATEGORIES).forEach(category => {
-      const id = suggestdeck(category.toLowerCase()).replace(BASE_URL, '')
+      const id = suggestdeck(category.toLowerCase()).url.replace(BASE_URL, '')
       const deck = decks.find(deck => deck.id === id)
       expect(deck.category).to.equal(category)
     })
   })
 
   it('should handle aliases', () => {
-    expect(suggestdeck('ic').replace(BASE_URL, '')).to.contain('i')
-    expect(suggestdeck('red').replace(BASE_URL, '')).to.contain('i')
-    expect(suggestdeck('sw').replace(BASE_URL, '')).to.contain('s')
-    expect(suggestdeck('yellow').replace(BASE_URL, '')).to.contain('s')
-    expect(suggestdeck('w').replace(BASE_URL, '')).to.contain('w')
-    expect(suggestdeck('wp').replace(BASE_URL, '')).to.contain('w')
-    expect(suggestdeck('blue').replace(BASE_URL, '')).to.contain('w')
-    expect(suggestdeck('sf').replace(BASE_URL, '')).to.contain('f')
-    expect(suggestdeck('green').replace(BASE_URL, '')).to.contain('f')
+    expect(suggestdeck('ic').url.replace(BASE_URL, '')).to.contain('i')
+    expect(suggestdeck('red').url.replace(BASE_URL, '')).to.contain('i')
+    expect(suggestdeck('sw').url.replace(BASE_URL, '')).to.contain('s')
+    expect(suggestdeck('yellow').url.replace(BASE_URL, '')).to.contain('s')
+    expect(suggestdeck('w').url.replace(BASE_URL, '')).to.contain('w')
+    expect(suggestdeck('wp').url.replace(BASE_URL, '')).to.contain('w')
+    expect(suggestdeck('blue').url.replace(BASE_URL, '')).to.contain('w')
+    expect(suggestdeck('sf').url.replace(BASE_URL, '')).to.contain('f')
+    expect(suggestdeck('green').url.replace(BASE_URL, '')).to.contain('f')
   })
 
   it('should handle including a card', () => {
-    expect(suggestdeck('N48')).to.contain('n48')
-    expect(suggestdeck('Earyn')).to.contain('n48')
-    expect(suggestdeck('rof')).to.contain('f8')
+    expect(suggestdeck('N48').url).to.contain('n48')
+    expect(suggestdeck('Earyn').url).to.contain('n48')
+    expect(suggestdeck('rof').url).to.contain('f8')
   })
 
   it('should handle multi-searches', () => {
-    const id = suggestdeck('ic d1').replace(BASE_URL, '')
+    const id = suggestdeck('ic d1').url.replace(BASE_URL, '')
     const deck = decks.find(deck => deck.id === id)
 
     expect(deck.category).to.equal('DIAMOND_1')
@@ -53,6 +53,6 @@ describe('Bot — !suggestdeck', () => {
 
   it('should ignore unknown terms', () => {
     const output = suggestdeck('ic foobar')
-    expect(output).to.contain('~~foobar~~')
+    expect(output.fields.pop().value).to.contain('foobar')
   })
 })
