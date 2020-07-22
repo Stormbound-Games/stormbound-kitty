d132ec9b40d195846447574dabb71963a9c9c326
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-22 09:26:36 +0200

    Migrate Trivia scores into a different path

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -6,7 +6,7 @@ const API_BASE_URL = 'https://jsonbin.org/kittysparkles'
 const getScores = guildId =>
   typeof window !== 'undefined' && window.Cypress
     ? Promise.resolve({})
-    : fetch(API_BASE_URL + '/' + guildId, {
+    : fetch(API_BASE_URL + '/' + guildId + '/scores', {
         method: 'GET',
         headers: { Authorization: 'token ' + API_TOKEN },
       }).then(response => response.json())
@@ -16,7 +16,7 @@ const setScore = (id, guildId, update = +1) =>
     .then(scores => +(scores[id] || 0))
     .then(score => {
       if (typeof window !== 'undefined' && window.Cypress) return
-      return fetch(API_BASE_URL + '/' + guildId, {
+      return fetch(API_BASE_URL + '/' + guildId + '/scores', {
         method: 'PATCH',
         headers: { Authorization: 'token ' + API_TOKEN },
         body: JSON.stringify({ [id]: score + update }),
