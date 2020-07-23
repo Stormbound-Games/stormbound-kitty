4fa1ce2ffa1c9f5569819a3f809802e296bb3bc6
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-23 15:22:50 +0200

    Make it possible to use buttons in navigation

--- src/components/NavLink/index.js
+++ src/components/NavLink/index.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default React.memo(({ active, children, to, href, target, rel }) =>
-  to ? (
-    <Link
-      to={to}
-      children={children}
-      className={['Header__link', active && 'Header__link--active']
-        .filter(Boolean)
-        .join(' ')}
-    />
-  ) : (
-    <a
-      href={href}
-      target={target}
-      rel={rel}
-      children={children}
-      className='Header__link'
-    />
-  )
+export default React.memo(
+  ({ active, children, to, href, onClick, target, rel }) =>
+    onClick ? (
+      <button onClick={onClick} children={children} className='Header__link' />
+    ) : to ? (
+      <Link
+        to={to}
+        children={children}
+        className={['Header__link', active && 'Header__link--active']
+          .filter(Boolean)
+          .join(' ')}
+      />
+    ) : (
+      <a
+        href={href}
+        target={target}
+        rel={rel}
+        children={children}
+        className='Header__link'
+      />
+    )
 )
