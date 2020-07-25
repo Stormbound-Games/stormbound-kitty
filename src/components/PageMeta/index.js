c8f3b432bb4cf7754fad7e436c63f1c2a0df37be
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-25 17:10:01 +0200

    Update HTML base content

--- src/components/PageMeta/index.js
+++ src/components/PageMeta/index.js
@@ -12,9 +12,7 @@ export default React.memo(function PageMeta(props) {
       <meta name='description' content={props.description} />
       {!!props.author && <meta name='author' content={props.author} />}
       <meta property='og:description' content={props.description} />
-      <meta property='og:site_name' content='Stormbound Kitty' />
       <meta property='og:url' content={window.location.href} />
-      <meta property='og:type' content='website' />
       {!!props.image ? (
         <meta
           property='og:image'
