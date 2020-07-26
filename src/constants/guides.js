14d6077080398eba0ff198bfbd03530d34252fcd
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-26 10:15:02 +0200

    Move some guides in a new category

--- src/constants/guides.js
+++ src/constants/guides.js
@@ -3,6 +3,10 @@ export const CATEGORIES = {
     slug: 'essentials',
     name: { short: 'Essentials', long: 'Essential Guides' },
   },
+  PLAYSTYLE: {
+    slug: 'playstyle',
+    name: { short: 'Playstyle', long: 'Playstyle Guides' },
+  },
   IN_DEPTH: {
     slug: 'in-depth',
     name: { short: 'In Depth', long: 'In-depth Guides' },
