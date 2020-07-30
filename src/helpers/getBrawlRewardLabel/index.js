7b1aab141c8ecd7754e6b529a60c33791d1c401b
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-07-30 14:51:18 +0200

    Make it possible for ‘getBrawlRewardLabel’ to return React components

--- src/helpers/getBrawlRewardLabel/index.js
+++ src/helpers/getBrawlRewardLabel/index.js
@@ -1,15 +1,26 @@
-const getBrawlRewardLabel = milestone => {
+import React from 'react'
+import { Rubies, Stones } from '../../components/Resource'
+
+const getBrawlRewardLabel = (milestone, enhanced = false) => {
   const amount = milestone.rewardAmount
 
   switch (milestone.reward) {
     case 'HUMBLE_BOOK':
       return `${amount} Humble Book${amount === 1 ? '' : 's'}`
     case 'RUBIES':
-      return `${amount} rub${amount === 1 ? 'y' : 'ies'}`
+      return enhanced ? (
+        <Rubies amount={amount} />
+      ) : (
+        `${amount} rub${amount === 1 ? 'y' : 'ies'}`
+      )
     case 'CLASSIC_TOME':
       return `${amount} Classic Tome${amount === 1 ? '' : 's'}`
     case 'FUSION_STONES':
-      return `${amount} fusion stone${amount === 1 ? '' : 's'}`
+      return enhanced ? (
+        <Stones amount={amount} />
+      ) : (
+        `${amount} fusion stone${amount === 1 ? '' : 's'}`
+      )
     case 'MYTHIC_TOME':
       return `${amount} Mythic Tome${amount === 1 ? '' : 's'}`
     case 'LEGENDARY_CARD':
