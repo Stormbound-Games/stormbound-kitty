2e6a1520f61f7106a23b3f476bb8854d806da145
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-08-03 19:44:48 +0200

    Update the resource guide

--- src/components/GuideResources/index.js
+++ src/components/GuideResources/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import Guide from '../Guide'
 import Info from '../Info'
 import ResourceIcon from '../ResourceIcon'
-import { Coins } from '../Resource'
+import { Coins, Stones } from '../Resource'
 import Title from '../Title'
 import guides from '../../data/guides'
 import cards from '../../data/cards'
@@ -17,15 +17,8 @@ export default React.memo(function GuideResources(props) {
       <p>
         In this guide, I would like to give some insights on how to best manage
         the resources in this game. This is a work-in-progress and will be
-        expanded on in the near future.
-      </p>
-      <p className='Highlight'>
-        This guide has been partially updated for the{' '}
-        <Link to='/changelog/07-2020'>July 2020 release</Link> but is not fully
-        up-to-date. The <a href='#resources'>Resources</a> section is reflecting
-        the new economy changes, but the{' '}
-        <a href='#general-tactics'>General Tactics</a> section has not been
-        updated yet.
+        expanded on in the near future.This guide is based on the July 2020
+        update.
       </p>
 
       <ol style={{ columns: '16em' }}>
@@ -43,17 +36,33 @@ export default React.memo(function GuideResources(props) {
             </li>
           </ul>
         </li>
+
         <li>
-          <a href='#general-tactics'>General Tactics</a>
+          <a href='#income'>Income</a>
           <ul>
             <li>
-              <a href='#grinding'>Grinding</a>
+              <a href='#general'>General</a>
+            </li>
+            <li>
+              <a href='#exchanging'>Exchanging</a>
             </li>
             <li>
               <a href='#hoarding'>Hoarding</a>
             </li>
+          </ul>
+        </li>
+
+        <li>
+          <a href='#spending'>Spending</a>
+          <ul>
             <li>
-              <a href='#spending'>Spending</a>
+              <a href='#setting-a-goal'>Setting a goal</a>
+            </li>
+            <li>
+              <a href='#reaching-diamond-1'>Reaching Diamond 1</a>
+            </li>
+            <li>
+              <a href='#full-collection-level-5'>Full collection level 5</a>
             </li>
           </ul>
         </li>
@@ -71,13 +80,16 @@ export default React.memo(function GuideResources(props) {
 
       <Title id='resources'>Resources</Title>
 
-      <h3>
+      <h3 id='coins'>
         <ResourceIcon resource='COIN' /> Coins
       </h3>
 
       <p>Coins are available through 5 different means:</p>
       <ul>
-        <li>Tier 1 daily missions (100).</li>
+        <li>
+          Tier 1 and 3 daily missions (100 for tier 1 + 150 for tier 3, where
+          the odds of having coins as a reward are 6 to 10).
+        </li>
         <li>
           Daily victories (30 extra for the first daily win, as well as 5, 10 or
           20 depending on whether the game is played on mobile or Steam, with or
@@ -100,7 +112,7 @@ export default React.memo(function GuideResources(props) {
         then win another game (with ads), bringing the total to 415.
       </Info>
 
-      <h3>
+      <h3 id='rubies'>
         <ResourceIcon resource='RUBY' /> Rubies
       </h3>
 
@@ -115,22 +127,73 @@ export default React.memo(function GuideResources(props) {
         <li>Shop purchases as part of bundles or rubies only.</li>
       </ul>
 
-      <h3>
+      <h3 id='fusion-stones'>
         <ResourceIcon resource='STONE' /> Fusion Stones
       </h3>
 
-      <p>Fusion Stones are available through 3 different means:</p>
+      <p>Fusion Stones are available through 4 different means:</p>
       <ul>
-        <li>Tier 3 daily missions (1 or 2).</li>
-        <li>Weekly Brawl (10 at Milestone 4, 50 at Milestone 7).</li>
+        <li>
+          Tier 3 daily missions (2, where the odds of having fusion stones as a
+          reward are 4 to 10).
+        </li>
+        <li>
+          Weekly Brawl (10 at Milestone 4, 50 at Milestone 7, 200 at Milestone
+          10).
+        </li>
         <li>
           <Link to='/collection/books'>Sometimes in tomes and books</Link>.
         </li>
         <li>Shop purchases as part of bundles.</li>
       </ul>
-      <p style={{ marginBottom: '3em' }}>
-        Note: Fusion Stones used to be rewarded in exchange of lvl 5 card
-        duplicates but these are now replaced with coins.
+
+      <Title id='income'>Income</Title>
+
+      <h3 id='general'>General</h3>
+
+      <p>
+        Have a look at the{' '}
+        <Link to='/income-calculator'>income calculator</Link> to know how your
+        playstyle affects your income. The general advice here is to at least do
+        these 2 things:
+      </p>
+      <ol>
+        <li>Open your daily free Humble book for a random card.</li>
+        <li>
+          Complete your daily missions. Find some people to play with to easily
+          complete the tougher missions.
+        </li>
+      </ol>
+
+      <p>
+        Playing on mobile can greatly reduce the time it takes to maximise coins
+        through wins, especially with a solid rush deck for this. Brawl
+        typically isn’t worth the costs beyond Milestone 5 (Mythic Tome) and
+        even then you would want to achieve at least a win-rate of 50% (roughly{' '}
+        <Coins amount={1780} />
+        ).
+      </p>
+
+      <h3 id='exchanging'>Exchanging</h3>
+
+      <p>
+        Exchanging resources such as <ResourceIcon resource='RUBY' /> rubies for{' '}
+        <ResourceIcon resource='COIN' /> coins is generally not worth it. The
+        conversion rates are bad and you will most likely find a better use for
+        the resource. Duplicate cards at level 5 used to be exchanged for{' '}
+        <ResourceIcon resource='STONE' /> fusion stones instead of coins. There
+        is some indication this might change again in the future so it is
+        recommended you hold on to your duplicates for now.
+      </p>
+
+      <h3 id='hoarding'>Hoarding</h3>
+
+      <p>
+        If you’re planning on playing Stormbound for a while it is always
+        advisable to set aside some resources for sales, offers and certain
+        cards or books/tomes you might want to get in the shop. It has happened
+        in the past that books and tomes were temporarily cheaper and people who
+        had saved a lot of rubies made good deals.
       </p>
 
       <Info title='Community rewards' icon='crown'>
@@ -139,11 +202,12 @@ export default React.memo(function GuideResources(props) {
         social channels and the Discord server for more information.
       </Info>
 
-      <Title id='general-tactics'>General Tactics</Title>
+      <Title id='spending'>Spending</Title>
 
       <p>
-        How to best use your resources depends on your goals and values. In
-        terms of goals, Stormbound can be broken down into a few:
+        How to best spend your resources depends on how you want to play the
+        game. Casual play or goal orientated. In terms of goals, Stormbound can
+        be broken down into a few main ones:
       </p>
 
       <ul>
@@ -152,153 +216,144 @@ export default React.memo(function GuideResources(props) {
           Completing one’s collection (having at least one copy of each card)
         </li>
         <li>Bringing full collection to level 5</li>
-        <li>Getting to the top of the weekly Brawl leaderboard</li>
       </ul>
 
       <p>
-        Some of these goals are a lot harder to achieve. Consider that upgrading
-        your full collection to level 5 costs you{' '}
-        <Coins amount={cards.filter(card => !card.token).length * 1450} />,
-        which would take you at least 500 days to grind with in-game rewards. As
-        a <abbr title='Free-To-Play'>F2P</abbr> player myself, I’ve run multiple
-        calculations and it woud take over 4 years of playing in the current
-        state to get to a full collection at level 5. It is very likely the game
-        will change before then, as would the calculations, so I’ll leave this
-        for now.
+        I’ll outline the best strategy for using your resources for each goal.
+      </p>
+
+      <h3 id='casual-play'>Casual play</h3>
+
+      <p>
+        If you’re looking to just play when you can and are not hell-bend on any
+        specific goal or grinding resources there is still some advice on how to
+        use your time and resources efficiently.
+      </p>
+      <p>
+        This game is all about creating decks. For that, a bigger collection is
+        better. The best way to grow your collection fairly evenly in the
+        beginning is by buying Noble Books, Heroic Tomes and opening your daily
+        Humble Book.
       </p>
 
       <p>
-        Perhaps more important is to look at other goals and what you want to
-        get out of this game. Most of you will have decided to play or stick
-        with Stormbound for the fun of it. As an intricate strategy game there
-        are a lot of great plays to be found in trying out different decks and
-        figuring out the changing meta of the weekly Brawl.
+        Once you have most common and rare cards, you could consider keeping
+        some <ResourceIcon resource='COIN' /> coins and{' '}
+        <ResourceIcon resource='RUBY' /> rubies aside to buy singular cards in
+        the shop or save up <ResourceIcon resource='RUBY' /> rubies for a Mythic
+        Tome. You’ll have a feel for the cards and maybe also have some
+        favourite cards and/or factions to buy more specific copies from. Please
+        refer to the section below for some recommended cards or have a look at
+        the <Link to='/list/ranked'>tier list</Link> to see which cards have
+        high value in this game.
       </p>
 
       <p>
-        Personally, I’ve found that building a main deck that can get you to
-        Platinum 1, and then upgrading cards that you want to use with in the
-        current Brawl, while building other decks to have fun with in Ranked
-        Gold/Platinum is best.
+        I advise you to hold on to your <ResourceIcon resource='STONE' /> fusion
+        stones until you have good candidate to upgrade it on. This will
+        probably be a legendary card as you cannot ever buy those specifically
+        in the shop. Crafting a card you haven’t gotten yet greatly diminishes
+        the value of <ResourceIcon resource='STONE' /> fusion stones, so think
+        twice about doing that.
       </p>
 
       <p>
-        Obviously you might also want to set yourself a different challenge,
-        such as trying to reach Diamond 1 in the least amount of days from a new
-        account, or with the lowest average card level.
+        Upgrading cards can get costly so be mindful of what you upgrade. It is
+        not a great idea to try and upgrade everything but rather focus on the
+        cards you use. If you cannot play much, make sure you open your daily
+        free card and aim to complete your daily missions, favouring the{' '}
+        <Coins amount={150} /> mission over the <Stones amount={2} /> mission
+        (each mission can be re-rolled once a day before completing).
       </p>
 
+      <h3 id='reaching-diamond-1'>Reaching Diamond 1</h3>
+
       <p>
-        The main point being that your resource management depends a little bit
-        on your personal goals. However, it is possible to give some general
-        advice on how to best use coins, rubies and fusion stones.
+        Solely reaching the top-league can be achieved within 6 months of
+        starting the game if planned correctly. It would be best to focus on a
+        single (cheap) deck to do so. Read{' '}
+        <Link to='/guides/d1-with-sf-commons'>
+          Kitty’s guide on achieving it with a mostly common SF deck
+        </Link>
+        , or try out{' '}
+        <Link to='/deck/3n13n23s13n33s243s23n633n673s63n153s83s11/detail'>
+          Reckless Rush
+        </Link>
+        ’s deck.
+      </p>
+      <p>
+        The reason why a cheap deck, i.e. low rarity cards, is best is because
+        you need higher level cards in order to be able to compete in the
+        Diamond league (and to some extent the Platinum league). It can take a
+        long time to find the right copies of epic and legendary cards, and the
+        latter are hard to come by, so for getting to Diamond 1 fast it is
+        highly recommended to build a deck without.
+      </p>
+      <p>
+        Resource-wise it is simple: buy Noble Books until you have most cards in
+        the deck and then focus on buying copies of those cards when they appear
+        in the shop. Save <ResourceIcon resource='RUBY' /> rubies for any epic
+        card copies you might have in your deck, and with a high surplus buy
+        Heroic Tomes.
+      </p>
+      <p>
+        Leave Brawl beyond Milestone 1 aside as it is costly, the many modifiers
+        will probably make your deck less competitive and the rewards are skewed
+        towards higher tier cards. That being said,{' '}
+        <Link to='/deck/3n13n23s13n33s243s23n633n673s63n153s83s11/detail'>
+          Reckless Rush
+        </Link>
+        ’s deck does perform well in several current Brawl modifiers. However,
+        even then it is probably not worth trying to go any further than the{' '}
+        <Stones amount={10} /> milestone.
       </p>
 
-      <h3 id='grinding'>Grinding</h3>
+      <h3 id='full-collection-level-5'>Full collection level 5</h3>
 
       <p>
-        My main tip is to make your daily grinding as easy as possible. There
-        are daily missions for Ironclad Bolstering, spells, structures, token
-        units, push units forward, Swarm, freeze, push ’n pull with Ironclad,
-        poison, mana Winter, convert, amongst others so make decks to complete
-        these easily. You can also play with friends to complete these
-        challenges.
-      </p>
-
-      <p>
-        If you don’t have much time to play, prioritise completing your daily
-        challenges. For the daily wins, build a good rush deck. It takes about
-        an hour to win the 11 games necessary to reach the daily coins cap.
-      </p>
-
-      <h3 id='hoarding'>Hoarding</h3>
-
-      <p>
-        If you’re planning on playing for a while it is always advisable to set
-        aside some resources for sales, offers and certain cards or books/tomes
-        you might want to get in the shop. It happened in the past that books
-        were temporarily cheaper and people having saved a lot of rubies made
-        good deals.
-      </p>
-
-      <h3 id='spending'>Spending</h3>
-
-      <h4>
-        <ResourceIcon resource='COIN' /> Coins
-      </h4>
-
-      <p>
-        In the beginning it is advisable to expand your collection by purchasing
-        Noble Books. However, in the long-term it is inefficient to keep buying
-        these. Depending on your strategy and the amount of commons in your
-        preferred deck(s), it can be best to buy singular specific cards in the
-        shop.
-      </p>
-
-      <p>
-        If you are in it for the long-run, it’s almost exclusively better to
-        save your coins for high value cards in the shop, and using your coins
-        for Brawl (in which getting to the Mythic Tome for under{' '}
-        <Coins amount='1,800' /> is a rough goal). Have a look at the{' '}
-        <Link to='/list/ranked'>tier-list</Link> to see which cards have high
-        value in this game.
-      </p>
-
-      <h4>
-        <ResourceIcon resource='RUBY' /> Rubies
-      </h4>
-
-      <p>
-        As with coins, using rubies on specific (high value) cards in the shop
-        for use your preferred deck(s) can be more efficient. The Classic Tome
-        isn’t worth the value, especially compared with the cost of a Noble
-        Book. Rubies in general get a terrible coin exchange. If your goal is to
-        bring the full collection to level 5, it is most efficient to buy Heroic
-        Tomes.
-      </p>
-
-      <h4>
-        <ResourceIcon resource='STONE' /> Fusion Stones
-      </h4>
-
-      <p>
-        It is not advisable to use these to craft cards as it diminishes the
-        value significantly compared to using it for duplicates. As the rarest
-        of resources I would recommend holding on to these until you have a good
-        use for them, usually a specific Epic or Legendary card. However, if you
-        want to complete your collection it is best to hold off until the end to
-        fill out the gaps in your collection.
-      </p>
-
-      <p>
-        Unfortunately it’s difficult to draw a definitive conclusion on how best
-        to use your resources as it depends on your play style and goals.
-      </p>
-
-      <p>
-        To help you in making these decisions, here are some stats and figures:
+        This goal is a lot harder to achieve than the other two. Consider the
+        following. The amount of cards needed for each rarity tier is:
       </p>
       <ul>
-        <li>
-          There are {cards.filter(card => !card.token).length} different cards
-          to collect in the game.
-        </li>
-        <li>
-          7,451 copies needed in total with distribution being 44.3% for
-          commons, 33.6% for rares, 16.5% for epics and 5.6% for legendaries.
-        </li>
-        <li>
-          Upgrading your collection to have all cards level 5 costs{' '}
-          <Coins amount='272,600' />.
-        </li>
-        <li>
-          The closest you can get to matching the card distribution after
-          considering the cards already gained through ranking and the campaign,
-          is by aiming for daily ruby mission, playing the weekly Brawl up to
-          the Mythic Tome if it costs you less than <Coins amount='1,800' />,
-          and buying Heroic Tomes.
-        </li>
+        <li>Common: 3,300 cards</li>
+        <li>Rare: 2,501 cards</li>
+        <li>Epic: 1,230 cards</li>
+        <li>Legendary: 441 cards</li>
       </ul>
+
+      <p>
+        Upgrading your full collection to level 5 costs{' '}
+        <Coins amount={cards.filter(card => !card.token).length * 1450} />,
+        which would take you at least 410 days of max grind to save up,
+        including in-game rewards and no Brawl.
+      </p>
+      <p>
+        Since Sheepyard has taken over development of the game, it’s very likely
+        the game’s collection and economy will change before you get to complete
+        your collection. I recommend not going straight for the complete level 5
+        collection goal because the grind will be very long and arduous (getting
+        max coins each day will require about 2.5 hours game play on mobile, and
+        about 4 hours on Steam).
+      </p>
+      <p>
+        However, there are two tools on this website that can greatly help in
+        getting you to complete your collection should you want to shoot for it.
+        Uploading your <Link to='/collection'>collection</Link>, and using the
+        <Link to='/income-calculator'>income calculator</Link>. With those you
+        can see how many more cards you need of each rarity and how to adjust
+        your income to match that. You might find for instance it’s better for
+        you to buy Heroic Tomes rather than Mythic Tomes (since the way the
+        cards are distributed and generally rewarded makes the rare cards the
+        hardest to complete). You might find preferring{' '}
+        <ResourceIcon resource='STONE' /> fusion stones missions better than the
+        <ResourceIcon resource='COIN' /> coins missions if you’ve saved up
+        enough and looking for final copies.
+      </p>
+
+      <p>
+        How you’ve gone about building your collection thusfar affects what’s
+        the best income strategy for you.
+      </p>
     </Guide>
   )
 })
