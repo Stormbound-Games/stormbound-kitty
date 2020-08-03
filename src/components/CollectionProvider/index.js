18739716d0cc1933daf113d67df66af87ca20ab7
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-08-03 09:06:57 +0200

    No longer store default collections

--- src/components/CollectionProvider/index.js
+++ src/components/CollectionProvider/index.js
@@ -38,25 +38,26 @@ export default function CollectionProvider(props) {
     try {
       let savedCollection = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-      if (savedCollection.length > 0) {
-        // It is possible that the locally saved collection does not contain all
-        // the cards in the game if it was recorded before a card gets added. In
-        // such case, we should update the collection with the missing card(s),
-        // and mark them as missing.
-        if (savedCollection.length !== cardsWithoutTokens.length) {
-          savedCollection = cardsWithoutTokens.map(
-            card =>
-              savedCollection.find(entry => entry.id === card.id) || {
-                id: card.id,
-                level: 1,
-                // In a default collection, missing cards should not be marked
-                // as missing since they are all considered owned and level 1 by
-                // default.
-                missing: !isDefaultCollection(savedCollection),
-                copies: 0,
-              }
-          )
-        }
+      // Default collections used to be locally saved but this is no longer the
+      // case so if the collection is a default one, remove it from storage.
+      if (isDefaultCollection(savedCollection)) {
+        localStorage.removeItem(STORAGE_KEY)
+      }
+
+      // It is possible that the locally saved collection does not contain all
+      // the cards in the game if it was recorded before a card gets added. In
+      // such case, we should update the collection with the missing card(s),
+      // and mark them as missing.
+      else if (savedCollection.length !== cardsWithoutTokens.length) {
+        savedCollection = cardsWithoutTokens.map(
+          card =>
+            savedCollection.find(entry => entry.id === card.id) || {
+              id: card.id,
+              level: 1,
+              missing: true,
+              copies: 0,
+            }
+        )
 
         setCollection(savedCollection)
         notify('Locally saved collection found and loaded.')
@@ -65,28 +66,15 @@ export default function CollectionProvider(props) {
   }, [notify])
 
   React.useEffect(() => {
-    const RogueSheep = collection.find(card => card.id === 'N77')
-
-    // When Rogue Sheep came out, I made the mistake of tagging it as missing by
-    // default. This is the desired behaviour for people recording their
-    // collection on the site (since they don’t own the card yet), but is not
-    // expected for a default collection, where all cards are considered owned
-    // and level 1 by default. In turn, this prevents Rogue Sheep from being
-    // used in decks because it is marked as missing from the default
-    // collection. This needs to be fixed by sniffing whether the collection is
-    // the default one with just Rogue Sheep missing (the bug to fix), and if
-    // that’s the case, making Rogue Sheep available. This check can eventually
-    // be removed, hopefully in a couple of weeks, once it is considered fixed
-    // for most people using the default collection.
-    if (isDefaultCollection(collection) && RogueSheep.missing) {
-      setCollection(
-        collection.map(card =>
-          card.id === RogueSheep.id ? { ...card, missing: false } : card
-        )
-      )
+    // If the collection is the default one, remove it from the local storage as
+    // this is not only unnecessary, but can also lead to hard-to-track bug
+    // where obsolete data get stored (e.g. a default collection of 188 cards
+    // before a 189th gets added to the game).
+    if (isDefaultCollection(collection)) {
+      localStorage.removeItem(STORAGE_KEY)
+    } else {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(collection))
     }
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(collection))
   }, [collection])
 
   const resetCollection = () => {
