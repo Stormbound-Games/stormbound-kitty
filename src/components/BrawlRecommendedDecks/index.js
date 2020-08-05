aea6518c3c62bfc16c0b7dc1d544ada9c9583fca
Kitty <53430374+KittySparkles@users.noreply.github.com>
2020-08-05 11:31:13 +0200

    Add a link to more Brawl decks

--- src/components/BrawlRecommendedDecks/index.js
+++ src/components/BrawlRecommendedDecks/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import decks from '../../data/decks'
 import { BrawlContext } from '../BrawlProvider'
 import { CollectionContext } from '../CollectionProvider'
 import Decks from '../Decks'
+import Icon from '../Icon'
 import Title from '../Title'
 import sortDeckSuggestions from '../../helpers/sortDeckSuggestions'
+import './index.css'
 
 export default React.memo(function BrawlRecommendedDecks(props) {
   const { id } = React.useContext(BrawlContext)
@@ -27,6 +30,13 @@ export default React.memo(function BrawlRecommendedDecks(props) {
     <>
       <Title>Recommended deck{props.limit === 1 ? '' : 's'}</Title>
       <Decks showUpgrades columns={props.columns} decks={brawlDecks} />
+      <p className='BrawlRecommendedDecks__more'>
+        <Icon icon='arrow-right' className='BrawlRecommendedDecks__icon' />{' '}
+        Check more{' '}
+        <Link to={'/deck/suggestions?category=BRAWL&brawl=' + id}>
+          decks for this brawl
+        </Link>
+      </p>
     </>
   )
 })
