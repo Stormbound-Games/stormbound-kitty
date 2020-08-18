2fa983a203f78ae925c2209229f883fe2af4aa04
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-08-18 08:52:29 +0200

    Make bot help command more loose

--- src/bot/handle.js
+++ src/bot/handle.js
@@ -27,7 +27,7 @@ export default client => async message => {
     const user = message.mentions.users.first() || message.author
     const ping = command.ping === false ? '' : user
     const channelId = getChannelId(message, command)
-    const answer = content.includes('help')
+    const answer = /\bhelp\b/.test(content)
       ? await command.help(content, client, message)
       : await command.handler(content, client, message)
 
