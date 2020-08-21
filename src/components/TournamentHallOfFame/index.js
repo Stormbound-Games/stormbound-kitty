2fa7ea59f135f2254222bd2f4349261c7d73e2fa
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-08-21 10:32:25 +0200

    Improve the tournaments page

--- src/components/TournamentHallOfFame/index.js
+++ src/components/TournamentHallOfFame/index.js
@@ -8,11 +8,23 @@ import PageMeta from '../PageMeta'
 import Row from '../Row'
 import Teaser from '../Teaser'
 import Title from '../Title'
+import { Rubies } from '../Resource'
 import tournaments from '../../constants/tournaments.json'
 import toSentence from '../../helpers/toSentence'
 import getRawCardData from '../../helpers/getRawCardData'
-import formatDate from '../../helpers/formatDate'
 import './index.css'
+
+const formatDate = date => {
+  const formatter = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: 'long',
+  })
+  const parts = formatter.formatToParts(date)
+  const month = parts[0].value
+  const year = parts[2].value
+
+  return month + ' ' + year
+}
 
 const getDate = string => {
   if (!string) return null
@@ -90,21 +102,99 @@ export default React.memo(function TournamentHallOfFame(props) {
             </tr>
           </thead>
           <tbody>
-            {tournaments.map(tournament => (
-              <tr key={tournament.name}>
-                <td>
-                  {tournament.date
-                    ? formatDate(getDate(tournament.date))
-                    : 'Unknown date'}
-                </td>
-                <td>{tournament.name}</td>
-                <td>{toSentence(tournament.hosts, 'and')}</td>
-                <td>{toSentence(tournament.winners, 'and')}</td>
-              </tr>
-            ))}
+            {tournaments
+              .slice(0)
+              .reverse()
+              .map(tournament => (
+                <tr key={tournament.name}>
+                  <td>
+                    {tournament.date
+                      ? formatDate(getDate(tournament.date))
+                      : 'Unknown date'}
+                  </td>
+                  <td>{tournament.name}</td>
+                  <td>{toSentence(tournament.hosts, 'and')}</td>
+                  <td>{toSentence(tournament.winners, 'and')}</td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
+
+      <FAQSection
+        id='faq'
+        title='FAQ'
+        entries={[
+          {
+            id: 'about-tournaments',
+            question: 'What exactly are tournaments? How do they work?',
+            answer:
+              'Tournaments are organised by community members and yield in-game rewards such as cards and rubies. They usually face about 20 players divided in brackets, eliminating more and more of them until there is only one (or more) winner left. Some tournmanents have different rules, but they more or less all work like this.',
+          },
+          {
+            id: 'hosting-a-tournament',
+            question: 'Who can host a tournament?',
+            answer:
+              'In theory, everyone can. Kepp and Brzoza on Discord are responsible for the schedule. If you would like to host an event, have them invite you to a dedicated Discord server where all the event hosts organise events. In there, you will find the schedule, and will be able to talk about your ideas and get some help.',
+          },
+          {
+            id: 'head-count',
+            question:
+              'How many participants usually join an event? What kind of rewards can there be?',
+            answer: (
+              <>
+                <p>
+                  These both depend on the kind of event, whether it’s a{' '}
+                  <span className='Highlight'>Tournament</span> or a{' '}
+                  <span className='Highlight'>Joust</span>.
+                </p>{' '}
+                <p>
+                  Tournaments can have unlimited members, and have a prize pool
+                  of about <Rubies amount={400} /> to distribute between the
+                  winners. They get official media coverage, but require at
+                  least 2 hosts, last longer, and are bound to a pretty long
+                  schedule.
+                </p>
+                <p>
+                  Jousts on the other hand are smaller (up to 32 players), can
+                  be self-hosted on a short schedule and usually have less
+                  pressure on them. They do not benefit from media coverage and
+                  the prize pool is about <Rubies amount={160} />.
+                </p>
+                <p>
+                  This distinction helps the hosts with experimenting more with
+                  events, without having to wait months for other tournaments to
+                  be hosted, and generally makes things clearer for everyone.
+                </p>
+              </>
+            ),
+          },
+          {
+            id: 'tools',
+            question: 'Are there tools to help organise everything?',
+            answer: (
+              <>
+                The <Link to='/deck'>deck builder</Link> to compose decks,{' '}
+                <a
+                  href='https://challonge.com/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Challonge
+                </a>{' '}
+                to create and display brackets, Google Forms for the
+                registration, and Discord for communication.
+              </>
+            ),
+          },
+          {
+            id: 'rules',
+            question: 'How to write good rules that prevent cheating?',
+            answer:
+              'Have a look at the rules of previous tournaments to avoid starting from scratch. Make sure to have them proofread by some tournament organisers so they are bulletproof. ',
+          },
+        ]}
+      />
 
       <Notice icon='crown'>
         For more information about ongoing tournaments and how to participate,
@@ -118,62 +208,6 @@ export default React.memo(function TournamentHallOfFame(props) {
         </a>
         .
       </Notice>
-
-      <FAQSection
-        id='faq'
-        title='FAQ'
-        entries={[
-          {
-            id: 'hosting-an-event',
-            question:
-              'I have a tournament idea and would like to host an event. Where do I start?',
-            answer:
-              'Please message Kepp or Brzoza on Discord so they invite you to a dedicated Discord server where all the event hosts organise events. In there, you will find the schedule, and will be able to talk about your ideas and even get some help.',
-          },
-          {
-            id: 'overlapping-events',
-            question: 'Can I host a tournament while another is ongoing?',
-            answer:
-              'We usually tend to avoid that, unless you want to host something small, due to the player base not being big enough to host multiple events at the same time. Who knows, maybe one day…!',
-          },
-          {
-            id: 'head-count',
-            question: 'How many participants should I expect to join my event?',
-            answer:
-              'Generally, one can expect around 30 participants, 16 being the minimum. Some tournaments even reached 60 participants though but they become increasingly more difficult to organise!',
-          },
-          {
-            id: 'rewards',
-            question: 'How many rewards should I ask for?',
-            answer:
-              'Usually the prizes are around a Mythic Tome for the 1st player, Heroic Tome for the 2nd, Classic Tome for 3rd. One can also look into giving a few rubies to more players and other resources like fusion stones could also be taken into account.',
-          },
-          {
-            id: 'tools',
-            question: 'What tools/sites should I use to help me?',
-            answer: (
-              <>
-                The <Link to='/deck'>deck builder</Link> to compose decks,
-                <a
-                  href='https://challonge.com/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  Challonge
-                </a>{' '}
-                to create and display brackets, Google Forms for the submission
-                form, and Discord for communication.
-              </>
-            ),
-          },
-          {
-            id: 'rules',
-            question: 'How to write good rules that prevent cheating?',
-            answer:
-              'Have a look at the rules of previous tournaments to avoid starting from scratch. Make sure to have them proofread by some tournament organisers so they are bulletproof. ',
-          },
-        ]}
-      />
 
       <PageMeta
         title='Tournaments — Hall of Fame'
