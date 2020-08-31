449149c04199102f715006f0473d767cbb80d8b8
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-08-31 20:14:54 +0200

    Fix navigation tests

--- cypress/integration/navigation/active.js
+++ cypress/integration/navigation/active.js
@@ -4,7 +4,7 @@ describe('Navigation — Active', () => {
   before(() => cy.visit('/'))
 
   it('should have proper active state', () => {
-    Array.from({ length: 8 }, (_, i) => {
+    Array.from({ length: 6 }, (_, i) => {
       cy.get(s.LINK)
         .eq(i)
         .click()
@@ -16,21 +16,5 @@ describe('Navigation — Active', () => {
         .find(s.ACTIVE)
         .should('have.length', 1)
     })
-  })
-
-  it('should have have a proper collapsible menu on mobile', () => {
-    cy.viewport('iphone-5')
-      .get(s.NAV)
-      .should('not.be.visible')
-      .get('[aria-controls="navigation-target"]')
-      .should('be.visible')
-      .click()
-      .get(s.NAV)
-      .should('be.visible')
-      .get(s.LINK)
-      .first()
-      .click()
-      .get(s.NAV)
-      .should('not.be.visible')
   })
 })
