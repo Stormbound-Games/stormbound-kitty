7de51c338d01b9a325ed5773e1c2992773a8d954
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-12 12:20:26 +0200

    Improve user guides

--- src/components/MemberGuides/index.js
+++ src/components/MemberGuides/index.js
@@ -1,33 +1,22 @@
 import React from 'react'
-import Banner from '../Banner'
+import Column from '../Column'
+import GuideTeaser from '../GuideTeaser'
 import MemberSection from '../MemberSection'
-import getRawCardData from '../../helpers/getRawCardData'
-import toSentence from '../../helpers/toSentence'
+import Row from '../Row'
+import chunk from '../../helpers/chunk'
 
 export default React.memo(function MemberGuides(props) {
   if (props.guides.length === 0) return null
 
   return (
     <MemberSection title='Guides'>
-      {props.guides.map(guide => {
-        const authors = toSentence(guide.authors, 'and')
-
-        return (
-          <Banner
-            key={guide.name}
-            faction={guide.faction}
-            title={guide.name}
-            subline={`By ${authors}`}
-            copy={guide.excerpt}
-            cta={{
-              'aria-label': `Read ${guide.name} by ${authors}`,
-              to: '/guides/' + guide.slug,
-              children: 'Read guide',
-            }}
-            image={'/assets/images/cards/' + getRawCardData(guide.cardId).image}
-          />
-        )
-      })}
+      {chunk(props.guides, 3).map((row, index) => (
+        <Row key={index} desktopOnly wideGutter>
+          <Column width='1/3'>{row[0] && <GuideTeaser {...row[0]} />}</Column>
+          <Column width='1/3'>{row[1] && <GuideTeaser {...row[1]} />}</Column>
+          <Column width='1/3'>{row[2] && <GuideTeaser {...row[2]} />}</Column>
+        </Row>
+      ))}
     </MemberSection>
   )
 })
