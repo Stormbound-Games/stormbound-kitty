1234438eb11defecdf8aec740a02f510688d0393
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-16 09:09:09 +0200

    Fix duplicated slug

--- src/constants/guides.js
+++ src/constants/guides.js
@@ -10,7 +10,7 @@ export const CATEGORIES = {
     icon: 'power',
   },
   BRAWL: {
-    slug: 'brawl',
+    slug: 'brawl-mode',
     name: { short: 'Brawl', long: 'Brawl Guides' },
     icon: 'crown',
   },
