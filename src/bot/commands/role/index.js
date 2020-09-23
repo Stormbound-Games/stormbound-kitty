2edba1cf209c46139ed7b5fbe7afaa8ea1585fb4
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-23 17:40:33 +0200

    Add SWCC role to bot

--- src/bot/commands/role/index.js
+++ src/bot/commands/role/index.js
@@ -11,7 +11,7 @@ const LEAGUE_ROLES = [
   'Iron',
   'Starter',
 ]
-const MISC_ROLES = ['Tournamentee', 'Streambound', 'Quest Helper']
+const MISC_ROLES = ['Tournamentee', 'Streambound', 'Quest Helper', 'SWCC']
 const ROLES = [...LEAGUE_ROLES, ...MISC_ROLES]
 
 const getAvailableRoles = guild => {
