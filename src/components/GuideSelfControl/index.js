20d074f8662d7cffa894863456cba3b565dc89e1
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-24 12:32:59 +0200

    Improve the Gazette

--- src/components/GuideSelfControl/index.js
+++ src/components/GuideSelfControl/index.js
@@ -34,10 +34,14 @@ export default React.memo(function GuideNobleCoalition(props) {
 
       <p>
         The “Self Control” Brawl is a very cheap and fast one. Most games are
-        done or settled by turn 10, and it relies quite a lot of the level of
-        cheap cards, neutral and Swarm alike. It is important to clear the
-        opponent’s front-line regularly early on because it can start
-        steamrolling very quickly.
+        done or settled by turn 10, and it relies quite a lot on the level of
+        cheap cards, neutral and Swarm alike.
+      </p>
+      <p>
+        It is important to clear the opponent’s front-line regularly early on
+        because it can start steamrolling very quickly. Be especially careful to{' '}
+        <WikiLink id='S3' /> which can easily be brought back into the hand
+        given it has 2 movement.
       </p>
 
       <Title>Possible Decks</Title>
@@ -89,7 +93,8 @@ export default React.memo(function GuideNobleCoalition(props) {
             </p>
             <p>
               If you main Shadowfen and your cards are maxed out, you might be
-              able to pull this off by avoiding Swarm entirely.
+              able to pull this off by avoiding Swarm entirely. TheMirc also did
+              run Shadowfen in that Brawl.
             </p>
           </Column>
           <Column>
@@ -118,13 +123,13 @@ export default React.memo(function GuideNobleCoalition(props) {
           probably a must have.
         </li>
         <li>
-          <WikiLink id='N2' /> is surprisingly good in that Brawl. Not only can
-          it stop runners (or Counselor Ahmi from coming back in hand) but it
-          also gives some meat for Pan Heralds.
+          <WikiLink id='N2' /> is surprisingly efficient in that Brawl. Not only
+          can it stop runners (or Counselor Ahmi from coming back in hand) but
+          it also gives some meat for Pan Heralds.
         </li>
         <li>
           <WikiLink id='S1' /> is really nice, especially since it can attack
-          sideways.
+          sideways. Definitely a card to include.
         </li>
         <li>
           <WikiLink id='N3' /> are also as good as always, although some
@@ -136,7 +141,7 @@ export default React.memo(function GuideNobleCoalition(props) {
           Pan Heralds.
         </li>
         <li>
-          <WikiLink id='N2' /> is amazing in that Brawl and definitely a
+          <WikiLink id='N4' /> is amazing in that Brawl and definitely a
           must-have.
         </li>
         <li>
@@ -154,7 +159,7 @@ export default React.memo(function GuideNobleCoalition(props) {
           can play 2 or 3 times in a row and this is how most games end.
         </li>
         <li>
-          I’m very skeptical about <WikiLink id='S6' />. Everything goes so fast
+          <WikiLink id='S6' /> is a bit of an odd ball. Everything goes so fast
           that there is little need for the commanding ability, and Counselor
           Ahmi is almost always a better play at 3 mana. I guess it’s good with
           a control strategy, especially if level 5.
@@ -165,10 +170,9 @@ export default React.memo(function GuideNobleCoalition(props) {
           it becomes hard to clear with Lawless Herd and Counselor Ahmi.
         </li>
         <li>
-          Speaking of control strategy, <WikiLink id='S7' /> can be a decent
-          call, and Cusack ran it all the way through I think. I tried it on a
-          few games but couldn’t make it work (literally dropped it only once
-          and Critical Pancake destroyed it on the spot).
+          <WikiLink id='S7' /> can be a decent call for control strategies, as
+          it can distracts the opponent a little. It goes a bit against rush
+          though, so it might be a tough play.
         </li>
         <li>
           <WikiLink id='S14' /> is the name of the game. Ideally, open cheap and
@@ -181,15 +185,15 @@ export default React.memo(function GuideNobleCoalition(props) {
           movement, and becomes a decent finisher.
         </li>
         <li>
-          I didn’t try <WikiLink id='S16' />, and I’m not sure they’re worth it.
-          5 mana is a lot in a Brawl so fast, and I’m pretty sure there are
-          better plays than that.{' '}
+          <WikiLink id='S16' /> might look like it’s interesting with 1 movement
+          for extra satyrs, but 5 mana is a lot in such a fast Brawl, and there
+          are better plays than that (usually 2 or 3 cards instead).
         </li>
         <li>
-          I ran <WikiLink id='N18' /> for a little while, and it does do good
-          damage but it’s just too expensive. If you have to drop it, you’re in
-          trouble, and if you’re in trouble, you can’t really spend 4 mana on
-          just that.
+          <WikiLink id='N18' /> sounds interesting in a Brawl where so many
+          satyrs are played but it’s just too expensive. If you have to drop it,
+          you’re in trouble, and if you’re in trouble, you can’t really spend 4
+          mana on just that.
         </li>
         <li>
           <WikiLink id='S28' /> are undeniably strong with their 1 movement, but
@@ -197,12 +201,8 @@ export default React.memo(function GuideNobleCoalition(props) {
           stage. I’m not sure it will help that much.
         </li>
         <li>
-          I have seen virtually no <WikiLink id='S21' /> for the same reason:
-          it‘s just too expensive, too little, too late.
-        </li>
-        <li>
-          Also seen no <WikiLink id='S20' /> for the same reasons: this is a
-          runner Brawl, so it‘s not that efficient.
+          <WikiLink id='S21' /> and <WikiLink id='S20' /> are just too expensive
+          here, and all in all too little too late.
         </li>
         <li>
           <WikiLink id='N9' /> might help in some situations, but what kills you
@@ -211,14 +211,9 @@ export default React.memo(function GuideNobleCoalition(props) {
           for 18 damage at turn 9.
         </li>
         <li>
-          I have seen a <WikiLink id='N44' /> + <WikiLink id='N29' /> in what I
-          can only assume was a heavy control deck, but it was a total fiasco
-          and they lost both games very quickly. It’s just too expensive.
-        </li>
-        <li>
-          I have seen a <WikiLink id='N13' /> but it’s definitely worse than a
-          Moonlit Aerie and performs incredibly poorly in that Brawl where
-          everything is cheap and everything moves.
+          <WikiLink id='N13' /> is definitely worse than Moonlit Aerie and
+          performs incredibly poorly in that Brawl where everything is cheap and
+          everything moves.
         </li>
         <li>
           <WikiLink id='N63' /> is not necessarily a bad choice, although it
