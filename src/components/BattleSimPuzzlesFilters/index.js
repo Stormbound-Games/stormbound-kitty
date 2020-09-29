8e34815e617b7fa04d5c440e7a2cc4e4e1ded937
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-29 21:45:13 +0200

    Remove incorrect puzzle difficult filters

--- src/components/BattleSimPuzzlesFilters/index.js
+++ src/components/BattleSimPuzzlesFilters/index.js
@@ -32,8 +32,6 @@ export default React.memo(function BattleSimPuzzlesFilters(props) {
             <option value='1'>1</option>
             <option value='2'>2</option>
             <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
           </select>
         </Column>
 
