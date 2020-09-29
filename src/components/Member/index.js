9ef1950b04ad6ad2dde5d9f0f1d3dfa3955d5f4e
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-29 22:07:53 +0200

    Capitalise user name

--- src/components/Member/index.js
+++ src/components/Member/index.js
@@ -91,15 +91,17 @@ export default React.memo(function Member(props) {
     ...userDonations.map(addType('DONATION')),
   ].sort((a, b) => b.date - a.date)
 
+  const displayName = capitalise(id)
+
   return (
-    <Article title={id}>
+    <Article title={displayName}>
       <Article.FullWidth>
         {content.length > 0 ? (
           <Row desktopOnly wideGutter>
             <Column width='1/3'>
               <p>
-                <span className='Highlight'>{id}</span> is a member of the
-                Stormbound community. Their contribution can be found below.
+                <span className='Highlight'>{displayName}</span> is a member of
+                the Stormbound community. Their contribution can be found below.
               </p>
               <ul className='Member__toc'>
                 <li>
@@ -133,8 +135,8 @@ export default React.memo(function Member(props) {
               </ul>
               {userDonations.length > 0 && (
                 <Info icon='heart' title='Financial contributor'>
-                  {capitalise(id)} is one of the generous contributors who can
-                  make Stormbound-Kitty a reality. Thank you and welcome to the{' '}
+                  {displayName} is one of the generous contributors who can make
+                  Stormbound-Kitty a reality. Thank you and welcome to the{' '}
                   <abbr title='Kitty Appreciation Team'>KAT</abbr>!
                 </Info>
               )}
@@ -156,17 +158,17 @@ export default React.memo(function Member(props) {
               className='Error__image'
             />
             <p>
-              No ‘{id}’ user could be found, or no content was associated to
-              that user. If you believe this is a bug, please report it to
-              Kitty#1909 on Discord.
+              No ‘{displayName}’ user could be found, or no content was
+              associated to that user. If you believe this is a bug, please
+              report it to Kitty#1909 on Discord.
             </p>
           </div>
         )}
       </Article.FullWidth>
 
       <PageMeta
-        title={id}
-        description={`Find all of ${id}’s contributions to Stormbound-Kitty such as stories, decks, puzzles or guides.`}
+        title={displayName}
+        description={`Find all of ${displayName}’s contributions to Stormbound-Kitty such as stories, decks, puzzles or guides.`}
       />
     </Article>
   )
