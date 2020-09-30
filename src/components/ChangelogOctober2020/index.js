be5e268d2d9cb17439f0f1cc05b20d4d8487fb03
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-30 15:41:25 +0200

    Add compensation to notes

--- src/components/ChangelogOctober2020/index.js
+++ src/components/ChangelogOctober2020/index.js
@@ -4,6 +4,7 @@ import Article from '../Article'
 import CardBuilderCardDisplay from '../CardBuilderCardDisplay'
 import Column from '../Column'
 import FAQSection from '../FAQSection'
+import Info from '../Info'
 import PageMeta from '../PageMeta'
 import { Coins, Rubies, Stones } from '../Resource'
 import Row from '../Row'
@@ -176,6 +177,22 @@ export default React.memo(function ChangelogOctober2020(props) {
           1/2/3/4/5 to 2/3/4/5/6).
         </li>
       </ul>
+
+      <Info icon='heart' title='Nerf compensation'>
+        <p>
+          Like last time, some compensation in the form of coins and fusion
+          stones will be provided to owners of Toxic Sacrifice and Booming
+          Professors, proportional to the level and rarity of the card. Find
+          below the reimbursement values for each rarity and level
+          (coins/stones).
+        </p>
+        <ul style={{ marginBottom: 0 }}>
+          <li>Common card: 0/0, 10/0, 20/1, 50/2, 120/5</li>
+          <li>Rare card: 0/0, 15/2, 30/3, 90/7, 190/10</li>
+          <li>Epic card: 0/0, 15/2, 40/5, 120/10, 250/20</li>
+          <li>Legendary card: 0/1, 20/5, 50/10, 150/20, 300/50</li>
+        </ul>
+      </Info>
 
       <Title id='friendly-matches-settings'>Friendly matches settings</Title>
 
