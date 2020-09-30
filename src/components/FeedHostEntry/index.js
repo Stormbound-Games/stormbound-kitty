05cd20022fa1c242bcc69c390f5472bfaee08cac
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-30 13:24:10 +0200

    Add co-hosts to the feed host entry

--- src/components/FeedHostEntry/index.js
+++ src/components/FeedHostEntry/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import FeedEntry from '../FeedEntry'
+import MemberList from '../MemberList'
 
 export default React.memo(function FeedHostEntry(props) {
   const name = props.hosts.find(u => u.toLowerCase() === props.user)
 
   return (
     <FeedEntry icon='users' date={props.date}>
-      {name} has organised {props.name}.
+      {name} has organised
+      {props.hosts.length > 1 ? (
+        <>
+          , alongside{' '}
+          <MemberList members={props.hosts.filter(winner => winner !== name)} />
+          ,
+        </>
+      ) : null}{' '}
+      {props.name}.
     </FeedEntry>
   )
 })
