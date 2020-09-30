7050f8ee7d29f74180220c687306c9115900692b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-30 11:04:27 +0200

    Add link to story category in feed

--- src/components/FeedStoryEntry/index.js
+++ src/components/FeedStoryEntry/index.js
@@ -4,11 +4,13 @@ import FeedEntry from '../FeedEntry'
 import { STORY_CATEGORIES } from '../../constants/stories'
 
 export default React.memo(function FeedStoryEntry(props) {
-  const category = STORY_CATEGORIES[props.category].shortName.toLowerCase()
+  const category = STORY_CATEGORIES[props.category].shortName
+  const prefix = /^[aeiou]/.test(category.toLowerCase()) ? 'an' : 'a'
 
   return (
     <FeedEntry icon='quill' date={props.date}>
-      {props.author} has written a {category} story called{' '}
+      {props.author} has written {prefix}{' '}
+      <Link to={'/stories/' + props.category}>{category}</Link> story called{' '}
       <Link to={'/story/' + props.id}>{props.title}</Link>.
       <blockquote>{props.content}</blockquote>
     </FeedEntry>
