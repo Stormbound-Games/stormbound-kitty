d3a6deaf811a09863473414162034f948193fbf4
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-09-30 10:39:07 +0200

    Replace recommended deck with Gazette when available

--- src/components/BrawlTracker/index.js
+++ src/components/BrawlTracker/index.js
@@ -2,16 +2,23 @@ import React from 'react'
 import BrawlMilestones from '../BrawlMilestones'
 import BrawlMatches from '../BrawlMatches'
 import Column from '../Column'
+import { BrawlContext } from '../BrawlProvider'
 import BrawlCharts from '../BrawlCharts'
 import BrawlOutcome from '../BrawlOutcome'
 import BrawlRecommendedDecks from '../BrawlRecommendedDecks'
+import { BRAWL_DATA } from '../BrawlIndex'
 import BrawlReset from '../BrawlReset'
 import BrawlSetup from '../BrawlSetup'
+import GuideTeaser from '../GuideTeaser'
 import Row from '../Row'
 import Title from '../Title'
+import guides from '../../data/guides'
 
 export default React.memo(function BrawlTracker(props) {
   const [setup, setSetup] = React.useState('MOBILE_WITHOUT_ADS')
+  const { brawl: currentBrawl } = React.useContext(BrawlContext)
+  const { title } = BRAWL_DATA.find(brawl => brawl.id === currentBrawl.id) || {}
+  const guide = guides.find(guide => guide.name === title)
 
   return (
     <div>
@@ -22,7 +29,11 @@ export default React.memo(function BrawlTracker(props) {
           <BrawlOutcome setup={setup} />
           <BrawlSetup setup={setup} setSetup={setSetup} />
           <BrawlReset />
-          <BrawlRecommendedDecks limit={1} columns={1} />
+          {guide ? (
+            <GuideTeaser {...guide} />
+          ) : (
+            <BrawlRecommendedDecks limit={1} columns={1} />
+          )}
         </Column>
         <Column width='2/3'>
           <BrawlMatches />
