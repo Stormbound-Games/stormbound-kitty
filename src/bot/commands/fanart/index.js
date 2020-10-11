8bf886c7ea8d2ab8c6c1c08faeb8071e26769458
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-10-11 13:34:19 +0200

    Add a fanart command

---
+++ src/bot/commands/fanart/index.js
@@ -0,0 +1,31 @@
+import getEmbed from '../../../helpers/getEmbed'
+import arrayRandom from '../../../helpers/arrayRandom'
+import artworks from '../../../data/art'
+
+export default {
+  command: 'fanart',
+  label: '👩‍🎨  Fan-art',
+  help: function () {
+    return getEmbed()
+      .setTitle(`${this.label}: help`)
+      .setURL('https://stormbound-kitty.com/fan-art')
+      .setDescription(
+        `Get a random fan-art from the community. For instance, \`!${this.command}\`.`
+      )
+  },
+  handler: function (message) {
+    const embed = getEmbed()
+    const { image, author, date } = arrayRandom(artworks)
+
+    embed
+      .setTitle(`${this.label}`)
+      .setURL('https://stormbound-kitty.com/member/' + author)
+      .addFields([
+        { name: 'author', value: author, inline: true },
+        { name: 'date', value: date, inline: true },
+      ])
+      .setImage('https://stormbound-kitty.com/assets/images/art/' + image)
+
+    return embed
+  },
+}
