f731951c499568932c087559557ab76609488c83
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-10-12 14:36:48 +0200

    Make ‘useLazyLoad’ a little more performant

--- src/hooks/useLazyLoad.js
+++ src/hooks/useLazyLoad.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import { useInView } from 'react-intersection-observer'
 
+const LOADING_DURATION = 350
+
 const useLazyLoad = (collection, itemsPerPage, automatic = true) => {
   const [ref, inView] = useInView()
   const [loading, setLoading] = React.useState(false)
   const [page, setPage] = React.useState(1)
+  const canLoadMore =
+    collection.length && page * itemsPerPage < collection.length
   const loadMore = React.useCallback(() => {
-    if (collection.length && page * itemsPerPage < collection.length) {
+    if (canLoadMore) {
       setLoading(true)
       setTimeout(() => {
         setPage(page => page + 1)
         setLoading(false)
-      }, 500)
+      }, LOADING_DURATION)
     }
-  }, [collection.length, itemsPerPage, page])
+  }, [canLoadMore])
 
   React.useEffect(() => {
     if (inView && automatic) loadMore()
-  }, [automatic, inView, loadMore])
+  }, [inView, automatic, loadMore])
 
   return {
     items: collection.slice(0, page * itemsPerPage),
     loading,
-    loadMore: page * itemsPerPage < collection.length ? loadMore : undefined,
+    loadMore: canLoadMore ? loadMore : undefined,
     ref,
   }
 }
