d1699ca1129c3c3d35b62b490103a2d34aaa1ea4
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-10-20 18:03:10 +0200

    Fix broken links

--- src/components/FeedStoryEntry/index.js
+++ src/components/FeedStoryEntry/index.js
@@ -11,7 +11,7 @@ export default React.memo(function FeedStoryEntry(props) {
     <FeedEntry icon='quill' date={props.date}>
       {props.author} has written {prefix}{' '}
       <Link to={'/stories/' + props.category}>{category}</Link> story called{' '}
-      <Link to={'/story/' + props.id}>{props.title}</Link>.
+      <Link to={'/stories/' + props.id}>{props.title}</Link>.
       <blockquote>{props.content}</blockquote>
     </FeedEntry>
   )
