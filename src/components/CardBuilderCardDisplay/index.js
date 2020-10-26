a2b65b3912383b779263ccf37eeabb5af9396b04
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-10-26 12:40:09 +0100

    Extract ‘isLevelAvailable’

--- src/components/CardBuilderCardDisplay/index.js
+++ src/components/CardBuilderCardDisplay/index.js
@@ -1,39 +1,14 @@
 import React from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import { CollectionContext } from '../CollectionProvider'
-import { RARITY_COPIES } from '../../constants/game'
 import Card from '../Card'
 import Column from '../Column'
 import CTA from '../CTA'
 import Only from '../Only'
 import Row from '../Row'
 import getRawCardData from '../../helpers/getRawCardData'
+import isLevelAvailable from '../../helpers/isLevelAvailable'
 import './index.css'
-
-const isLevelAvailable = (card, level) => {
-  // If the card is missing from the collection, it means there are 0 extra
-  // copies which means no level can be highlighted. Similarly, if the card is
-  // already level 5, we can short-circuit the function.
-  if (card.missing || card.level === 5) return false
-
-  // Begin with all levels not being available, either because they have been
-  // reached already, or because we don’t know yet whether they can be.
-  const isLevelAvailable = [false, false, false, false, false]
-  const { rarity } = getRawCardData(card.id)
-  let { copies } = card
-
-  // Go from the current card level (from 1 to 4) to 5, and check if we have
-  // enough copies to reach the next missing level; deduct the number of copies
-  // used for each level.
-  for (let i = card.level; i < 5; i++) {
-    let copiesForNextLevel = RARITY_COPIES[rarity].copies[i - 1]
-
-    isLevelAvailable[i] = copies - copiesForNextLevel >= 0
-    copies -= copiesForNextLevel
-  }
-
-  return isLevelAvailable[level - 1]
-}
 
 export default React.memo(function CardBuilderCardDisplay(props) {
   const match = useRouteMatch()
---
+++ src/helpers/isLevelAvailable/index.js
@@ -0,0 +1,29 @@
+import { RARITY_COPIES } from '../../constants/game'
+import getRawCardData from '../getRawCardData'
+
+const isLevelAvailable = (card, level) => {
+  // If the card is missing from the collection, it means there are 0 extra
+  // copies which means no level can be highlighted. Similarly, if the card is
+  // already level 5, we can short-circuit the function.
+  if (card.missing || card.level === 5) return false
+
+  // Begin with all levels not being available, either because they have been
+  // reached already, or because we don’t know yet whether they can be.
+  const isLevelAvailable = [false, false, false, false, false]
+  const { rarity } = getRawCardData(card.id)
+  let { copies } = card
+
+  // Go from the current card level (from 1 to 4) to 5, and check if we have
+  // enough copies to reach the next missing level; deduct the number of copies
+  // used for each level.
+  for (let i = card.level; i < 5; i++) {
+    let copiesForNextLevel = RARITY_COPIES[rarity].copies[i - 1]
+
+    isLevelAvailable[i] = copies - copiesForNextLevel >= 0
+    copies -= copiesForNextLevel
+  }
+
+  return isLevelAvailable[level - 1]
+}
+
+export default isLevelAvailable
