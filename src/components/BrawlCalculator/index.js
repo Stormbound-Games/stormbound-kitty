36527125109de16e5bc163dccc850e644edb9279
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-10-31 11:12:09 +0100

    Set up basic layout for the Brawl calculator

--- src/components/BrawlCalculator/index.js
+++ src/components/BrawlCalculator/index.js
@@ -1,5 +1,35 @@
 import React from 'react'
+import Column from '../Column'
+import HeaderBanner from '../HeaderBanner'
+import PageMeta from '../PageMeta'
+import Row from '../Row'
+import Title from '../Title'
 
 export default React.memo(function BrawlCalculator(props) {
-  return 'Brawl calculator'
+  return (
+    <>
+      <HeaderBanner title='Brawl Calculator' />
+      <Row desktopOnly wideGutter>
+        <Column width='1/3'>
+          <Title>What is this</Title>
+          <p>
+            This is a calculator to plan your Brawl journey. Whether you have
+            limited funds and would like to know how far you can go, or have an
+            objective in mind and would like to know how much it will cost, this
+            calculator is made for you.
+          </p>
+        </Column>
+        <Column width='1/3'>
+          <Title>Settings</Title>
+        </Column>
+        <Column width='1/3'>
+          <Title>Outcome</Title>
+        </Column>
+      </Row>
+      <PageMeta
+        title='Brawl Calculator'
+        description='Find a calculator for making the best out of the Brawl based on your current savins or your objectives'
+      />
+    </>
+  )
 })
