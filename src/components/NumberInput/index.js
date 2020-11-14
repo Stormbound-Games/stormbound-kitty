9c0594ee9f62b3aa812a052af2712a2118605626
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-11-14 15:27:26 +0100

    Make the NumberInput component uncontrolled

--- src/components/NumberInput/index.js
+++ src/components/NumberInput/index.js
@@ -1,52 +1,48 @@
 import React from 'react'
 
 export default React.memo(function NumberInput(props) {
-  const [value, setValue] = React.useState(props.value || '')
-  const { onChange, onKeyDown } = props
+  const { value, onChange, onKeyDown } = props
   const max = typeof props.max === 'number' ? props.max : Infinity
   const min = typeof props.min === 'number' ? props.min : -Infinity
   const step = typeof props.step === 'number' ? props.step : 1
 
-  // If the boundaries are updated, clamp the current value between them.
+  // If the boundaries (or the value) are updated, clamp the value between them.
   React.useEffect(() => {
-    if (typeof value === 'number' && max < value) setValue(max)
-    if (typeof value === 'number' && min > value) setValue(min)
-  }, [min, max, value])
-
-  // When the value gets updated, fire the `onChange` callback.
-  React.useEffect(() => {
-    if (typeof onChange === 'function') onChange(value)
-  }, [onChange, value])
+    if (typeof value === 'number' && max < value) onChange(max)
+    if (typeof value === 'number' && min > value) onChange(min)
+  }, [min, max, value, onChange])
 
   const handleChange = React.useCallback(
     event => {
       const value = Number(event.target.value)
 
-      // If the value would result in no longer being a number, or being above the
-      // maximum, or being below the mininum, do not update it.
+      // If the value is not empty and not a number, or above the maximum, or
+      // below the mininum, do not update it.
       if (event.target.value && (isNaN(value) || value < min || value > max))
         return
 
       // If the field is empty, let it be empty and do not force a 0, which cannot
       // be removed and cause confusion.
-      setValue(event.target.value ? value : '')
+      onChange(event.target.value ? value : '')
     },
-    [min, max]
+    [min, max, onChange]
   )
 
   // If pressing the up arrow while being below the max, bump the value by 1
   // step; same for down.
   const handleKeyDown = React.useCallback(
     event => {
+      const value = Number(event.target.value)
+
       if (event.keyCode === 38 && value < max) {
-        setValue(value => Math.min(value + step, max))
+        onChange(Math.min(value + step, max))
       } else if (event.keyCode === 40 && value > min) {
-        setValue(value => Math.max(value - step, min))
+        onChange(Math.max(value - step, min))
       }
 
       if (typeof onKeyDown === 'function') onKeyDown(event)
     },
-    [min, max, step, value, onKeyDown]
+    [min, max, step, onChange, onKeyDown]
   )
 
   return (
