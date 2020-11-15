8387e61d53abe1debc23b735c4e9ff67b1210ebb
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-11-15 12:57:19 +0100

    Simplify FAQ ToC

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -4,33 +4,10 @@ import FAQSection from '../FAQSection'
 import PageMeta from '../PageMeta'
 import StructuredData from '../StructuredData'
 import Title from '../Title'
-import TogglableContent from '../TogglableContent'
 import categories from '../../data/faq'
 import './index.css'
 
 export default React.memo(function FAQ() {
-  const [expanded, setExpanded] = React.useState([])
-  const toggle = id =>
-    expanded.includes(id)
-      ? setExpanded(ids => ids.filter(i => i !== id))
-      : setExpanded(ids => [...ids, id])
-
-  React.useEffect(() => {
-    if (!window.location.hash) return
-
-    const requestedId = window.location.hash.slice(1)
-    const category = categories.find(
-      cat =>
-        cat.id === requestedId ||
-        cat.entries.find(entry => entry.id === requestedId)
-    )
-
-    if (category && !expanded.includes(category.id)) {
-      setExpanded(ids => [...ids, category.id])
-    }
-    // eslint-disable-next-line
-  }, [])
-
   return (
     <Article title='FAQ'>
       <Article.Narrow>
@@ -40,28 +17,13 @@ export default React.memo(function FAQ() {
           {categories.map(category => (
             <li key={category.id}>
               <a href={'#' + category.id}>{category.title}</a>
-              <TogglableContent
-                isExpanded={expanded.includes(category.id)}
-                id={category.id + '-questions'}
-                renderToggle={toggleProps => (
-                  <button
-                    className='ButtonAsLink FAQ__toggle'
-                    onClick={() => toggle(category.id)}
-                  >
-                    (
-                    {expanded.includes(category.id) ? '- collapse' : '+ expand'}
-                    )
-                  </button>
-                )}
-              >
-                <ul className='FAQ__toc-list'>
-                  {category.entries.map(entry => (
-                    <li key={entry.id}>
-                      <a href={'#' + entry.id}>{entry.question}</a>
-                    </li>
-                  ))}
-                </ul>
-              </TogglableContent>
+              <ul className='FAQ__toc-list'>
+                {category.entries.map(entry => (
+                  <li key={entry.id}>
+                    <a href={'#' + entry.id}>{entry.question}</a>
+                  </li>
+                ))}
+              </ul>
             </li>
           ))}
         </ul>
