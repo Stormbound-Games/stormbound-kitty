a2f270b1391344e286e806f55e417629c9bdf42d
SimonC <sd.castagna@gmail.com>
2020-12-15 14:00:51 +0100

    Created deck advice and test for booming professors (#233)
    * created deck advice and t test for booming professors

---
+++ src/helpers/getDeckAdvice/advice/BOOMING_PROFESSORS/index.js
@@ -0,0 +1,15 @@
+export default cards => {
+  const cardIds = cards.map(card => card.id)
+  const hasBoomingProfessors = cardIds.includes('I28')
+  const hasDestructobots = cardIds.includes('I1')
+
+  // Destructobots synergizes well with Booming Professors.
+  if (!hasBoomingProfessors || hasDestructobots) return null
+
+  return {
+    name: 'Suboptimal Booming Professors',
+    description:
+      'This deck includes Booming Professors but not Destructobots, which it synergizes with well. Consider including Destructobots in your deck.',
+    highlight: ['I28'],
+  }
+}
