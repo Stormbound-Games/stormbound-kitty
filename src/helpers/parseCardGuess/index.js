e3c1582cc863e048cafed88df5691e31de3d8cfc
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-12-21 13:02:38 +0100

    Add support for multi-factions in advanced search

--- src/helpers/parseCardGuess/index.js
+++ src/helpers/parseCardGuess/index.js
@@ -9,7 +9,8 @@ const parseCardGuess = message => {
   if (message === 'hero') return ['hero', true]
   if (message === 'elder') return ['elder', true]
   if (Object.keys(RARITIES).includes(message)) return ['rarity', message]
-  if (Object.keys(FACTIONS).includes(message)) return ['faction', message]
+  if (message.split(',').every(chunk => Object.keys(FACTIONS).includes(chunk)))
+    return ['faction', message]
   if (Object.keys(TYPES).includes(message)) return ['type', message]
   if (Object.keys(RACES).includes(message)) return ['race', message]
   const [key, value] = handleSearchAlias(message)
