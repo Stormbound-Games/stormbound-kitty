c41df9bdcdad2f0146d80c1920782967519e518f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2020-12-25 12:30:18 +0100

    Fix broken test

--- cypress/integration/dryRunner/cards/QueenOfHerds.js
+++ cypress/integration/dryRunner/cards/QueenOfHerds.js
@@ -1,6 +1,6 @@
 import s from '../selectors'
 
-const DECK_ID = '5n15n25n35n235n45n55n65n625n635n675s35s21'
+const DECK_ID = '5xn1n2s1n3n23n4n5n6n62n67n63s21'
 
 describe('Dry-runner — Queen of Herds', () => {
   it('should be possible to play satyrs with Queen of Herds', () => {
@@ -10,21 +10,17 @@ describe('Dry-runner — Queen of Herds', () => {
 
       .drEndTurn(7)
 
-      .drSetRNG('FRIENDLY')
-
       .drPlay('S21')
-      .drPlay('S3')
 
       .get(s.CARD_LOG_IMAGE)
-      .should('have.length', 4)
+      .should('have.length', 3)
   })
 
   it('should not be possible to play satyrs from the hand with Queen of Herds', () => {
-    const HAND = ['S21', 'N1', 'N2', 'S3']
+    const HAND = ['S21', 'N1', 'N2', 'S1']
     cy.visit(`/deck/${DECK_ID}/dry-run?mode=MANUAL`)
       .drDrawHand(HAND)
       .drEndTurn(7)
-      .drSetRNG('FRIENDLY')
 
       .drPlay('S21')
 
