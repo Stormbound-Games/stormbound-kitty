a9767968656933d0466879ec32f1b0a6d3414a3f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-01-14 16:27:40 +0100

    Add release notes for Temple of Focus

---
+++ src/components/ReleaseNotesTempleOfFocus/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Article from '../Article'
+import CardBuilderCardDisplay from '../CardBuilderCardDisplay'
+import Notice from '../Notice'
+import ReleaseNotes from '../ReleaseNotes'
+import getInitialCardData from '../../helpers/getInitialCardData'
+
+export default React.memo(function ReleaseNotesTempleOfFocus(props) {
+  return (
+    <ReleaseNotes id='temple_of_focus'>
+      <Article.Narrow>
+        <p>
+          On January 17th, a new card will make its entrance. It’s a structure
+          from Swarm of the East, it’s epic, and it’s a bit of a new mechanic
+          while still making sense in the context of the overall faction!
+        </p>
+      </Article.Narrow>
+
+      <Article.Embed>
+        <CardBuilderCardDisplay {...getInitialCardData('S29')} />
+      </Article.Embed>
+
+      <Article.Narrow>
+        <Notice>
+          <span className='Highlight'>Meow</span> and happy fighting, fellow
+          Stormbounders!
+        </Notice>
+      </Article.Narrow>
+    </ReleaseNotes>
+  )
+})
--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -41,6 +41,7 @@ const GUIDE_COMPONENTS = {
 }
 
 const RELEASE_COMPONENTS = {
+  temple_of_focus: load('ReleaseNotesTempleOfFocus'),
   ios_01_2021: load('ReleaseNotesIOSJanuary2021'),
   end_of_2020: load('ReleaseNotesEndOf2020'),
   '12_2020': load('ReleaseNotesDecember2020'),
