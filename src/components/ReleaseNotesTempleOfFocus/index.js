0049397a03cf2b2ad66215542fdcd6e736b4be3d
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-01-14 19:30:05 +0100

    Add promotion pack

--- src/components/ReleaseNotesTempleOfFocus/index.js
+++ src/components/ReleaseNotesTempleOfFocus/index.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Article from '../Article'
 import CardLink from '../CardLink'
 import CardBuilderCardDisplay from '../CardBuilderCardDisplay'
+import Image from '../Image'
 import Notice from '../Notice'
 import ReleaseNotes from '../ReleaseNotes'
+import { Coins, Stones } from '../Resource'
 import getInitialCardData from '../../helpers/getInitialCardData'
 
 export default React.memo(function ReleaseNotesTempleOfFocus(props) {
@@ -24,6 +26,17 @@ export default React.memo(function ReleaseNotesTempleOfFocus(props) {
       </Article.Embed>
 
       <Article.Narrow>
+        <p>
+          Additionally, there will be a promotion pack to put your hands on this
+          sweet new card. It costs $9.99, grants 3 copies of{' '}
+          <CardLink id='S29' />, <Stones amount={5} /> and{' '}
+          <Coins amount={750} />. It will only be available during January 17th
+          and January 24th, so be sure to consider it!
+        </p>
+        <Image
+          src='/assets/images/releases/promotion_temple_of_focus.png'
+          alt='3 copies of Temple of Focus + 5 Fusion Stones + 750 coins for $9.99'
+        />
         <Notice>
           <span className='Highlight'>Meow</span> and happy fighting, fellow
           Stormbounders!
