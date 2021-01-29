c86b4d214b57419e51fe7f993c94416436fcbd5b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-01-29 15:15:41 +0100

    Fix a broken test

--- src/helpers/getAverageStonesPerBook/spec.js
+++ src/helpers/getAverageStonesPerBook/spec.js
@@ -6,7 +6,7 @@ describe('The `getAverageStonesPerBook` helper', () => {
     HEROIC: 2.321590909091104,
     CLASSIC: 0.862800802140173,
     NOBLE: 0.4314004010695419,
-    HUMBLE: 0.4314004010695419,
+    HUMBLE: 0.14380013368984246,
   }
 
   Object.keys(FUSION_STONES).forEach(bookType => {
