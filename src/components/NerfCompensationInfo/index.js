e5814247f7ce0effd8c5d3e72bcacd9815f07667
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-02-22 13:39:31 +0100

    Add rarity icon in nerf info

--- src/components/NerfCompensationInfo/index.js
+++ src/components/NerfCompensationInfo/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Info from '../Info'
+import ResourceIcon from '../ResourceIcon'
 
 export default React.memo(function NerfCompensationInfo(props) {
   return (
@@ -11,10 +12,22 @@ export default React.memo(function NerfCompensationInfo(props) {
         rarity and level (coins/stones).
       </p>
       <ul style={{ marginBottom: 0 }}>
-        <li>Common card: 0/0, 10/0, 20/1, 50/2, 120/5</li>
-        <li>Rare card: 0/0, 15/2, 30/3, 90/7, 190/10</li>
-        <li>Epic card: 0/0, 15/2, 40/5, 120/10, 250/20</li>
-        <li>Legendary card: 0/1, 20/5, 50/10, 150/20, 300/50</li>
+        <li>
+          <ResourceIcon resource='COMMON' />
+          &nbsp; Common card: 0/0, 10/0, 20/1, 50/2, 120/5
+        </li>
+        <li>
+          <ResourceIcon resource='RARE' />
+          &nbsp; Rare card: 0/0, 15/2, 30/3, 90/7, 190/10
+        </li>
+        <li>
+          <ResourceIcon resource='EPIC' />
+          &nbsp; Epic card: 0/0, 15/2, 40/5, 120/10, 250/20
+        </li>
+        <li>
+          <ResourceIcon resource='LEGENDARY' />
+          &nbsp; Legendary card: 0/1, 20/5, 50/10, 150/20, 300/50
+        </li>
       </ul>
     </Info>
   )
