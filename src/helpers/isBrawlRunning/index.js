deba96a3d2300be06d0016876826c27927023aae
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-03-14 12:08:38 +0100

    Fix an issue with ‘isBrawlRunning’

--- src/helpers/isBrawlRunning/index.js
+++ src/helpers/isBrawlRunning/index.js
@@ -1,20 +1,21 @@
-const isBrawlRunning = () => {
-  const now = new Date()
-  const dayOfTheWeek = now.getDay()
-  const hours = now.getHours()
+const isBrawlRunning = (date = new Date()) => {
+  const dayOfTheWeek = date.getDay()
+  const hours = date.getHours()
 
   switch (dayOfTheWeek) {
+    // The Brawl ends at 7AM UTC on Monday.
     case 1:
+      return hours < 7
     case 2:
     case 3:
       return false
     case 5:
     case 6:
-      return true
-    case 4:
-      return hours >= 9
     case 0:
-      return hours < 10
+      return true
+    // The Brawl starts at 7AM UTC on Thursday.
+    case 4:
+      return hours >= 7
     default:
       return false
   }
