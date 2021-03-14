eefe5faf27b03b2987821062621a4534ab58fe5a
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-03-14 12:09:19 +0100

    Fix an issue with ‘getCurrentBrawl’

--- src/helpers/getCurrentBrawl/index.js
+++ src/helpers/getCurrentBrawl/index.js
@@ -1,11 +1,11 @@
 import { BRAWLS, CYCLE_START } from '../../constants/brawl'
 
 function weeksBetween(d1, d2) {
-  return Math.round((d2 - d1) / (7 * 24 * 60 * 60 * 1000))
+  return Math.floor((d2 - d1) / (7 * 24 * 60 * 60 * 1000))
 }
 
-const getCurrentBrawl = () => {
-  const weeks = weeksBetween(CYCLE_START, Date.now())
+const getCurrentBrawl = (date = new Date()) => {
+  const weeks = weeksBetween(CYCLE_START, date)
   const index = weeks % BRAWLS.length
 
   return BRAWLS[index] || BRAWLS[0]
