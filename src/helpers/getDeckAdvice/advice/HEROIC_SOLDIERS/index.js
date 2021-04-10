ad348913200c49addd31898c32607d01a07d5f83
NightlyHerb <82109216+NightlyHerb@users.noreply.github.com>
2021-04-11 00:05:28 +0900

    Add HEROIC_SOLDIERS deck advice
    Solves issue #202.
    HEROIC_SOLDIERS deck advice basically recommends Fluffy Badboxers
    when you have a Heroic Soldiers in your deck.

---
+++ src/helpers/getDeckAdvice/advice/HEROIC_SOLDIERS/index.js
@@ -0,0 +1,14 @@
+export default cards => {
+  const cardIds = cards.map(card => card.id)
+  const hasHeroicSoldiers = cardIds.includes('N32')
+  const hasFluffyBadboxers = cardIds.includes('N60')
+
+  // Heroic Soldiers can often be replaced with Fluffy Badboxers
+  if (!hasHeroicSoldiers || hasFluffyBadboxers) return null
+
+  return {
+    name: 'Use of Heroic Soldiers',
+    description: 'Consider replacing Heroic Soldiers with Fluffy Badboxers if you have it with the same or more strength.',
+    highlight: ['N32'],
+  }
+}
---
+++ src/helpers/getDeckAdvice/advice/HEROIC_SOLDIERS/spec.js
@@ -0,0 +1,26 @@
+import advice from '.'
+import getResolvedCardData from '../../../getResolvedCardData'
+import serialisation from '../../../serialisation'
+import modifyDeck from '../../../modifyDeck'
+
+const getCards = (id, modifier = 'NONE') =>
+  modifyDeck(serialisation.deck.deserialise(id), modifier).map(
+    getResolvedCardData
+  )
+
+describe('The `HEROIC_SOLDIERS` advice', () => {
+  it('should not be returned if it doesn’t have Heroic Soldiers', () => {
+    const cards = getCards('1xn1n2n3n13n23n16n19n28n30n82n72n54')
+    expect(advice(cards)).to.equal(null)
+  })
+
+  it('should not be returned if it has Fluffy Badboxers', () => {
+    const cards = getCards('1xn1n2n3n13n23n16n19n28n30n60n72n54')
+    expect(advice(cards)).to.equal(null)
+  })
+
+  it('should be returned if it has Heroic Soldiers but not Fluffy Badboxers', () => {
+    const cards = getCards('1xn1n2n3n13n23n16n19n28n30n32n72n54')
+    expect(advice(cards)).to.not.equal(null)
+  })
+})
