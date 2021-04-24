e99e2d2d89734e016d93be1910e6985570d43b1d
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-04-24 12:06:49 +0200

    Add release to navigation

--- src/components/Header/useNavigation.js
+++ src/components/Header/useNavigation.js
@@ -38,15 +38,15 @@ export default () => {
           icon: 'bullhorn',
           items: [
             {
-              label: 'Update 04-2021',
-              to: '/releases/04-2021',
-              id: '04_2021',
+              label: 'Update 05-2021',
+              to: '/releases/05-2021',
+              id: '05_2021',
               new: true,
             },
             {
-              label: 'Update 03-2021',
-              to: '/releases/03-2021',
-              id: '03_2021',
+              label: 'Update 04-2021',
+              to: '/releases/04-2021',
+              id: '04_2021',
             },
             {
               label: 'All Releases',
