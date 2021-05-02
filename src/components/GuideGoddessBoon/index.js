73f3dde54bcb13bf3995bdd1f19f651aba782516
Simon Castagna <sd.castagna@gmail.com>
2021-05-02 14:20:50 +0200

    Add Info box directing users to community decks in brawl guides

--- src/components/GuideGoddessBoon/index.js
+++ src/components/GuideGoddessBoon/index.js
@@ -48,6 +48,14 @@ export default React.memo(function GuideGoddessBoon(props) {
         provide a lot of value against any lumbering snow titans.
       </p>
 
+      <Info icon='eye' title='Brawl Decks'>
+        <p>
+          Check the{' '}<Link to='/deck/suggestions?category=BRAWL&brawl=FROSTLING_STRENGTH'>
+          community-provided decks</Link> for this brawl. You might find a deck that 
+          suits you, or that you can base your own creation on.
+        </p>
+      </Info>
+
       <Title>Possible Decks</Title>
 
       <Guide.FullWidth padding='60px'>
