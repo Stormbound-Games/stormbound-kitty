3ead24a8b9f93506871674853fbca9de58d55183
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-05-22 12:35:19 +0200

    Make getVictoryCoins helper consider leagues

--- src/helpers/getVictoryCoins/index.js
+++ src/helpers/getVictoryCoins/index.js
@@ -1,10 +1,33 @@
-const getVictoryCoins = setup => {
+const getBaseCoinsPerLeague = league => {
+  switch (league) {
+    case 'HEROES':
+      return 25
+    case 'DIAMOND':
+      return 20
+    case 'PLATINUM':
+      return 15
+    case 'GOLD':
+    case 'SILVER':
+    case 'BRONZE':
+    case 'IRON':
+      return 10
+    default:
+      return 0
+  }
+}
+
+const getVictoryCoins = (setup, league) => {
   switch (setup) {
     case 'MOBILE_WITHOUT_ADS':
-      return 5
+      // @TODO: remove ternary when `league` is being provided in every single
+      // call of this helper.
+      return league ? getBaseCoinsPerLeague(league) : 5
     case 'MOBILE_WITH_ADS':
-      return 20
+      // @TODO: remove ternary when `league` is being provided in every single
+      // call of this helper.
+      return league ? getBaseCoinsPerLeague(league) * 2 : 20
     case 'STEAM_OR_WEB':
+      // @TODO: figure out what are the values for Steam based on the league.
       return 10
     case 'NONE':
       return 0
--- src/helpers/getVictoryCoins/spec.js
+++ src/helpers/getVictoryCoins/spec.js
@@ -3,10 +3,28 @@ import getVictoryCoins from './'
 describe('The `getVictoryCoins` helper', () => {
   it('should handle MOBILE_WITHOUT_ADS', () => {
     expect(getVictoryCoins('MOBILE_WITHOUT_ADS')).to.equal(5)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'STARTER')).to.equal(10)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'IRON')).to.equal(10)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'BRONZE')).to.equal(10)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'SILVER')).to.equal(10)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'GOLD')).to.equal(10)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'PLATINUM')).to.equal(15)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'DIAMOND')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITHOUT_ADS', 'HEROES')).to.equal(25)
   })
+
   it('should handle MOBILE_WITH_ADS', () => {
     expect(getVictoryCoins('MOBILE_WITH_ADS')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'STARTER')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'IRON')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'BRONZE')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'SILVER')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'GOLD')).to.equal(20)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'PLATINUM')).to.equal(30)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'DIAMOND')).to.equal(40)
+    expect(getVictoryCoins('MOBILE_WITH_ADS', 'HEROES')).to.equal(50)
   })
+
   it('should handle STEAM_OR_WEB', () => {
     expect(getVictoryCoins('STEAM_OR_WEB')).to.equal(10)
   })
