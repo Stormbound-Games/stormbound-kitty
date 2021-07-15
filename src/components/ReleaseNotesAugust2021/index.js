5e4450ce63f62b7f4972183168d5be44c8e5aadf
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-07-15 16:18:31 +0200

    Add Brawl bonuses explanations

--- src/components/ReleaseNotesAugust2021/index.js
+++ src/components/ReleaseNotesAugust2021/index.js
@@ -4,16 +4,22 @@ import Article from '../Article'
 import CardLink from '../CardLink'
 import CardBuilderCardDisplay from '../CardBuilderCardDisplay'
 import FAQSection from '../FAQSection'
+import Guide from '../Guide'
 import Image from '../Image'
 import Info from '../Info'
 import NerfCompensationInfo from '../NerfCompensationInfo'
 import ReleaseNotes from '../ReleaseNotes'
+import Row from '../Row'
 import { Coins, Rubies, Stones } from '../Resource'
 import ResourceIcon from '../ResourceIcon'
 import Title from '../Title'
-import getInitialCardData from '../../helpers/getInitialCardData'
+import useViewportSize from '../../hooks/useViewportSize'
+import './index.css'
 
 export default React.memo(function ReleaseNotesAugust2021(props) {
+  const { viewportWidth } = useViewportSize()
+  const CardsContainer = viewportWidth >= 700 ? Guide.FullWidth : React.Fragment
+
   return (
     <ReleaseNotes id='08_2021'>
       <Article.Narrow>
@@ -26,6 +32,9 @@ export default React.memo(function ReleaseNotesAugust2021(props) {
         <ol style={{ columns: '16em' }}>
           <li>
             <a href='#balance-changes'>Balance changes</a>
+          </li>
+          <li>
+            <a href='#brawl-bonuses'>Brawl bonuses</a>
           </li>
           <li>
             <a href='#faq'>FAQ</a>
@@ -87,6 +96,150 @@ export default React.memo(function ReleaseNotesAugust2021(props) {
         </p>
 
         <NerfCompensationInfo title='Compensation' ids={['N77']} />
+
+        <Title id='brawl-bonuses'>Brawl bonuses</Title>
+        <p>
+          The “hearts” counter has arguably worsen the Brawl experience for
+          players. This is because it should have been part of a broader update
+          around the Brawl progression system, which has been split in half
+          (regrettably so). The second part of that new system will be
+          introduced in August in the form of Brawl winning bonuses.
+        </p>
+
+        <Info icon='gift' title='Compensation'>
+          <p>
+            To apologize for the recent sub-par experience with the Brawl, every
+            player logging into the game before the 29th of July will be
+            rewarded with <Coins amount={500} />. Additionally, the Brawl
+            starting on July 29th will be discounted 10% for everyone, and 20%
+            for Premium Pass holders.
+          </p>
+        </Info>
+
+        <Row desktopOnly>
+          <Row.Column>
+            <p>
+              The idea is that after <span className='Highlight'>every</span>{' '}
+              victory in Brawl, the player will be offered one of two random
+              bonuses, amongst a set of 9 different possible bonuses (see the
+              complete list below). Some of them will provide resources while
+              some will improve the player’s livelihood in Brawl by redeeming
+              and refilling hearts.
+            </p>
+            <p>
+              A player begins a Brawl tier with 3 full green hearts (just like
+              it is right now). When losing a match, a heart gets emptied (red).
+              If all hearts are empty, then the player’s crowns get reset to the
+              beginning of the current milestone. Many of the bonuses will help
+              the player not getting to that point though.
+            </p>
+          </Row.Column>
+          <Row.Column>
+            <img
+              style={{ marginTop: 0 }}
+              src='/assets/images/releases/brawl_bonuses_choice.jpg'
+              alt=''
+            />
+          </Row.Column>
+        </Row>
+
+        <CardsContainer>
+          <div className='ReleaseNotesAugust2021__cards'>
+            <Image
+              src='/assets/images/iconography/brawl_COINS.png'
+              alt='Coins bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_RUBIES.png'
+              alt='Rubies bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_FUSION_STONES.png'
+              alt='Fusion stones bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_FORTRESS_LEVEL.png'
+              alt='Fortress level bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_LIFE_UP.png'
+              alt='Life up bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_ALL_LIVES_UP.png'
+              alt='All lives up'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_IRON_SLOT.png'
+              alt='Iron slot bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_GOLD_SLOT.png'
+              alt='Gold slot bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+            <Image
+              src='/assets/images/iconography/brawl_ICE_ARMOR.png'
+              alt='Ice armor bonus'
+              className='ReleaseNotesAugust2021__card'
+            />
+          </div>
+        </CardsContainer>
+
+        <p>Here is the breakdown of every victory bonus:</p>
+
+        <ul>
+          <li>
+            <span className='Highlight'>Resources bonuses:</span> Picking these
+            bonuses will immediately grant the player a certain amount of
+            resources, varying based on the Brawl difficulty.
+          </li>
+          <li>
+            <span className='Highlight'>Fortress bonus:</span> Picking this
+            bonus will increase the Fortress Level of the player by 1 within the
+            current Brawl difficulty and only for Brawl matches. It will not
+            increase the Fortress Level beyond the cap for that Brawl. This
+            bonus cannot be drawn if already capped.
+          </li>
+          <li>
+            <span className='Highlight'>Life/Lives Up bonuses:</span> Picking
+            these bonuses will refill a heart (or all hearts), giving the player
+            extra chances to climb the milestones without being reset to the
+            start of the current milestone. These bonuses cannot be found if all
+            hearts are currently full.
+          </li>
+          <li>
+            <span className='Highlight'>Iron heart bonus:</span> Picking this
+            bonus will grant the player an extra heart (up to 5 at most), which
+            begins empty. Once filled (via a life up bonus), it grants the
+            player an extra life. If emptied again, that heart gets removed
+            entirely as it is not permanent. This bonus cannot be found if there
+            are already 5 hearts.
+          </li>
+          <li>
+            <span className='Highlight'>Gold solidify bonus:</span> Picking this
+            bonus will solidify an iron heart so it becomes permanent. It does
+            not change the heart status: if empty, it stays empty; if full, it
+            stays full. It just makes it permanent so the heart no longer
+            disappears after being emptied. From there on, the heart behaves
+            like the 3 starting ones. This bonus cannot be found if there is no
+            iron heart.
+          </li>
+          <li>
+            <span className='Highlight'>Ice armor bonus:</span> Picking this
+            bonus will apply an ice armor to a full heart, which protects it
+            from being emptied for one loss. After a loss, the ice armor gets
+            removed. This bonus cannot be found in there are no full unprotected
+            hearts.
+          </li>
+        </ul>
 
         <FAQSection
           id='faq'
