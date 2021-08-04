e43be82002880473252364369a1314eaaa4ec5fb
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-04 16:52:28 +0200

    Update assets URLs

--- src/helpers/getFusionStonesCard/index.js
+++ src/helpers/getFusionStonesCard/index.js
@@ -8,33 +8,41 @@ const FUSION_STONES_BASE = {
   strength: null,
   movement: null,
 }
+
 const FUSION_STONES_COMMON = {
   ...FUSION_STONES_BASE,
   id: 'R1',
   ability: '5 Fusion Stones',
   rarity: 'common',
-  image: 'http://localhost:3000/assets/images/iconography/stones_common.png',
+  image:
+    'http://stormbound-kitty.com/assets/images/iconography/stones_common.png',
 }
+
 const FUSION_STONES_RARE = {
   ...FUSION_STONES_BASE,
   id: 'R2',
   ability: '10 Fusion Stones',
   rarity: 'rare',
-  image: 'http://localhost:3000/assets/images/iconography/stones_rare.png',
+  image:
+    'http://stormbound-kitty.com/assets/images/iconography/stones_rare.png',
 }
+
 const FUSION_STONES_EPIC = {
   ...FUSION_STONES_BASE,
   id: 'R3',
   ability: '25 Fusion Stones',
   rarity: 'epic',
-  image: 'http://localhost:3000/assets/images/iconography/stones_epic.png',
+  image:
+    'http://stormbound-kitty.com/assets/images/iconography/stones_epic.png',
 }
+
 const FUSION_STONES_LEGENDARY = {
   ...FUSION_STONES_BASE,
   id: 'R4',
   ability: '50 Fusion Stones',
   rarity: 'legendary',
-  image: 'http://localhost:3000/assets/images/iconography/stones_legendary.png',
+  image:
+    'http://stormbound-kitty.com/assets/images/iconography/stones_legendary.png',
 }
 
 export const FUSION_STONES = [
