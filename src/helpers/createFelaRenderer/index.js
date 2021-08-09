f3fe586044979d48188cb659961f1bf0639cb3b5
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-09 09:28:12 +0200

    Add renderer function

---
+++ src/helpers/createFelaRenderer/index.js
@@ -0,0 +1,48 @@
+import { createRenderer } from 'fela'
+import embedded from 'fela-plugin-embedded'
+import extend from 'fela-plugin-extend'
+import enforceLongHands from 'fela-enforce-longhands'
+import sortClassNames from 'fela-sort-classnames'
+import sortMediaQueryMobileFirst from 'fela-sort-media-query-mobile-first'
+import responsiveValue from 'fela-plugin-responsive-value'
+import namedKeys from 'fela-plugin-named-keys'
+
+const toObject = (acc, prop) => ({ ...acc, [prop]: true })
+
+const getMediaQueries = values => {
+  if (values.length === 2) return ['@media (min-width: 700px)']
+}
+
+// prettier-ignore
+const responsiveProperties = [
+  'padding', 'paddingTop', 'paddingRight', 'paddingBottom', 'paddingLeft',
+  'margin', 'marginTop', 'marginRight', 'marginBottom', 'marginLeft',
+]
+
+function createFelaRenderer() {
+  return createRenderer({
+    optimizeCaching: true,
+    plugins: [
+      extend(),
+      embedded(),
+      responsiveValue(
+        getMediaQueries,
+        responsiveProperties.reduce(toObject, {})
+      ),
+      namedKeys({
+        medium: '@media (min-width: 700px)',
+        small: '@media (max-width: 699px)',
+      }),
+    ],
+    enhancers: [
+      sortClassNames(),
+      sortMediaQueryMobileFirst(),
+      enforceLongHands(),
+    ],
+    keyframePrefixes: [],
+    filterClassName: className =>
+      !'ad,ads,adv,bi,fb,ig,pin,tw,vk'.split(',').includes(className),
+  })
+}
+
+export default createFelaRenderer
