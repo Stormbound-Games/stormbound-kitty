85687f8bd02fb79fb04f5a2a6f576407b64e28e4
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-12 13:32:15 +0200

    Make it possible to render teasers across 2 columns

--- src/components/Teasers/index.js
+++ src/components/Teasers/index.js
@@ -4,11 +4,15 @@ import Teaser from '../Teaser'
 import chunk from '../../helpers/chunk'
 
 export default React.memo(function Teasers(props) {
-  return chunk(props.items, 3).map((row, index) => (
+  const columns = props.columns || 3
+
+  return chunk(props.items, columns).map((row, index) => (
     <Row key={index} desktopOnly wideGutter>
-      <Row.Column width='1/3'>{row[0] && <Teaser {...row[0]} />}</Row.Column>
-      <Row.Column width='1/3'>{row[1] && <Teaser {...row[1]} />}</Row.Column>
-      <Row.Column width='1/3'>{row[2] && <Teaser {...row[2]} />}</Row.Column>
+      {Array.from({ length: columns }, (_, index) => (
+        <Row.Column key={index} width='1/3'>
+          {row[index] && <Teaser {...row[index]} />}
+        </Row.Column>
+      ))}
     </Row>
   ))
 })
