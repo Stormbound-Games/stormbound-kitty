879058c2b0b1d1ee6164384b24069611971549aa
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-12 12:48:25 +0200

    Reorder items in dry runnr header

--- src/components/DryRunnerHeader/index.js
+++ src/components/DryRunnerHeader/index.js
@@ -37,6 +37,14 @@ export default React.memo(function DryRunnerHeader(props) {
     <Spacing bottom='LARGER'>
       <Row desktopOnly>
         <Row.Column width='1/3' align='center'>
+          <ResetButton
+            label='Reset game'
+            confirm='Are you sure you want to reset the game? Don’t worry, you’ll keep your deck.'
+            reset={props.resetGame}
+          />
+        </Row.Column>
+
+        <Row.Column width='1/3' align='center'>
           <span className={css(styles.mana)}>
             Current mana:{' '}
             <Mana
@@ -47,14 +55,6 @@ export default React.memo(function DryRunnerHeader(props) {
               extend={{ marginLeft: '1em' }}
             />
           </span>
-        </Row.Column>
-
-        <Row.Column width='1/3' align='center'>
-          <ResetButton
-            label='Reset game'
-            confirm='Are you sure you want to reset the game? Don’t worry, you’ll keep your deck.'
-            reset={props.resetGame}
-          />
         </Row.Column>
 
         <Row.Column width='1/3' align='center'>
