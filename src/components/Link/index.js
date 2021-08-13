9d141c6f52ef459a87c534571c3046abd9b1b46f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-13 12:15:03 +0200

    Restore ref in link component

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -4,49 +4,55 @@ import { Link as RouterLink } from 'react-router-dom'
 import Icon from '../Icon'
 import styles from './styles'
 
-export default React.memo(function Link({
-  extend,
-  inNewTab,
-  hideNewTabIndicator,
-  ...props
-}) {
-  const { css } = useFela()
+export default React.memo(
+  React.forwardRef(function Link(
+    { extend, inNewTab, hideNewTabIndicator, ...props },
+    ref
+  ) {
+    const { css } = useFela()
 
-  if (props.href) {
-    const shouldUseNewTab =
-      (typeof inNewTab === 'undefined' && props.href.startsWith('http')) ||
-      inNewTab
+    if (props.href) {
+      const shouldUseNewTab =
+        (typeof inNewTab === 'undefined' && props.href.startsWith('http')) ||
+        inNewTab
+
+      return (
+        <a
+          {...props}
+          ref={ref}
+          className={css(styles.link, extend)}
+          target={shouldUseNewTab ? '_blank' : undefined}
+          rel={shouldUseNewTab ? 'noopener noreferrer' : undefined}
+        >
+          {props.children}
+          {shouldUseNewTab && !hideNewTabIndicator && (
+            <Icon icon='arrow-top-right' extend={styles.newTab} />
+          )}
+        </a>
+      )
+    }
+
+    if (props.to) {
+      return (
+        <RouterLink
+          {...props}
+          innerRef={ref}
+          className={css(styles.link, extend)}
+        >
+          {props.children}
+        </RouterLink>
+      )
+    }
 
     return (
-      <a
+      <button
         {...props}
-        className={css(styles.link, extend)}
-        target={shouldUseNewTab ? '_blank' : undefined}
-        rel={shouldUseNewTab ? 'noopener noreferrer' : undefined}
+        ref={ref}
+        type={props.type || 'button'}
+        className={css(styles.link, styles.buttonAsLink, extend)}
       >
         {props.children}
-        {shouldUseNewTab && !hideNewTabIndicator && (
-          <Icon icon='arrow-top-right' extend={styles.newTab} />
-        )}
-      </a>
+      </button>
     )
-  }
-
-  if (props.to) {
-    return (
-      <RouterLink {...props} className={css(styles.link, extend)}>
-        {props.children}
-      </RouterLink>
-    )
-  }
-
-  return (
-    <button
-      {...props}
-      type={props.type || 'button'}
-      className={css(styles.link, styles.buttonAsLink, extend)}
-    >
-      {props.children}
-    </button>
-  )
-})
+  })
+)
