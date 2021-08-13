6c3901c82355d30ee7edfa44e23bb6c15344ff3f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-13 12:15:37 +0200

    Restore ref in input component

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,32 +3,30 @@ import { useFela } from 'react-fela'
 import VisuallyHidden from '../VisuallyHidden'
 import styles from './styles'
 
-export default React.memo(function Input({
-  hideLabel,
-  extend,
-  label,
-  ...props
-}) {
-  const { css } = useFela()
-  const name = props.name || props.id
+export default React.memo(
+  React.forwardRef(function Input({ hideLabel, extend, label, ...props }, ref) {
+    const { css } = useFela()
+    const name = props.name || props.id
 
-  return (
-    <>
-      {hideLabel ? (
-        <VisuallyHidden as='label' htmlFor={props.id}>
-          {label}
-        </VisuallyHidden>
-      ) : (
-        <label htmlFor={props.id} className={css(styles.label)}>
-          {label}
-        </label>
-      )}
-      <input
-        type='text'
-        {...props}
-        name={name}
-        className={css(styles.input, extend)}
-      />
-    </>
-  )
-})
+    return (
+      <>
+        {hideLabel ? (
+          <VisuallyHidden as='label' htmlFor={props.id}>
+            {label}
+          </VisuallyHidden>
+        ) : (
+          <label htmlFor={props.id} className={css(styles.label)}>
+            {label}
+          </label>
+        )}
+        <input
+          type='text'
+          {...props}
+          name={name}
+          className={css(styles.input, extend)}
+          ref={ref}
+        />
+      </>
+    )
+  })
+)
