7ab1ea07b908f4f3a8f2cf5d0762459fe822214f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-13 13:13:05 +0200

    Prevent all megamenus from re-rendering all the time

--- src/components/HeaderMegaMenu/index.js
+++ src/components/HeaderMegaMenu/index.js
@@ -22,63 +22,68 @@ const useOutsideAlerter = (props, ref) => {
   }, [handleClickOutside])
 }
 
-export default React.memo(function HeaderMegaMenu(props) {
-  const { css } = useFela({ isOpen: props.open })
-  const ref = React.useRef(null)
+export default React.memo(
+  function HeaderMegaMenu(props) {
+    const { css } = useFela({ isOpen: props.open })
+    const ref = React.useRef(null)
 
-  useOutsideAlerter(props, ref)
+    useOutsideAlerter(props, ref)
 
-  return (
-    <div
-      ref={ref}
-      className={css(styles.menu)}
-      style={{ '--columns': props.columns.length }}
-      onMouseOver={props.onMouseOver}
-      onMouseOut={props.onMouseOut}
-    >
-      <Row isDesktopOnly>
-        {props.columns.map((column, index) => (
-          <Row.Column width={`1/${props.columns.length}`} key={index}>
-            <h2 className={css(styles.title)}>
-              {column.icon ? (
-                <Icon icon={column.icon} extend={styles.icon} />
-              ) : null}
-              {column.to ? (
-                <Link to={column.to}>{column.title}</Link>
-              ) : (
-                column.title
-              )}
-            </h2>
-            <ul className={css(styles.list)}>
-              {column.items.map(item => (
-                <li key={item.label} className={css(styles.item)}>
-                  <Link
-                    href={item.href}
-                    to={item.to}
-                    inNewTab={!!item.href}
-                    extend={styles.link({
-                      isActive: props.active.includes(item.id),
-                    })}
-                  >
-                    {item.label}
-                  </Link>
-                  {item.new && <NewPulse />}
-                </li>
-              ))}
-            </ul>
-          </Row.Column>
-        ))}
-      </Row>
-      {props.close && (
-        <button
-          type='button'
-          onClick={props.close}
-          aria-label='Close menu'
-          className={css(styles.close)}
-        >
-          &times;
-        </button>
-      )}
-    </div>
-  )
-})
+    return (
+      <div
+        ref={ref}
+        className={css(styles.menu)}
+        style={{ '--columns': props.columns.length }}
+        onMouseOver={props.onMouseOver}
+        onMouseOut={props.onMouseOut}
+      >
+        <Row isDesktopOnly>
+          {props.columns.map((column, index) => (
+            <Row.Column width={`1/${props.columns.length}`} key={index}>
+              <h2 className={css(styles.title)}>
+                {column.icon ? (
+                  <Icon icon={column.icon} extend={styles.icon} />
+                ) : null}
+                {column.to ? (
+                  <Link to={column.to}>{column.title}</Link>
+                ) : (
+                  column.title
+                )}
+              </h2>
+              <ul className={css(styles.list)}>
+                {column.items.map(item => (
+                  <li key={item.label} className={css(styles.item)}>
+                    <Link
+                      href={item.href}
+                      to={item.to}
+                      inNewTab={!!item.href}
+                      extend={styles.link({
+                        isActive: props.active.includes(item.id),
+                      })}
+                    >
+                      {item.label}
+                    </Link>
+                    {item.new && <NewPulse />}
+                  </li>
+                ))}
+              </ul>
+            </Row.Column>
+          ))}
+        </Row>
+        {props.close && (
+          <button
+            type='button'
+            onClick={props.close}
+            aria-label='Close menu'
+            className={css(styles.close)}
+          >
+            &times;
+          </button>
+        )}
+      </div>
+    )
+  },
+  function areEqual(prevProps, nextProps) {
+    return prevProps.open === nextProps.open
+  }
+)
