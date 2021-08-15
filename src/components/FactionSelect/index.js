1ceb6bf46f7e050d98891a88e4441924aaa24f5f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 11:07:02 +0200

    Simplify faction selects

--- src/components/FactionSelect/index.js
+++ src/components/FactionSelect/index.js
@@ -3,35 +3,28 @@ import Select from '../Select'
 import { FACTIONS } from '../../constants/game'
 import capitalise from '../../helpers/capitalise'
 
-export default React.memo(function FactionSelect(props) {
+export default React.memo(function FactionSelect({
+  anyLabel,
+  emptyLabel,
+  withAny,
+  withEmpty,
+  withExtendedVersions,
+  withNeutral,
+  ...props
+}) {
   const label = props.label || 'Faction'
   const id = props.id || 'faction'
 
   return (
-    <Select
-      hideLabel={props.hideLabel}
-      label={label}
-      form={props.form}
-      name={props.name || id}
-      id={id}
-      required={props.required}
-      value={props.value}
-      defaultValue={props.defaultValue}
-      onChange={props.onChange}
-      data-testid={props['data-testid']}
-      className={props.className}
-      extend={props.extend}
-    >
-      {props.withAny && <option value='*'>{props.anyLabel || 'Any'}</option>}
-      {props.withEmpty && (
-        <option value=''>{props.emptyLabel || 'Pick a faction'}</option>
-      )}
+    <Select {...props} label={label} id={id}>
+      {withAny && <option value='*'>{anyLabel || 'Any'}</option>}
+      {withEmpty && <option value=''>{emptyLabel || 'Pick a faction'}</option>}
       {Object.keys(FACTIONS)
-        .filter(faction => props.withNeutral || faction !== 'neutral')
+        .filter(faction => withNeutral || faction !== 'neutral')
         .map(faction => (
           <React.Fragment key={faction}>
             <option value={faction}>{capitalise(faction)}</option>
-            {props.withExtendedVersions && faction !== 'neutral' && (
+            {withExtendedVersions && faction !== 'neutral' && (
               <option value={'neutral,' + faction}>
                 {capitalise(faction)} + Neutral
               </option>
