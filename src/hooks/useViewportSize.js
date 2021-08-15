8bc24533b020f79464e28ec737a207b7877ce8c1
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 14:56:14 +0200

    Make useViewportSize SSR-friendly

--- src/hooks/useViewportSize.js
+++ src/hooks/useViewportSize.js
@@ -2,13 +2,17 @@ import React from 'react'
 import getViewportSize from '../helpers/getViewportSize'
 
 const useViewportSize = () => {
-  const [dimensions, setDimensions] = React.useState(getViewportSize())
+  const [dimensions, setDimensions] = React.useState({
+    width: Infinity,
+    height: Infinity,
+  })
   const recordViewportWidth = React.useCallback(
     () => setDimensions(getViewportSize()),
     []
   )
 
   React.useEffect(() => {
+    recordViewportWidth()
     window.addEventListener('resize', recordViewportWidth)
     return () => window.removeEventListener('resize', recordViewportWidth)
   }, [recordViewportWidth])
