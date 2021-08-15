6dfbae32039547b95cd66fe151f3f261bf6be7cf
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 15:31:01 +0200

    Migrate book simulator page

---
+++ src/pages/simulators/books/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import BookOpeningSimulator from '../../../components/BookOpeningSimulator'
+import Layout from '../../../components/Layout'
+
+export default () => (
+  <Layout active={['TOOLS', 'BOOK_SIMULATOR']}>
+    <BookOpeningSimulator />
+  </Layout>
+)
