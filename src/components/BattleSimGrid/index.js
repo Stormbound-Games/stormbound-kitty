e64da6b4ccd5a45277530e59832f4869f6c294ef
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 21:13:00 +0200

    Grix battle sim grid markers

--- src/components/BattleSimGrid/index.js
+++ src/components/BattleSimGrid/index.js
@@ -4,8 +4,37 @@ import Cell from '../BattleSimCell'
 import Row from '../BattleSimRow'
 import styles from './styles'
 
+const GridRow = props => {
+  const x = props.rowIndex
+  const { css } = useFela({ withGridMarkers: props.gridMarkers, rowIndex: x })
+
+  return (
+    <Row index={x} board={props.board}>
+      {props.row.map((cell, y) => (
+        <div key={'col-' + y} className={css(styles.cell)}>
+          <div className={css(styles.cellContent)}>
+            <Cell
+              {...cell}
+              data-testid={`cell-${'ABCDE'[x]}${'1234'[y]}`}
+              mode={props.mode}
+              activePlayer={props.activePlayer}
+              isActive={
+                props.activeCell &&
+                x === props.activeCell[0] &&
+                y === props.activeCell[1]
+              }
+              {...props.dndProps(x, y)}
+              onClick={props.onCellClick(x, y)}
+            />
+          </div>
+        </div>
+      ))}
+    </Row>
+  )
+}
+
 export default React.memo(function BattleSimGrid(props) {
-  const { css } = useFela({ withGridMarkes: props.gridMarkers })
+  const { css } = useFela()
 
   return (
     <div
@@ -13,27 +42,7 @@ export default React.memo(function BattleSimGrid(props) {
       data-testid={props.gridMarkers ? 'board-grid-with-markers' : 'board-grid'}
     >
       {props.board.map((row, x) => (
-        <Row index={x} board={props.board} key={x}>
-          {row.map((cell, y) => (
-            <div key={'col-' + y} className={css(styles.cell({ rowIndex: x }))}>
-              <div className={css(styles.cellContent)}>
-                <Cell
-                  {...cell}
-                  data-testid={`cell-${'ABCDE'[x]}${'1234'[y]}`}
-                  mode={props.mode}
-                  activePlayer={props.activePlayer}
-                  isActive={
-                    props.activeCell &&
-                    x === props.activeCell[0] &&
-                    y === props.activeCell[1]
-                  }
-                  {...props.dndProps(x, y)}
-                  onClick={props.onCellClick(x, y)}
-                />
-              </div>
-            </div>
-          ))}
-        </Row>
+        <GridRow {...props} row={row} rowIndex={x} key={x} />
       ))}
     </div>
   )
