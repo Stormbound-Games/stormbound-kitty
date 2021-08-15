43116af9536a47ac65d5da3f1c68a92cff0a0df7
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 15:15:30 +0200

    Migrate FAQ page

---
+++ src/pages/faq/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import FAQ from '../../components/FAQ'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout active={['HOME', 'FAQ']}>
+    <FAQ />
+  </Layout>
+)
