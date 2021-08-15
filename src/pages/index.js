4620dd7fbc40b41ab7641954d6d090b2bcc59d49
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 15:00:59 +0200

    Migrate home page

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Home from '../components/Home'
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout active={['HOME', 'NEWS']}>
+    <Home />
+  </Layout>
+)
