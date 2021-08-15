72ade196f7c5501d5d770b3ef91db7e1568dbe45
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-15 15:04:21 +0200

    Migrate videos page

---
+++ src/pages/videos/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Videos from '../../components/Videos'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout active={['COMMUNITY', 'VIDEOS']}>
+    <Videos />
+  </Layout>
+)
