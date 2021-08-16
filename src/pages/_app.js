f9dc872e936e7591bd8b76c9e7ef143ee8c903e4
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-16 14:29:00 +0200

    Move meta tags to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,13 +11,33 @@ import createFelaRenderer from '~/helpers/createFelaRenderer'
 const fallbackRenderer = createFelaRenderer()
 
 function App({ Component, pageProps, renderer = fallbackRenderer }) {
+  const description =
+    'Stormbound-Kitty aims at providing tools and information about Sheepyard’s great game, and extending its players’ experience outside the game'
   return (
     <>
       <Head>
+        <link rel='shortcut icon' href='/favicon.png' />
+        <link rel='apple-touch-icon' href='/favicon.png' />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, viewport-fit=cover'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no'
         />
+        <meta name='theme-color' content='#101F26' />
+        <link rel='manifest' href='/manifest.json' />
+        <title>Stormbound-Kitty</title>
+        <meta name='author' content='Kitty' />
+        <meta name='og:site_name' content='Stormbound-Kitty' />
+        <meta name='og:type' content='website' />
+        <meta name='og:title' content='Stormbound-Kitty' />
+        <meta name='twitter:title' content='Stormbound-Kitty' />
+        <meta name='og:url' content='https://stormbound-kitty.com' />
+        <meta property='og:image' content='/favicon.png' />
+        <meta name='description' content={description} />
+        <meta name='og:description' content={description} />
+        <meta name='twitter:description' content={description} />
+        <script>
+          {`if (!Object.values) Object.values = o => Object.keys(o).map(k => o[k])`}
+        </script>
       </Head>
       <RendererProvider renderer={renderer}>
         <ErrorBoundary>
