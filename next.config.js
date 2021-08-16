ec2a6076483d41920ffec53cdc322feb424e28ad
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-16 10:38:47 +0200

    Add redirects

---
+++ next.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  async redirects() {
+    return [
+      { source: '/guides', destination: '/guides/essentials', permanent: true },
+      { source: '/guides/lexicon', destination: '/lexicon', permanent: true },
+      {
+        source: '/guides/known-bugs',
+        destination: '/known-bugs',
+        permanent: true,
+      },
+      {
+        source: '/list/ranked/display',
+        destination: '/list/ranked',
+        permanent: true,
+      },
+      {
+        source: '/list/equals/display',
+        destination: '/list/equals',
+        permanent: true,
+      },
+      {
+        source: '/collection/books',
+        destination: '/calculators/books',
+        permanent: true,
+      },
+      {
+        source: '/income-calculator',
+        destination: '/calculators/income',
+        permanent: true,
+      },
+      {
+        source: '/changelog/releases',
+        destination: '/releases',
+        permanent: true,
+      },
+      {
+        source: '/changelog/cards',
+        destination: '/changelog',
+        permanent: true,
+      },
+      { source: '/donate', destination: '/about', permanent: true },
+    ]
+  },
+}
