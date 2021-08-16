691bd74233db156fa9d1d00931a96be5e755c278
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-16 16:14:26 +0200

    Disable ESLint during builds

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       { source: '/guides', destination: '/guides/essentials', permanent: true },
