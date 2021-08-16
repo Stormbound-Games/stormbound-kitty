8a7b48c5d30dd20ceccc78b2c4da1cf51bfaaecc
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-16 09:39:08 +0200

    Migrate members page

---
+++ src/pages/members/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Members from '../../components/Members'
+import Layout from '../../components/Layout'
+
+export default () => (
+  <Layout active={['COMMUNITY', 'MEMBERS']}>
+    <Members />
+  </Layout>
+)
