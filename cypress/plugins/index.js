9e96502656b3b0ceb1b31f37fd5dff0a7347de4e
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-17 22:54:54 +0200

    Make Cypress handle aliased paths

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,29 @@
+const path = require('path')
+const findWebpack = require('find-webpack')
+const preprocessor = require('@cypress/webpack-preprocessor')
+
 module.exports = (on, config) => {
   require('@cypress/react/plugins/react-scripts')(on, config)
+
+  // Find the Webpack configuration used by `react-scripts`.
+  const webpackOptions = findWebpack.getWebpackOptions()
+
+  if (!webpackOptions) {
+    throw new Error('Could not find Webpack in this project')
+  }
+
+  // Add aliased paths support to the Webpack configuration.
+  webpackOptions.resolve.alias['~'] = path.resolve('src')
+
+  // Just passing `webpackOptions` to the preprocessor won’t work because
+  // `react-scripts` by default includes plugins that split specs into chunks,
+  // etc. Thus, we use a module that carefully removes only plugins that we
+  // found to be breaking the bundling.
+  // https://github.com/bahmutov/find-webpack
+  // https://github.com/cypress-io/cypress-webpack-preprocessor/issues/31
+  findWebpack.cleanForCypress({ reactScripts: true }, webpackOptions)
+
+  on('file:preprocessor', preprocessor({ webpackOptions, watchOptions: {} }))
 
   return config
 }
