f80a2c035f7ffedcb22d6a5d0184e95184dffa35
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-17 15:37:48 +0200

    Make Cypress understand app aliases

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,24 @@
+const path = require('path')
+const preprocessor = require('@cypress/webpack-preprocessor')
+
+const ALIASES = {
+  '~/bot': path.resolve('src/bot'),
+  '~/components': path.resolve('src/components'),
+  '~/constants': path.resolve('src/constants'),
+  '~/data': path.resolve('src/data'),
+  '~/helpers': path.resolve('src/helpers'),
+  '~/hooks': path.resolve('src/hooks'),
+  '~/pages': path.resolve('src/pages'),
+  '~/styles': path.resolve('src/styles'),
+}
+
 module.exports = (on, config) => {
   require('@cypress/react/plugins/react-scripts')(on, config)
+
+  on(
+    'file:preprocessor',
+    preprocessor({ webpackOptions: { resolve: { alias: ALIASES } } })
+  )
 
   return config
 }
