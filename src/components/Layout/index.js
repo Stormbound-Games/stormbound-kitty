9aab62a903252e40c7fd80b3f795445591d2485b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-18 12:31:05 +0200

    Remove layout scrolling hack

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,38 +5,14 @@ import EyeCatcher from '~/components/EyeCatcher'
 import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import Link from '~/components/Link'
-import useRouter from '~/hooks/useRouter'
 import styles from './styles'
 
 const SearchDialog = dynamic(() => import('~/components/SearchDialog'))
 
 export default React.memo(function Layout(props) {
   const { css } = useFela()
-  const { location } = useRouter()
   const searchDialog = React.useRef(null)
   const [isSearchReady, setIsSearchReady] = React.useState(false)
-
-  React.useLayoutEffect(() => {
-    const hash = window.location.hash
-
-    // react-router and @loadable/component are making it incredibly hard to use
-    // anchor links, and I didn’t find a better solution than literally waiting
-    // half a second to hope for the bundle to be fully loaded… The `:target`
-    // pseudo-class still doesn’t work for content which was loaded after the
-    // hash was assigned (which is the case for chunks). Also, there is this
-    // webkit bug that causes the `:target` pseudo-class not to work with
-    // history pushes… Anyway, that’s all garbage, and I’m kind of done with it.
-    // See: https://bugs.webkit.org/show_bug.cgi?id=83490
-    setTimeout(() => {
-      const node = hash && document.getElementById(hash.slice(1))
-
-      if (node && node.scrollIntoView) {
-        node.scrollIntoView(true)
-      } else {
-        window.scrollTo(0, 0)
-      }
-    }, 500)
-  }, [location.hash])
 
   return (
     <div className={css(styles.layout)}>
