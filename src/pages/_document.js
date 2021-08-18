e0dbc32e9d59487015886ee3c0f51051ea0c0467
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-18 12:52:54 +0200

    Add _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { renderToNodeList } from 'react-fela'
+import createFelaRenderer from '~/helpers/createFelaRenderer'
+import renderRootStyles from '~/helpers/renderRootStyles'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(context) {
+    const renderer = createFelaRenderer()
+    const originalRenderPage = context.renderPage
+
+    renderRootStyles(renderer)
+
+    context.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App =>
+          function EnhancedApp(props) {
+            return <App {...props} renderer={renderer} />
+          },
+      })
+
+    const initialProps = await Document.getInitialProps(context)
+    const styles = [...initialProps.styles, ...renderToNodeList(renderer)]
+
+    return { ...initialProps, styles }
+  }
+
+  render() {
+    return (
+      <Html lang='en'>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <div id='notification-root' role='status' aria-live='polite'></div>
+        </body>
+      </Html>
+    )
+  }
+}
