d7696b0a0e95125cfb4adab91ed1af4311ab257f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-18 22:46:08 +0200

    Add ‘useIsMounted’ hook

---
+++ src/hooks/useIsMounted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useIsMounted = () => {
+  const [isMounted, setIsMounted] = React.useState(false)
+
+  React.useEffect(() => setIsMounted(true), [])
+
+  return isMounted
+}
+
+export default useIsMounted
