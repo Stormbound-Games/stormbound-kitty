6c84f0d4e9fbb502249f0e497c67b9399d76c1f6
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-19 14:50:04 +0200

    Make it possible to pass a default width to ‘useFluidSizing’

--- src/hooks/useFluidSizing.js
+++ src/hooks/useFluidSizing.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import useViewportSize from './useViewportSize'
 
-export default ratio => {
+export default (ratio, defaultWidth = undefined) => {
   const { viewportWidth } = useViewportSize()
   const ref = React.createRef()
-  const [fontSize, setFontSize] = React.useState(undefined)
+  const [fontSize, setFontSize] = React.useState(
+    defaultWidth ? defaultWidth * ratio + 'px' : undefined
+  )
 
   React.useEffect(() => {
     setFontSize(ref.current.offsetWidth * ratio + 'px')
