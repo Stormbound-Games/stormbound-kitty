896d85f6f6e9c2f4a4077d6e342d849319fbdef6
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-22 21:39:41 +0200

    Fix font size in embedded battle sims

--- src/components/BattleSimAppMobile/styles.js
+++ src/components/BattleSimAppMobile/styles.js
@@ -1,8 +1,13 @@
+/**
+ * 1. Make sure embedded sims within editorial content have the expected font
+ *    size, which was originally 90% of the root font size (hence `0.9rem`).
+ */
 const root = {
   position: 'relative',
   flex: '1 1 100%',
   transition: 'transform 0.25s ease-in-out',
   overflowX: 'hidden',
+  fontSize: '0.9rem' /* 1 */,
 }
 
 const panel = ({ type, isActive }) => ({
