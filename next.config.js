5763d1746ebd335f3f630882bd7f6d90227eb10c
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-22 13:31:14 +0200

    Add security headers

--- next.config.js
+++ next.config.js
@@ -51,6 +51,31 @@ module.exports = {
   async headers() {
     return [
       {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+      {
         source: '/:all*(woff2)',
         locale: false,
         headers: [
