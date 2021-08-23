0d541b41e36e19dfe4ff11a9f8f69c9c6e33be59
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-23 17:00:59 +0200

    Fix date parsing issues with certain timezones

--- src/helpers/parseDate/index.js
+++ src/helpers/parseDate/index.js
@@ -4,7 +4,9 @@ const parseDate = date => {
   const chunks = date.split('/')
 
   if (chunks.length === 2) {
-    return new Date(+chunks[1], +chunks[0] - 1, 1)
+    // Halfway through the month as 1st of the month can lead to off-by-one
+    // errors depending on timezones
+    return new Date(+chunks[1], +chunks[0] - 1, 15)
   }
 
   return new Date(+chunks[2], +chunks[1] - 1, +chunks[0])
