e525b1c9e953e21fa9c87d062e853e4cddc5c8b9
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-25 21:23:57 +0200

    Update notification styles

--- src/components/Notification/styles.js
+++ src/components/Notification/styles.js
@@ -12,52 +12,58 @@ const notificationPseudo = {
   backgroundColor: 'inherit',
 }
 
-/**
- * 1. Arbitrary width for the notifications on desktop.
- */
 const notification = {
-  width: '18em' /* 1 */,
-  position: 'fixed',
-  backgroundColor: 'var(--black)',
-  border: '1px double var(--beige)',
-  right: '1.5em',
-  bottom: '1.5em',
-  padding: 'var(--s-base)',
-  boxShadow: '0 0 0.25em 0.5em #0000001a',
-  margin: 0,
+  width: '100%',
+
   zIndex: 20,
+  position: 'fixed',
+  bottom: 0,
+  left: 0,
+  right: 0,
 
-  '::before': {
-    ...notificationPseudo,
-    bottom: '100%',
-    borderBottom: 0,
-  },
+  textAlign: 'center',
+  fontSize: '90%',
 
-  '::after': {
-    ...notificationPseudo,
-    top: '100%',
-    borderTop: 0,
-  },
+  borderTop: '1px solid var(--dark-beige)',
+  padding: 'var(--s-smaller) var(--s-large)',
+  margin: 0,
 
-  small: {
-    bottom: 0,
-    left: 0,
-    right: 0,
-    border: 0,
-    borderTop: '1px solid var(--beige)',
-    width: '100%',
-    fontSize: '90%',
-    borderRadius: 0,
-    boxShadow: '0 -0.2em 0 0.25em #0000001a',
+  backgroundColor: 'var(--black)',
+  borderRadius: 0,
+  boxShadow: '0 -0.2em 0 0.5em #00000033',
 
-    '::after': { content: 'none' },
-    '::before': { content: 'none' },
+  medium: {
+    width: 'calc(1200px - var(--s-large))',
+    maxWidth: 'calc(100% - var(--s-base) * 2)',
+
+    bottom: '1.5em',
+    left: '50%',
+    transform: 'translateX(-50%)',
+
+    textAlign: 'left',
+    fontSize: '125%',
+
+    border: '1px solid var(--dark-beige)',
+    boxShadow: '0 0 1em 0.5em #00000033',
+
+    '::before': {
+      ...notificationPseudo,
+      bottom: '100%',
+      borderBottom: 0,
+    },
+
+    '::after': {
+      ...notificationPseudo,
+      top: '100%',
+      borderTop: 0,
+    },
   },
 }
 
 const icon = {
   position: 'relative',
   top: '1.5px',
+  marginRight: 'var(--s-smaller)',
 }
 
 const styles = { notification, icon }
