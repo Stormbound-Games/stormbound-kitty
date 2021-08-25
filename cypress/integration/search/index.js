57f6971dec8ccdf00a0d32bf3092a6f845b1b473
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-25 09:04:55 +0200

    Fix Cypress test for search

--- cypress/integration/search/index.js
+++ cypress/integration/search/index.js
@@ -3,7 +3,11 @@ import s from './selectors'
 describe('Search', () => {
   it('should be possible to do a search', () => {
     cy.visit('/')
-    cy.get(s.SEARCH_BUTTON).click().get(s.SEARCH_DIALOG).should('be.visible')
+    cy.get(s.SEARCH_BUTTON)
+      .last()
+      .click()
+      .get(s.SEARCH_DIALOG)
+      .should('be.visible')
     cy.get(s.SEARCH_INPUT).type('card').type('{enter}')
     cy.get(s.SEARCH_RESULT).first().click()
     cy.url().should('not.be', '/')
