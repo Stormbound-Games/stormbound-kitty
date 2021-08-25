840dee53a29ee580a7099e69984f00ac435a3be6
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-25 21:58:11 +0200

    Make the notification more customisable

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -5,11 +5,12 @@ import styles from './styles'
 
 export default React.memo(function Notification(props) {
   const { css } = useFela()
+  const Component = props.as || 'p'
 
   return props.isVisible ? (
-    <p className={css(styles.notification)}>
+    <Component className={css(styles.notification, props.extend)}>
       {props.icon && <Icon extend={styles.icon} icon={props.icon} />}{' '}
       {props.children}
-    </p>
+    </Component>
   ) : null
 })
