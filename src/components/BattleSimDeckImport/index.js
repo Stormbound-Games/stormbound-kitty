f44f0cab2f38b162117dc538e8b02bc69dbe0026
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-26 09:35:33 +0200

    Move ‘BattleSimDeckImport’ to function component

--- src/components/BattleSimDeckImport/index.js
+++ src/components/BattleSimDeckImport/index.js
@@ -5,119 +5,107 @@ import Deck from '~/components/Deck'
 import Dialog from '~/components/Dialog'
 import serialisation from '~/helpers/serialisation'
 
-class BattleSimDeckImport extends React.Component {
-  state = {
-    deckURL: '',
-    deck: [],
-    hand: [],
-    error: null,
-  }
+export default React.memo(function BattleSimDeckImport(props) {
+  const dialog = React.useRef(null)
+  const [deckURL, setDeckURL] = React.useState('')
+  const [deck, setDeck] = React.useState([])
+  const [hand, setHand] = React.useState([])
+  const [error, setError] = React.useState(null)
+  const { importDeck: importDeckFromProps } = props
 
-  open = () => this.dialog.show()
-  close = () => this.dialog.hide()
+  const registerDialog = instance => {
+    dialog.current = instance
 
-  registerDialog = ref => {
-    this.dialog = ref
-
-    if (this.dialog) {
-      this.dialog.on('hide', () => {
-        this.setState({
-          deckURL: '',
-          deck: [],
-          hand: [],
-          error: null,
-        })
+    if (dialog.current) {
+      dialog.current.on('hide', () => {
+        setDeckURL('')
+        setDeck([])
+        setHand([])
+        setError(null)
       })
     }
   }
 
-  setDeck = event => {
+  const defineDeck = React.useCallback(event => {
     const url = event.target.value
 
-    this.setState({ deckURL: url }, () => {
-      try {
-        const [, id] = decodeURIComponent(url).match(
-          /\/deck\/([\w=%]+)(?:\/|$|\?)/i
-        )
+    setDeckURL(url)
 
-        this.setState({ deck: serialisation.deck.deserialise(id), error: '' })
-      } catch (error) {
-        this.setState({
-          error: 'Unfortunately this deck could not be imported.',
-        })
-      }
-    })
-  }
-
-  importDeck = () => {
-    this.props.importDeck({ cards: this.state.deck, hand: this.state.hand })
-    this.close()
-  }
-
-  addToHand = ({ id }) => {
-    if (this.state.hand.includes(id)) {
-      this.setState({ hand: this.state.hand.filter(i => i !== id) })
-    } else if (this.state.hand.length < 4) {
-      this.setState({ hand: [...this.state.hand, id] })
+    try {
+      const re = /\/deck\/([\w=%]+)(?:\/|$|\?)/i
+      const [, id] = decodeURIComponent(url).match(re)
+      setDeck(serialisation.deck.deserialise(id))
+      setError(null)
+    } catch {
+      setError('Unfortunately this deck could not be imported.')
     }
-  }
+  }, [])
 
-  render() {
-    return (
-      <>
-        <CTA
-          type='CTA'
-          onClick={this.open}
-          extend={{
-            margin: 'var(--s-base) auto var(--s-large)',
-            display: 'block',
-          }}
-        >
-          Import deck
-        </CTA>
-        <Dialog
-          id='battle-sim-deck-import'
-          title='Import a deck'
-          dialogRef={this.registerDialog}
-          image='/assets/images/cards/olf_the_hammer.png'
-          close={this.close}
-          ctaProps={{
-            onClick: this.importDeck,
-            children: 'Import deck',
-            disabled: this.state.deck.length === 0,
-          }}
-        >
-          <p>
-            You can import a deck directly from the deck builder: paste its URL
-            in the field below. Once the deck has been succesfully loaded, you
-            can select up to 4 cards to put in your hand.
-          </p>
+  const importDeck = React.useCallback(() => {
+    importDeckFromProps({ cards: deck, hand })
+    dialog.current.hide()
+  }, [importDeckFromProps, deck, hand])
 
-          <Input
-            label='Deck URL'
-            type='url'
-            id='deck'
-            required
-            placeholder='e.g. https://stormbound-kitty.com/deck/NE4xLDRJMSw0TjMsNEk2LDRJOCw0STExLDRJMTUsNEkxMiw0TjI4LDRJMjAsNEkxOSw0STIx'
-            onChange={this.setDeck}
-            value={this.state.deckURL}
-            aria-describedby='deck-import-errors'
+  const addToHand = React.useCallback(({ id }) => {
+    setHand(hand => {
+      if (hand.includes(id)) return hand.filter(i => i !== id)
+      if (hand.length < 4) return [...hand, id]
+      return hand
+    })
+  }, [])
+
+  return (
+    <>
+      <CTA
+        type='CTA'
+        onClick={() => dialog.current.show()}
+        extend={{
+          margin: 'var(--s-base) auto var(--s-large)',
+          display: 'block',
+        }}
+      >
+        Import deck
+      </CTA>
+      <Dialog
+        id='battle-sim-deck-import'
+        title='Import a deck'
+        dialogRef={registerDialog}
+        image='/assets/images/cards/olf_the_hammer.png'
+        close={() => dialog.current.hide()}
+        ctaProps={{
+          onClick: importDeck,
+          children: 'Import deck',
+          disabled: deck.length === 0,
+        }}
+      >
+        <p>
+          You can import a deck directly from the deck builder: paste its URL in
+          the field below. Once the deck has been succesfully loaded, you can
+          select up to 4 cards to put in your hand.
+        </p>
+
+        <Input
+          label='Deck URL'
+          type='url'
+          id='deck'
+          required
+          placeholder='e.g. https://stormbound-kitty.com/deck/NE4xLDRJMSw0TjMsNEk2LDRJOCw0STExLDRJMTUsNEkxMiw0TjI4LDRJMjAsNEkxOSw0STIx'
+          onChange={defineDeck}
+          value={deckURL}
+          aria-describedby='deck-import-errors'
+        />
+        <p id='deck-import-errors'>{error}</p>
+
+        {deck.length > 0 && (
+          <Deck
+            orientation='horizontal'
+            deck={deck}
+            highlightedCards={hand}
+            onClick={addToHand}
+            onClickLabel='Put card in hand'
           />
-          <p id='deck-import-errors'>{this.state.error}</p>
-
-          {this.state.deck.length > 0 && (
-            <Deck
-              orientation='horizontal'
-              deck={this.state.deck}
-              highlightedCards={this.state.hand}
-              onClick={this.addToHand}
-              onClickLabel='Put card in hand'
-            />
-          )}
-        </Dialog>
-      </>
-    )
-  }
-}
-
-export default BattleSimDeckImport
+        )}
+      </Dialog>
+    </>
+  )
+})
