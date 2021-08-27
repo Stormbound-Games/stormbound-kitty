155bfac9160ee4e57bf142fd7d254615972a9efd
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-27 15:25:01 +0200

    Add local font fallback

--- src/helpers/renderRootStyles/index.js
+++ src/helpers/renderRootStyles/index.js
@@ -3,6 +3,7 @@ import { SPACING_TOKENS } from '~/styles/variables'
 const renderRootStyles = renderer => {
   renderer.renderFont('Nunito', ['/assets/fonts/nunito-700.woff2'], {
     fontDisplay: 'swap',
+    localAlias: ['Nunito'],
     unicodeRange:
       'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
   })
@@ -10,6 +11,7 @@ const renderRootStyles = renderer => {
   renderer.renderFont('Nunito', ['/assets/fonts/nunito-900.woff2'], {
     fontWeight: 'bold',
     fontDisplay: 'swap',
+    localAlias: ['Nunito', 'Nunito Bold', 'Nunito-Bold'],
     unicodeRange:
       'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
   })
