c889e5253da7e6842b5e996b3533f4df12166012
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-30 18:46:05 +0200

    Enable SSR for the popular decks page to handle query at runtime

--- src/pages/deck/suggestions/index.js
+++ src/pages/deck/suggestions/index.js
@@ -4,8 +4,15 @@ import Layout from '~/components/Layout'
 import DECKS from '~/data/decks'
 import getNavigation from '~/helpers/getNavigation'
 
-export async function getStaticProps() {
-  return { props: { navigation: getNavigation(), decks: DECKS } }
+// This page uses server-side rendering instead of static rendering because it
+// receives query parameters (for filtering) that need to be handled on the
+// server. This matters both to avoid decks flashing when JavaScript mounts but
+// also because the meta tags are based on the query parameters so the Discord
+// embeds look alright.
+export async function getServerSideProps() {
+  return {
+    props: { navigation: getNavigation(), decks: DECKS },
+  }
 }
 
 const DeckSuggestionsPage = ({ navigation, ...props }) => (
