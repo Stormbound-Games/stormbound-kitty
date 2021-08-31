b0f0446614ae519c7cfbcd89fffe9d7adccde153
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-08-31 17:59:42 +0200

    Adjust formula for Brawl discounted costs

--- src/helpers/getMilestoneCost/index.js
+++ src/helpers/getMilestoneCost/index.js
@@ -1,3 +1,6 @@
+const roundToUpper5 = value => Math.ceil(Math.ceil(value) / 5) * 5
+const roundToLower5 = value => Math.floor(Math.floor(value) / 5) * 5
+
 const getMilestoneCost = (milestone, costModifier = 1) => {
   // In some instances, the Premium Pass discount is artificially reduced or
   // increased to yield a certain result. Since it is not possible to retreive
@@ -6,15 +9,19 @@ const getMilestoneCost = (milestone, costModifier = 1) => {
   if (costModifier === 0.9 && typeof milestone.ppCost === 'number')
     return milestone.ppCost
 
-  const cost = Math.ceil(Math.ceil(milestone.cost * costModifier) / 5) * 5
+  const cost = milestone.cost * costModifier
+  const discountedCost = roundToUpper5(cost)
 
-  // If there is a discount (cost modifier below 1), but the cost remains the
-  // same, force a discount by rounding the cost the other way around.
-  if (costModifier < 1 && cost === milestone.cost) {
-    return Math.floor(Math.floor(milestone.cost * costModifier) / 5) * 5
+  // If there is a discount (cost modifier below 1), but the discounted cost
+  // remains the same as the original cost, force a discount by rounding the
+  // cost the other way around. That is, unless it would yield `0`, in which
+  // case keep the original cost as it is. This is because it was decided that
+  // only the very first milestone should be free.
+  if (costModifier < 1 && discountedCost === milestone.cost) {
+    return roundToLower5(cost) || discountedCost
   }
 
-  return cost
+  return discountedCost
 }
 
 export default getMilestoneCost
