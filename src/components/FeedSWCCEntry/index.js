a2f1c798522d46042e9eda95e344f97a4c1ff9b0
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-09-06 10:26:20 +0200

    Fix wording in SWCC entry feed

--- src/components/FeedSWCCEntry/index.js
+++ src/components/FeedSWCCEntry/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Link from '~/components/Link'
 import FeedEntry from '~/components/FeedEntry'
+import toSentence from '~/helpers/toSentence'
+
+const toArray = value => (Array.isArray(value) ? value : [value])
 
 export default React.memo(function FeedSWCCEntry(props) {
+  const authors = props.authors || toArray(props.author)
+  const verb = authors.length === 1 ? 'has' : 'have'
+
   return (
     <FeedEntry icon='wand' date={props.date}>
-      {props.author} has started a new season of the{' '}
+      {toSentence(authors)} {verb} started a new season of the{' '}
       <Link to='/card/contest'>Stormbound Weekly Card Contest</Link> (SWCC for
       short).
     </FeedEntry>
