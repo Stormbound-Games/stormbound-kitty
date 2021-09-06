074a614536498e3ceb43e918c3962e1e2b14fe44
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-09-06 10:36:56 +0200

    Prevent React key issues with teasers

--- src/components/Teasers/index.js
+++ src/components/Teasers/index.js
@@ -7,9 +7,12 @@ export default React.memo(function Teasers(props) {
   const columns = props.columns || 3
 
   return chunk(props.items, columns).map(row => (
-    <Row key={row.map(item => item.title).join('+')} isDesktopOnly>
+    <Row key={row.map(item => item.id || item.title).join('+')} isDesktopOnly>
       {Array.from({ length: columns }, (_, index) => (
-        <Row.Column key={row[index]?.title ?? index} width='1/3'>
+        <Row.Column
+          key={row[index]?.id ?? row[index]?.title ?? index}
+          width='1/3'
+        >
           {row[index] && <Teaser {...row[index]} />}
         </Row.Column>
       ))}
