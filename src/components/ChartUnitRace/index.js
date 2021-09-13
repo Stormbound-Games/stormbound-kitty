1727f975fae6bb59648893f257db2fb5ab3735ac
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-09-13 21:42:46 +0200

    Simplify the logic of the race chart

--- src/components/ChartUnitRace/index.js
+++ src/components/ChartUnitRace/index.js
@@ -9,36 +9,30 @@ import {
 } from 'recharts'
 import Title from '~/components/Title'
 import { TOOLTIP_STYLES } from '~/constants/stats'
-import card from '~/helpers/serialisation/card'
-import { TYPES, RACES, RARITIES } from '~/constants/game'
+import capitalise from '~/helpers/capitalise'
+import countCards from '~/helpers/countCards'
 
 export default React.memo(function ChartUnitRace(props) {
-
-  const races = ['ancient', 'dragon','feline', 'knight', 'pirate', 'dwarf', 'frostling', 'construct', 'rodent', 'raven', 'toad', 'satyr', 'undead']
-  const data = props.cards
-    .filter(card => !card.token)
-    .reduce(
-      (acc, card) => {
-        if (card.race == null) return acc
-        acc[races.indexOf(card.race)].value++
-        return acc
-      },
-      [
-        { name: 'Ancient', value: 0, color: '#96d2cf' },      
-        { name: 'Dragon', value: 0, color: '#ad5b8f' },      
-        { name: 'Feline', value: 0, color: '#f9a2bf' },
-        { name: 'Knight', value: 0, color: '#d2cfc8' },
-        { name: 'Pirate', value: 0, color: '#fade7d' },
-        { name: 'Dwarf', value: 0, color: '#f59138' },
-        { name: 'Frostling', value: 0, color: '#71c7e2' },
-        { name: 'Construct', value: 0, color: '#ef553c' },
-        { name: 'Rodent', value: 0, color: '#f1815c' },
-        { name: 'Raven', value: 0, color: '#589651' },
-        { name: 'Toad', value: 0, color: '#a4bf43' },
-        { name: 'Satyr', value: 0, color: '#f4ad84' },
-        { name: 'Undead', value: 0, color: '#d4dfe3' }
-      ]
-    )
+  const RACES = {
+    frostling: 'var(--winter)',
+    dwarf: 'var(--light-winter)',
+    construct: 'var(--ironclad)',
+    rodent: 'var(--light-ironclad)',
+    raven: 'var(--shadowfen)',
+    toad: 'var(--light-shadowfen)',
+    undead: 'var(--swarm)',
+    satyr: 'var(--light-swarm)',
+    knight: 'var(--neutral)',
+    pirate: 'var(--affordable)',
+    ancient: 'var(--upgradable)',
+    dragon: 'var(--dark-beige)',
+    feline: 'var(--beige)',
+  }
+  const data = Object.keys(RACES).map(race => ({
+    name: capitalise(race),
+    value: countCards({ race }, false),
+    color: RACES[race],
+  }))
 
   return (
     <>
