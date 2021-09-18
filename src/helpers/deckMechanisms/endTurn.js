bc5101d0b6ce2e44312c2cf9f3648bd6ac6e3cb0
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-09-18 17:16:20 +0200

    Implement Reserve Management and The Great Mill Brawl modifiers in the dry runner

--- src/helpers/deckMechanisms/endTurn.js
+++ src/helpers/deckMechanisms/endTurn.js
@@ -2,14 +2,27 @@ import { DEFAULT_MANA } from '~/constants/battle'
 import resolveManaRNG from './resolveManaRNG'
 import refill from './refill'
 
+export const DEFAULT_END_TURN_OPTIONS = {
+  modifier: null,
+}
+
 /**
  * Mutate the given state following a turn.
  * @param {Object} state - State being mutated
+ * @param {Object} opts - End turn options
+ * @param {Boolean} [opts.modifier = null] - Active Brawl modifier
  * @return {Object} Mutated state
  */
-const endTurn = state => {
-  // Reset the mana to 3 + the current turn
-  state.mana = DEFAULT_MANA + state.turn
+const endTurn = (state, opts) => {
+  const options = { ...DEFAULT_END_TURN_OPTIONS, ...opts }
+
+  // Reset the mana to 3 + the current turn, unless the “Reserve Management”
+  // Brawl modifier is active, in which case carry the leftover mana over.
+  if (options.modifier === 'RESERVE_MANAGEMENT') {
+    state.mana += DEFAULT_MANA + state.turn
+  } else {
+    state.mana = DEFAULT_MANA + state.turn
+  }
 
   // Increment the current turn by 1
   state.turn += 1
