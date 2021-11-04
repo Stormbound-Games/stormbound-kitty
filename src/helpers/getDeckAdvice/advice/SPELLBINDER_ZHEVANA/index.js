a459af67c58243d6fe80d72881d6443480493a9c
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2021-11-04 18:33:27 +0100

    Update Inefficient Spellbinder Zhevana’s deck advice to include more freeze cards

--- src/helpers/getDeckAdvice/advice/SPELLBINDER_ZHEVANA/index.js
+++ src/helpers/getDeckAdvice/advice/SPELLBINDER_ZHEVANA/index.js
@@ -1,20 +1,24 @@
-export default cards => {
-  const cardIds = cards.map(card => card.id)
-  const hasSpellbinderZhevana = cardIds.includes('W8')
-  const hasMidwinterChaos = cardIds.includes('W11')
-  const hasMomentsPeace = cardIds.includes('W6')
+const FREEZE_CARD_IDS = ['W1', 'W2', 'W6', 'W11', 'W32']
+const containsFreeze = deck =>
+  deck.map(card => card.id).some(id => FREEZE_CARD_IDS.includes(id))
 
-  // For Spellbinder Zhevana to be considered efficient, she needs frozen areas
-  // which can be provided by Midwinter Chaos or Moment’s Peace (but not
-  // Frosthexers).
-  if (!hasSpellbinderZhevana || hasMidwinterChaos || hasMomentsPeace) {
+const advice = cards => {
+  const cardIds = cards.map(card => card.id)
+  const hasFreezingCards = containsFreeze(cards)
+  const hasSpellbinderZhevana = cardIds.includes('W8')
+
+  // For Spellbinder Zhevana to be considered efficient, she needs freezing
+  // cards.
+  if (!hasSpellbinderZhevana || hasFreezingCards) {
     return null
   }
 
   return {
     name: 'Inefficient Spellbinder Zhevana',
     description:
-      "This deck includes Spellbinder Zhevana but doesn’t include efficient freeze cards. Consider including Moment's Peace or Midwinter Chaos.",
-    highlight: ['W8', 'W11', 'W6'],
+      'This deck includes Spellbinder Zhevana but doesn’t include efficient freeze cards. Consider including cards which can freeze units.',
+    highlight: FREEZE_CARD_IDS,
   }
 }
+
+export default advice
--- src/helpers/getDeckAdvice/advice/SPELLBINDER_ZHEVANA/spec.js
+++ src/helpers/getDeckAdvice/advice/SPELLBINDER_ZHEVANA/spec.js
@@ -24,6 +24,21 @@ describe('The `SPELLBINDER_ZHEVANA` advice', () => {
     expect(advice(cards)).toEqual(null)
   })
 
+  it('should not be returned if it has Icicle Burst', () => {
+    const cards = getCards('1n11w11w21n51n81n121w281w81w11w161n461n52')
+    expect(advice(cards)).toEqual(null)
+  })
+
+  it('should not be returned if it has Blizzard Bombs', () => {
+    const cards = getCards('1n11w11w21n51n81n121w281w81w321w161n461n52')
+    expect(advice(cards)).toEqual(null)
+  })
+
+  it('should not be returned if it has Frosthexers', () => {
+    const cards = getCards('1n11w11w21n51n81n121w281w81w21w161n461n52')
+    expect(advice(cards)).toEqual(null)
+  })
+
   it('should be returned if Zhevana is deemed inefficient', () => {
     const cards = getCards('1n501w191n621w171n701n51n631n141w81w281n151n41')
     expect(advice(cards)).not.toEqual(null)
