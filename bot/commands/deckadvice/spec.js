85de406182cbc2809f721cb384499aa4f5848f9b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-01-31 18:56:40 +0100

    Update some tests

--- bot/commands/deckadvice/spec.js
+++ bot/commands/deckadvice/spec.js
@@ -3,12 +3,12 @@ const deckadvice = command.handler.bind(command)
 
 describe('Bot — !deckadvice', () => {
   it('should return nothing for a missing term', () => {
-    deckadvice('').then(output => expect(output).toEqual(undefined))
+    return deckadvice('').then(output => expect(output).toEqual(undefined))
   })
 
   it('should handle a Stormbound-Kitty deck ID', () => {
-    deckadvice('5n35n125n163w54n184w95w125w133n394w153w194w21').then(output =>
-      expect(output).not.toEqual(undefined)
+    return deckadvice('5n35n125n163w54n184w95w125w133n394w153w194w21').then(
+      output => expect(output).not.toEqual(undefined)
     )
   })
 
@@ -16,28 +16,33 @@ describe('Bot — !deckadvice', () => {
     const url =
       'https://stormbound-kitty.com/deck/5n35n125n163w54n184w95w125w133n394w153w194w21'
 
-    deckadvice(url).then(output => expect(output).not.toEqual(undefined))
-    deckadvice(url + '/detail').then(output =>
-      expect(output).not.toEqual(undefined)
-    )
-    deckadvice(url + '/dry-run').then(output =>
-      expect(output).not.toEqual(undefined)
-    )
+    return Promise.all([
+      deckadvice(url).then(output => expect(output).not.toEqual(undefined)),
+      deckadvice(url + '/detail').then(output =>
+        expect(output).not.toEqual(undefined)
+      ),
+      deckadvice(url + '/dry-run').then(output =>
+        expect(output).not.toEqual(undefined)
+      ),
+    ])
   })
 
   it('should handle a lack of advice', () => {
-    deckadvice('5n31n45n121n145n163w54n181n94w95w125w133w19').then(output =>
-      expect(output.description).toContain('No particular suggestions')
+    return deckadvice('5n31n41n95n121n143w54n185w74w95w125w133w19').then(
+      output =>
+        expect(output.description).toContain('No particular suggestions')
     )
   })
 
   it('should return advice', () => {
-    deckadvice('3n631n703n335n133n415n193n592w102n614n225w23n7').then(output =>
-      expect(output.fields.length > 0).toEqual(true)
+    return deckadvice('3n631n703n335n133n415n193n592w102n614n225w23n7').then(
+      output => expect(output.fields.length > 0).toEqual(true)
     )
   })
 
   it('should return nothing for a no-match', () => {
-    deckadvice('flksdjf').then(output => expect(output).toEqual(undefined))
+    return deckadvice('flksdjf').then(output =>
+      expect(output).toEqual(undefined)
+    )
   })
 })
--- src/helpers/getDeckAdvice/advice/MANA_CURVE/spec.js
+++ src/helpers/getDeckAdvice/advice/MANA_CURVE/spec.js
@@ -11,16 +11,16 @@ const getCards = (id, modifier = 'NONE') =>
 describe('The `MANA_CURVE` advice', () => {
   it('should be returned if the mana curve is deemed to high', () => {
     const cards = getCards('5n185n195n595n205n215n225n755n415n645n245n715n65')
-    advice(cards).then(output => expect(output).not.toEqual(null))
+    return advice(cards).then(output => expect(output).not.toEqual(null))
   })
 
   it('should not be returned if the mana curve is deemed alright', () => {
     const cards = getCards('5n15n25w25n35n45n124w34w44w65n285w124w14')
-    advice(cards).then(output => expect(output).toEqual(null))
+    return advice(cards).then(output => expect(output).toEqual(null))
   })
 
   it('should not be returned if the deck average level is below 2.5', () => {
     const cards = getCards('1n11i11i61i81n151i111i131n211i141i161i211i22')
-    advice(cards).then(output => expect(output).toEqual(null))
+    return advice(cards).then(output => expect(output).toEqual(null))
   })
 })
