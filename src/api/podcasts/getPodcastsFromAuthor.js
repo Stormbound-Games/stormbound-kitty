b0dcef0250eff5e247a8b7f2c76046918614a13e
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-21 15:17:45 +0100

    Adjust a GROQ query

--- src/api/podcasts/getPodcastsFromAuthor.js
+++ src/api/podcasts/getPodcastsFromAuthor.js
@@ -1,11 +1,14 @@
-import getPodcasts from './getPodcasts'
+import { getEntries } from '~/helpers/sanity'
+import clean from './clean'
 
 const getPodcastsFromAuthor = async author => {
-  const podcasts = await getPodcasts()
+  const podcasts = await getEntries({
+    conditions: ['_type == "podcast"', 'count(hosts[lower(@) == $author]) > 0'],
+    params: { author },
+    options: { order: 'date asc' },
+  })
 
-  return podcasts.filter(podcast =>
-    podcast.hosts.map(host => host.toLowerCase()).includes(author)
-  )
+  return podcasts.map(clean)
 }
 
 export default getPodcastsFromAuthor
