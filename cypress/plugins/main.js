a2ff84d750faa09559fb2c3779443e5a62a18f7a
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-21 12:45:49 +0100

    Avoid using fs/promises

--- cypress/plugins/main.js
+++ cypress/plugins/main.js
@@ -1,7 +1,18 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 import path from 'path'
 import preprocessor from '@cypress/webpack-preprocessor'
 import getSearchIndex from '~/helpers/getSearchIndex'
+
+// For some reason I get a ` Cannot find module 'fs/promises'` error on Cypress
+// so we use a custom promise wrapper. Silly but heh, I don’t have the will to
+// investigate.
+const writeFile = (path, content) =>
+  new Promise((resolve, reject) => {
+    fs.writeFile(path, content, 'utf8', error => {
+      if (error) reject(error)
+      else resolve()
+    })
+  })
 
 const configuration = (on, config) => {
   // Retrieve the path registry for the route tests and write it as a fixture
@@ -11,7 +22,7 @@ const configuration = (on, config) => {
       const filePath = path.resolve('cypress', 'fixtures', 'registry.json')
       const payload = JSON.stringify(registry, null, 2)
 
-      return fs.writeFile(filePath, payload, 'utf8')
+      return writeFile(filePath, payload)
     })
     .then(() => {
       on(
