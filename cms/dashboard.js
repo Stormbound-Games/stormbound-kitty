60e52756c2ff9d646106ea3994c15a79e4a5becb
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-23 15:13:52 +0100

    Set up the Sanity dashboard

---
+++ cms/dashboard.js
@@ -0,0 +1,16 @@
+const dashboard = {
+  widgets: [
+    {
+      name: 'vercel',
+      layout: { width: 'full' },
+    },
+    {
+      name: 'project-info',
+    },
+    {
+      name: 'project-users',
+    },
+  ],
+}
+
+export default dashboard
