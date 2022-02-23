eba17e04207ecd6c5dd08da0b22b4bd50a6d71c7
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-23 09:58:02 +0100

    Reorder podcast episodes

--- src/api/podcasts/getPodcasts.js
+++ src/api/podcasts/getPodcasts.js
@@ -4,7 +4,7 @@ import clean from './clean'
 const getPodcasts = async ({ isPreview } = {}) => {
   const podcasts = await getEntries({
     conditions: ['_type == "podcast"'],
-    options: { order: 'date desc', isPreview },
+    options: { order: ['date desc, _createdAt desc'], isPreview },
   })
 
   return podcasts.map(clean)
