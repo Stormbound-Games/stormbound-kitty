e593141ca8684c162ef6feac7ff180721a182ede
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-23 12:22:07 +0100

    Remove rogue console.log

--- cms/webpack.sanity.js
+++ cms/webpack.sanity.js
@@ -4,7 +4,5 @@ module.exports = function (config, options) {
   config.resolve.alias['~'] = path.resolve(__dirname, '..', 'src')
   config.resolve.extensions.push('.json')
 
-  console.log(config, options)
-
   return config
 }
