70ee5429d88b2fe1af8423551b2dbc1478093a22
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-24 18:04:09 +0100

    Add automatic linking of card names

--- src/components/Embellish/index.js
+++ src/components/Embellish/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import iconify from '~/helpers/iconify'
+import replaceInString from '~/helpers/replaceInString'
+import getRawCardData from '~/helpers/getRawCardData'
+import CardLink from '~/components/CardLink'
+import cards from '~/data/cards'
+
+const CARDS_RE = new RegExp(
+  '(' +
+    cards
+      .filter(card => !card.token)
+      .map(card => card.name)
+      .join('|') +
+    ')',
+  'g'
+)
 
 export default React.memo(function Embellish(props) {
   const Component = props.as || React.Fragment
+  const content = replaceInString(
+    iconify(props.children),
+    CARDS_RE,
+    (match, index) => {
+      const { id } = getRawCardData(match, 'name')
+      return id ? <CardLink id={id} key={id + index} /> : match
+    }
+  )
 
-  return <Component>{iconify(props.children)}</Component>
+  return <Component>{content}</Component>
 })
