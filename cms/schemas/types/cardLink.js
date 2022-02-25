3d97f30f6219d9d0d70ca5486956fdf2998f0e0a
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-25 20:12:37 +0100

    Add note about automatic card links

--- cms/schemas/types/cardLink.js
+++ cms/schemas/types/cardLink.js
@@ -4,7 +4,13 @@ const cardLink = {
   title: 'Card link',
   name: 'cardLink',
   type: 'object',
-  fields: [cardId],
+  fields: [
+    {
+      ...cardId,
+      description:
+        'Note: card names are automatically linked provided their spelling match the official one.',
+    },
+  ],
 }
 
 export default cardLink
