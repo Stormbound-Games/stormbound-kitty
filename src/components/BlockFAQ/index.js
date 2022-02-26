872b2452520bc57e0591cabed40487fed3d74e1b
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-26 15:36:10 +0100

    Make FAQ block more resilient

--- src/components/BlockFAQ/index.js
+++ src/components/BlockFAQ/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import FAQSection from '~/components/FAQSection'
 
 export default React.memo(function BlockFAQ(props) {
-  return <FAQSection id='faq' title='FAQ' entries={props.value.entries} />
+  return <FAQSection id='faq' title='FAQ' entries={props.value.entries || []} />
 })
