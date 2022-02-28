15c2eff60e1ee688fde126ae9c319626a9deafde
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-02-28 22:27:55 +0100

    Remove the cards JSON database

--- cms/schemas/richText/card.js
+++ cms/schemas/richText/card.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { MdPadding } from 'react-icons/md'
 import cardRef from '../types/cardRef'
-import cards from '~/data/cards'
 
 const card = {
   title: 'Card',
@@ -9,16 +8,6 @@ const card = {
   type: 'object',
   icon: MdPadding,
   fields: [
-    {
-      title: 'Card',
-      name: 'cardId',
-      type: 'string',
-      options: {
-        list: cards
-          .map(card => ({ title: card.name, value: card.id }))
-          .sort((a, b) => a.title.localeCompare(b.title)),
-      },
-    },
     { ...cardRef, weak: true },
     {
       title: 'Level',
