06889ba985e3fbf192bd98591d2b2a1b2d5d8d02
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-01 12:41:48 +0100

    Remove cards data from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,12 @@ import Home from '~/components/Home'
 import Layout from '~/components/Layout'
 import getNews from '~/api/news/getNews'
 import getNavigation from '~/helpers/getNavigation'
-import getCards from '~/api/cards/getCards'
 
 export async function getStaticProps({ preview: isPreview = false }) {
-  const cards = await getCards({ isPreview })
   const navigation = await getNavigation({ isPreview })
   const news = await getNews({ isPreview })
 
-  return { props: { cards, news, navigation } }
+  return { props: { news, navigation } }
 }
 
 const Index = ({ navigation, cards, ...props }) => (
