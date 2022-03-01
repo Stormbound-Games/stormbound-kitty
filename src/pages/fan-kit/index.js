43de8b23f47856a6234376f43271bdd5b6d8d309
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-01 13:13:45 +0100

    Limit cards data on fan-kit page

--- src/pages/fan-kit/index.js
+++ src/pages/fan-kit/index.js
@@ -4,11 +4,18 @@ import Layout from '~/components/Layout'
 import getNavigation from '~/helpers/getNavigation'
 import getCards from '~/api/cards/getCards'
 
+const CARD_IDS = ['N23', 'N73', 'N22', 'N84', 'N21', 'N83']
+
 export async function getStaticProps({ preview: isPreview = false }) {
   const cards = await getCards({ isPreview })
   const navigation = await getNavigation({ isPreview })
 
-  return { props: { cards, navigation } }
+  return {
+    props: {
+      cards: cards.filter(card => CARD_IDS.includes(card.id)),
+      navigation,
+    },
+  }
 }
 
 const FanKitPage = ({ navigation, cards, ...props }) => (
