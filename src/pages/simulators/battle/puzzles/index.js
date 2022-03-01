8835a895368bb5ddc23336a94d6f843cc30bb0f0
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-01 12:49:12 +0100

    Remove cards data from puzzles page

--- src/pages/simulators/battle/puzzles/index.js
+++ src/pages/simulators/battle/puzzles/index.js
@@ -3,14 +3,12 @@ import BattleSimPuzzles from '~/components/BattleSimPuzzles'
 import Layout from '~/components/Layout'
 import getPuzzles from '~/api/puzzles/getPuzzles'
 import getNavigation from '~/helpers/getNavigation'
-import getCards from '~/api/cards/getCards'
 
 export async function getStaticProps({ preview: isPreview = false }) {
-  const cards = await getCards({ isPreview })
   const navigation = await getNavigation({ isPreview })
   const puzzles = await getPuzzles({ isPreview })
 
-  return { props: { cards, navigation, puzzles } }
+  return { props: { navigation, puzzles } }
 }
 
 const BattleSimPuzzlesPage = ({ navigation, cards, ...props }) => (
