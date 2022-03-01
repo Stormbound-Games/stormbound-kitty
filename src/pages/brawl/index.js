f1053d3384965677c56af2f8f63471d6b2a625a8
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-01 13:20:49 +0100

    Limit cards data on brawl index page

--- src/pages/brawl/index.js
+++ src/pages/brawl/index.js
@@ -3,12 +3,20 @@ import BrawlIndex from '~/components/BrawlIndex'
 import Layout from '~/components/Layout'
 import getNavigation from '~/helpers/getNavigation'
 import getCards from '~/api/cards/getCards'
+import { BRAWLS } from '~/constants/brawl'
+
+const CARD_IDS = BRAWLS.map(brawl => brawl.cardId)
 
 export async function getStaticProps({ preview: isPreview = false }) {
   const cards = await getCards({ isPreview })
   const navigation = await getNavigation({ isPreview })
 
-  return { props: { cards, navigation } }
+  return {
+    props: {
+      cards: cards.filter(card => CARD_IDS.includes(card.id)),
+      navigation,
+    },
+  }
 }
 
 const BrawlIndexPage = ({ navigation, cards, ...props }) => (
