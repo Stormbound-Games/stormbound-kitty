6b552ef469f8c232e60a9461799ec13759177eb1
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-04 16:23:25 +0100

    Fix the book calculator

--- src/pages/calculators/books/index.js
+++ src/pages/calculators/books/index.js
@@ -3,12 +3,20 @@ import BooksCalculator from '~/components/BooksCalculator'
 import Layout from '~/components/Layout'
 import getSiteSettings from '~/api/misc/getSiteSettings'
 import getBooks from '~/api/books/getBooks'
+import getCards from '~/api/cards/getCards'
 
 export async function getStaticProps({ preview: isPreview = false }) {
   const settings = await getSiteSettings({ isPreview })
+  const cards = await getCards({ isPreview })
   const books = await getBooks({ isPreview })
 
-  return { props: { settings, books } }
+  return {
+    props: {
+      settings,
+      books,
+      cards: cards.map(card => ({ rarity: card.rarity })),
+    },
+  }
 }
 
 const BooksCalculatorPage = ({ settings, cards, ...props }) => (
