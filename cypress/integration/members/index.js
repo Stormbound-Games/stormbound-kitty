3a8446a93c1178cd36bd83a48d107d857161ba57
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-09 14:24:31 +0100

    Add tests

---
+++ cypress/integration/members/index.js
@@ -0,0 +1,45 @@
+describe('Members page', () => {
+  it('should be possible to record oneself', () => {
+    cy.visit('/members')
+      .get('#user-name')
+      .find('input')
+      .first()
+      .type('Kitt', { force: true })
+      .type('{enter}', { force: true })
+      .get('#user-name')
+      .find('[class$="-singleValue"]')
+      .should('contain', 'Kitty')
+      .saveLocalStorage()
+  })
+
+  it('should be preserved upon reload', () => {
+    cy.restoreLocalStorage()
+      .reload()
+      .get('#user-name')
+      .find('[class$="-singleValue"]')
+      .should('contain', 'Kitty')
+      .saveLocalStorage()
+  })
+
+  it('should be reflected in the navigation', () => {
+    cy.restoreLocalStorage()
+      .wait(1000)
+      .get('header nav > ul > li')
+      .eq(4)
+      .find('button')
+      .first()
+      .contains('Kitty')
+      .next()
+      .find('ul')
+      .children()
+      .should('have.length', 5)
+      .saveLocalStorage()
+  })
+
+  it('should be reflected in one’s feed', () => {
+    cy.restoreLocalStorage()
+      .visit('/members/kitty')
+      .get('h1')
+      .contains('Activity Feed')
+  })
+})
