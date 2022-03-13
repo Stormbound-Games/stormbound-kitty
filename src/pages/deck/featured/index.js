91d1256889e97d1cb4135afbaf3f54e02ceec63e
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-13 10:13:00 +0100

    Add cache to featured decks page

--- src/pages/deck/featured/index.js
+++ src/pages/deck/featured/index.js
@@ -10,7 +10,21 @@ import getSiteSettings from '~/api/misc/getSiteSettings'
 // server. This matters both to avoid decks flashing when JavaScript mounts but
 // also because the meta tags are based on the query parameters so the Discord
 // embeds look alright.
-export async function getServerSideProps({ preview: isPreview = false }) {
+export async function getServerSideProps({
+  res: response,
+  preview: isPreview = false,
+}) {
+  // This value is considered fresh for 1 minute (s-maxage=60). If a request is
+  // repeated within the next 60 seconds, the previously cached value will still
+  // be fresh. If the request is repeated before 6 minutes, the cached value
+  // will be stale but still render (stale-while-revalidate=359). In th
+  // background, a revalidation request will be made to populate the cache with
+  // a fresh value. If you refresh the page, you will see the new value.
+  response.setHeader(
+    'Cache-Control',
+    'public, s-maxage=60, stale-while-revalidate=359'
+  )
+
   const decks = await getDecks({ isPreview })
   const settings = await getSiteSettings({ isPreview })
   const availableTags = await getDeckTags({ isPreview })
