5198f2133fc56d128973cf7b3616280a9f081be9
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-13 11:57:00 +0100

    Fix a typo

--- src/components/FeedReleaseEntry/index.js
+++ src/components/FeedReleaseEntry/index.js
@@ -5,7 +5,7 @@ import FeedEntry from '~/components/FeedEntry'
 export default React.memo(function FeedReleaseEntry(props) {
   return (
     <FeedEntry icon='bullhorn' date={props.date}>
-      {props.user.name} has published official release called titled{' '}
+      {props.user.name} has published official release notes titled{' '}
       <Link to={'/releases/' + props.slug}>{props.title}</Link>.
     </FeedEntry>
   )
