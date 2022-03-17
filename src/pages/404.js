c29f9d8411f67d27012518188f0f16d16d088b38
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-17 18:16:27 +0100

    Track 404 errors in Plausible

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Script from 'next/script'
 import Error from '~/components/Error'
 import Layout from '~/components/Layout'
 import getSiteSettings from '~/api/misc/getSiteSettings'
@@ -13,6 +14,9 @@ export default function Custom404({ settings, ...props }) {
   return (
     <Layout active={[]} settings={settings}>
       <Error {...props} error='404 — Page Not Found' />
+      <Script id='404-tracking' strategy='lazyOnload'>
+        {`if (plausible) plausible("404", { props: { path: document.location.pathname } });`}
+      </Script>
     </Layout>
   )
 }
