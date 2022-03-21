9dc014d8038ccdfed66ab1ef1f8b426655638705
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-21 22:16:37 +0100

    Rework the search dialog

--- src/components/SearchDialog/index.js
+++ src/components/SearchDialog/index.js
@@ -2,36 +2,23 @@ import React from 'react'
 import { useFela } from 'react-fela'
 import { useRouter } from 'next/router'
 import querystring from 'querystring'
-import { useCombobox } from 'downshift'
 import Dialog from '~/components/Dialog'
-import Loader from '~/components/Loader'
 import Icon from '~/components/Icon'
 import Input from '~/components/Input'
-import useDebounce from '~/hooks/useDebounce'
-import useNavigator from '~/hooks/useNavigator'
+import Link from '~/components/Link'
+import FeedEntry from '~/components/FeedEntry'
+import Loader from '~/components/Loader'
 import track from '~/helpers/track'
+import useDebounce from '~/hooks/useDebounce'
+import useSearchShortcut from '~/hooks/useSearchShortcut'
 import styles from './styles'
 
-const isSearchShortcut = event => {
-  const { key, metaKey } = event
-  const isTyping = ['TEXTAREA', 'INPUT'].includes(
-    document.activeElement.tagName
-  )
+async function runSearch(search) {
+  const query = querystring.stringify({ search })
+  const response = await fetch('/api/search?' + query)
+  const results = await response.json()
 
-  return (!isTyping && key === '/') || (metaKey && key === 'k')
-}
-
-const useSearchKeyboardShortcut = setIsOpen => {
-  const handleKeyUp = React.useCallback(
-    event => isSearchShortcut(event) && setIsOpen(isOpen => !isOpen),
-    [setIsOpen]
-  )
-
-  React.useEffect(() => {
-    document.addEventListener('keydown', handleKeyUp)
-
-    return () => document.removeEventListener('keydown', handleKeyUp)
-  }, [handleKeyUp])
+  return results.map(entry => ({ ...entry.item, score: entry.score }))
 }
 
 const Breadcrumbs = React.memo(function Breadcrumbs(props) {
@@ -51,47 +38,26 @@ const Breadcrumbs = React.memo(function Breadcrumbs(props) {
   }, [])
 })
 
-const Option = React.memo(function Option(props) {
-  const { css } = useFela()
-
-  return (
-    <span className={css(styles.option)}>
-      {props.label}
-      {props.breadcrumbs.length > 0 && (
-        <span className={css(styles.meta)}>
-          In <Breadcrumbs breadcrumbs={props.breadcrumbs} />
-        </span>
-      )}
-    </span>
-  )
-})
-
-async function runSearch(search) {
-  const query = querystring.stringify({ s: search })
-  const response = await fetch('/api/search?' + query)
-  return response.json()
-}
-
 export default React.memo(function SearchDialog(props) {
-  const { css } = useFela()
   const router = useRouter()
-  const navigator = useNavigator()
+  const { css } = useFela()
   const input = React.useRef(null)
   const [search, setSearch] = React.useState('')
   const debouncedSearch = useDebounce(search, 500)
   const [results, setResults] = React.useState([])
   const [isLoading, setIsLoading] = React.useState(false)
 
-  useSearchKeyboardShortcut(() => props.dialogRef.current?.show())
+  useSearchShortcut(() => props.dialogRef.current?.show())
 
-  // Update search results when the search term changes.
   React.useEffect(() => {
-    if (debouncedSearch) {
+    if (debouncedSearch && debouncedSearch.length >= 3) {
       track('site_search', { search: debouncedSearch })
       runSearch(debouncedSearch).then(results => {
         setResults(results)
         setIsLoading(false)
       })
+    } else {
+      setIsLoading(false)
     }
   }, [debouncedSearch])
 
@@ -102,36 +68,6 @@ export default React.memo(function SearchDialog(props) {
     // eslint-disable-next-line
   }, [router.asPath])
 
-  // Function executed when selecting a result to navigate to its path.
-  const handleNavigation = React.useCallback(
-    ({ selectedItem }) => {
-      if (selectedItem) {
-        setIsLoading(true)
-        navigator.push(selectedItem.path)
-      }
-    },
-    [navigator]
-  )
-
-  const {
-    getComboboxProps,
-    getInputProps,
-    getItemProps,
-    getMenuProps,
-    highlightedIndex,
-    isOpen,
-    selectedItem,
-    reset,
-  } = useCombobox({
-    items: results,
-    onInputValueChange: ({ inputValue }) => {
-      setIsLoading(inputValue.length > 0)
-      setSearch(inputValue)
-    },
-    onSelectedItemChange: handleNavigation,
-    itemToString: item => (item ? item.label : ''),
-  })
-
   const registerDialog = instance => {
     props.dialogRef.current = instance
 
@@ -139,72 +75,70 @@ export default React.memo(function SearchDialog(props) {
       // Push the focus at the end of the event queue to avoid having `/` being
       // filled inside the field when opening the search dialog with `/`.
       instance.on('show', () => setTimeout(() => input.current.focus(), 0))
-      // Clean up the local state when closing the dialog (either manually or
-      // after having navigated to a result).
-      instance.on('hide', () => reset())
     }
   }
-
-  const comboboxProps = getComboboxProps(
-    { className: css({ position: 'relative' }) },
-    { suppressRefError: true }
-  )
-  const menuProps = getMenuProps({}, { suppressRefError: true })
-  const inputProps = getInputProps(
-    {
-      hideLabel: true,
-      label: 'Search Stormbound-Kitty',
-      placeholder: 'e.g. calculator',
-      type: 'text',
-      id: 'search',
-      ref: input,
-      'data-testid': 'search-input',
-    },
-    { suppressRefError: true }
-  )
 
   return (
     <Dialog
       id='search-dialog'
-      title='Search'
+      title='Site search'
       dialogRef={registerDialog}
       close={() => props.dialogRef.current?.hide()}
       image='https://cdn.sanity.io/images/5hlpazgd/production/651e45679d864ebc514bbcd66757022ad8eee282-419x449.png'
       imageWidth={200}
       imageHeight={214}
+      extend={{
+        dialog: styles.dialog,
+        body: styles.body,
+      }}
     >
-      <div className={css(styles.body)}>
-        <div {...comboboxProps}>
-          <Input {...inputProps} />
-          {isLoading && (
-            <Loader
-              hideLabel
-              extend={styles.inputLoaderContainer}
-              extendSvg={styles.inputLoader}
-            />
-          )}
-        </div>
-        <ul {...menuProps} className={css(styles.list({ isOpen }))}>
-          {isOpen &&
-            results.map((item, index) => (
-              <li
-                key={item.path}
-                className={css(
-                  styles.item({
-                    isHighlighted: highlightedIndex === index,
-                    isSelected: selectedItem === item,
-                  })
-                )}
-                {...getItemProps({ index, item })}
-                data-testid='search-result'
+      <div className={css(styles.inputWrapper)}>
+        <Input
+          ref={input}
+          label='Search the site'
+          placeholder='e.g. Sparkly Kitties'
+          id='search'
+          value={search}
+          onChange={event => {
+            setIsLoading(true)
+            setSearch(event.target.value)
+          }}
+          data-testid='search-input'
+          hideLabel
+          autoFocus
+        />
+        {isLoading && (
+          <Loader
+            hideLabel
+            extend={styles.inputLoaderContainer}
+            extendSvg={styles.inputLoader}
+          />
+        )}
+      </div>
+      <div>
+        <ul className={css(styles.list)}>
+          {results.map(result => (
+            <li key={result.path} className={css(styles.item)}>
+              <FeedEntry
+                icon={result.icon}
+                right={
+                  <span className={css({ opacity: 0.6 })}>
+                    Rlv.
+                    <br />
+                    {(1 - result.score).toFixed(2)}
+                  </span>
+                }
               >
-                <Option {...item} />
-              </li>
-            ))}
+                <Link to={result.path}>{result.label}</Link>
+                <span className={css(styles.meta)}>
+                  In <Breadcrumbs breadcrumbs={result.breadcrumbs} />
+                </span>
+              </FeedEntry>
+            </li>
+          ))}
         </ul>
       </div>
-
-      <p className={css({ marginBottom: 0 })}>
+      <p className={css(styles.hint)}>
         Psst! Next time, you can use <kbd>/</kbd> or <kbd>CTRL</kbd> +{' '}
         <kbd>k</kbd> to quickly open the search from anywhere.
       </p>
