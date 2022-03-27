577935dfacb86fcce9eb22f46608b809e92aa3a0
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-27 11:28:37 +0200

    Simplify render of ‘Stories’

--- src/components/Stories/index.js
+++ src/components/Stories/index.js
@@ -10,50 +10,52 @@ import { formatDate } from '~/helpers/formatDate'
 export default React.memo(function Stories(props) {
   const { cardsIndex } = React.useContext(CardsContext)
 
-  return (
-    <>
-      {props.layout === 'GRID' ? (
-        <Teasers
-          items={props.stories.map(story => {
-            const card = {
-              ...getResolvedCardData(cardsIndex, {
-                id: story.cardId,
-                level: 1,
-              }),
-              ...story.card,
-            }
-            const meta = (
-              <>
-                By{' '}
-                <Link to={'/members/' + story.author.slug}>
-                  {story.author.name}
-                </Link>{' '}
-                in {formatDate(parseDate(story.date))}
-              </>
-            )
+  if (props.layout === 'GRID') {
+    return (
+      <Teasers
+        items={props.stories.map(story => {
+          const card = {
+            ...getResolvedCardData(cardsIndex, {
+              id: story.cardId,
+              level: 1,
+            }),
+            ...story.card,
+          }
+          const meta = (
+            <>
+              By{' '}
+              <Link to={'/members/' + story.author.slug}>
+                {story.author.name}
+              </Link>{' '}
+              in {formatDate(parseDate(story.date))}
+            </>
+          )
 
-            return {
-              ...story,
-              card,
-              meta,
-              excerpt: story.excerpt,
-              to: `/stories/${story.slug}`,
-            }
-          })}
-        />
-      ) : props.layout === 'LIST' ? (
-        props.stories.map(story => (
-          <ListLayoutItem
-            key={story.title}
-            date={story.date}
-            title={story.title}
-            author={story.author}
-            icon='quill'
-            path={`/stories/${story.slug}`}
-            excerpt={story.excerpt}
-          />
-        ))
-      ) : null}
-    </>
-  )
+          return {
+            ...story,
+            card,
+            meta,
+            excerpt: story.excerpt,
+            to: `/stories/${story.slug}`,
+          }
+        })}
+      />
+    )
+  }
+
+  if (props.layout === 'LIST') {
+    return props.stories.map(story => (
+      <ListLayoutItem
+        key={story.title}
+        date={story.date}
+        title={story.title}
+        author={story.author}
+        icon='quill'
+        path={`/stories/${story.slug}`}
+        excerpt={story.excerpt}
+      />
+    ))
+  }
+
+  return null
 })
