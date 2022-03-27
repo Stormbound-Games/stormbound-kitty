f50eda3de1dab28b2b3a1b6cecbe7bd6669959c8
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-27 11:21:35 +0200

    Add a ‘ListHeader’ component

---
+++ src/components/ListHeader/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useFela } from 'react-fela'
+import BlankButton from '~/components/BlankButton'
+import Icon from '~/components/Icon'
+import Select from '~/components/Select'
+import VisuallyHidden from '~/components/VisuallyHidden'
+import styles from './styles'
+
+export default React.memo(function ListHeader(props) {
+  const { css } = useFela()
+
+  return (
+    <div className={css(styles.header)}>
+      <p className={css(styles.label)}>{props.children}</p>
+      <div className={css(styles.sort)}>
+        <Select
+          id='order'
+          label='Sort by'
+          hideLabel
+          extend={styles.select}
+          value={props.order}
+          onChange={event => props.setOrder(event.target.value)}
+        >
+          <option value='' disabled>
+            Sort by…
+          </option>
+          {props.sorting.map(order => (
+            <option value={order.value} key={order.value}>
+              Sort by {order.title}
+            </option>
+          ))}
+        </Select>
+      </div>
+      <div className={css(styles.layout)}>
+        <BlankButton
+          extend={styles.layoutButton}
+          aria-pressed={props.layout === 'LIST'}
+          onClick={() => props.setLayout('LIST')}
+        >
+          <Icon icon='list' />
+          <VisuallyHidden>List layout</VisuallyHidden>
+        </BlankButton>
+        <BlankButton
+          extend={styles.layoutButton}
+          aria-pressed={props.layout === 'GRID'}
+          onClick={() => props.setLayout('GRID')}
+        >
+          <Icon icon='table' />
+          <VisuallyHidden>Grid layout</VisuallyHidden>
+        </BlankButton>
+      </div>
+    </div>
+  )
+})
---
+++ src/components/ListHeader/styles.js
@@ -0,0 +1,47 @@
+const header = {
+  display: 'flex',
+  borderBottom: '1px solid var(--dark-beige)',
+  color: 'var(--beige)',
+  marginBottom: 'var(--s-large)',
+  fontSize: '80%',
+
+  medium: { fontSize: '100%' },
+}
+
+// 1. Aligned with the bottom spacing of the Select component, which matters on
+//    mobile where a misalignment looks awkward.
+const label = {
+  display: 'flex',
+  alignItems: 'flex-end',
+  marginBottom: '0.65em' /* 1 */,
+}
+
+const sort = {
+  marginLeft: 'auto',
+  marginRight: 'var(--s-base)',
+  position: 'relative',
+}
+
+const select = {
+  border: 0,
+  borderRadius: 0,
+}
+
+const layout = {
+  display: 'flex',
+  justifyContent: 'flex-end',
+}
+
+const layoutButton = {
+  padding: 'var(--s-smaller)',
+  borderBottom: '1px solid transparent',
+
+  '[aria-pressed="true"]': {
+    color: 'white',
+    borderColor: 'var(--beige)',
+  },
+}
+
+const styles = { header, label, layout, select, sort, layoutButton }
+
+export default styles
