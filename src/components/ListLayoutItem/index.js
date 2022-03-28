6785c745bb51c8c0db8a1c5c2cf1e85823ab8497
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-28 11:57:42 +0200

    Support multiple authors

--- src/components/ListLayoutItem/index.js
+++ src/components/ListLayoutItem/index.js
@@ -3,22 +3,23 @@ import { useFela } from 'react-fela'
 import Spacing from '~/components/Spacing'
 import FeedEntry from '~/components/FeedEntry'
 import Link from '~/components/Link'
+import MemberList from '~/components/MemberList'
 import styles from './styles'
 
 export default React.memo(function ListLayoutItem(props) {
   const { css } = useFela()
+  const authors = Array.isArray(props.authors)
+    ? props.authors
+    : [props.author].filter(Boolean)
 
   return (
     <Spacing key={props.title} vertical='BASE'>
       <FeedEntry icon={props.icon} image={props.image} date={props.date}>
         <p className={css(styles.primary)}>
           <Link to={props.path}>{props.title}</Link>
-          {props.author ? (
+          {authors.length > 0 ? (
             <>
-              by{' '}
-              <Link to={'/members/' + props.author.slug}>
-                {props.author.name}
-              </Link>
+              &nbsp;by <MemberList members={authors} />
             </>
           ) : null}
         </p>
