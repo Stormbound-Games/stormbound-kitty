fa7414134490db2272bbbbdc0a05591de1a8c97c
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-28 12:04:27 +0200

    Fix releases order

--- src/api/releases/getReleases.js
+++ src/api/releases/getReleases.js
@@ -4,9 +4,9 @@ import { FIELDS, MAPPER } from './utils'
 const getReleases = async ({ isPreview, limit } = {}) => {
   const releases = await getEntries({
     conditions: ['_type == "release"'],
-    fields: FIELDS,
+    fields: '_createdAt,' + FIELDS,
     options: {
-      order: 'date desc',
+      order: 'date desc, _createdAt desc',
       slice: limit ? `0...${limit}` : undefined,
       isPreview,
     },
