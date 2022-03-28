f0cd43865cbade2ea9f79691dc3e040bdcb40ab2
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-28 16:16:40 +0200

    Remove route tests

--- cypress/integration/routes/index.js
+++
@@ -1,9 +0,0 @@
-import links from '../../fixtures/registry'
-
-describe('Routes', () => {
-  links.forEach(link => {
-    it(`${link.label} (${link.path})`, () => {
-      cy.visit(link.path).get('h1', { log: false }).should('exist')
-    })
-  })
-})
--- cypress/plugins/main.js
+++ cypress/plugins/main.js
@@ -2,7 +2,6 @@ import fs from 'fs'
 import path from 'path'
 import preprocessor from '@cypress/webpack-preprocessor'
 import indexArray from '~/helpers/indexArray'
-import getSearchIndex from '~/helpers/getSearchIndex'
 import getCards from '~/api/cards/getCards'
 
 // For some reason I get a ` Cannot find module 'fs/promises'` error on Cypress
@@ -17,20 +16,12 @@ const writeFile = (path, content) =>
   })
 
 const configuration = (on, config) => {
-  // Retrieve the path registry for the route tests and write it as a fixture
-  // file so it can be statically imported.
-  return Promise.all([getSearchIndex(false), getCards()])
-    .then(([registry, cards]) =>
-      Promise.all([
-        writeFile(
-          path.resolve('cypress', 'fixtures', 'registry.json'),
-          JSON.stringify(registry, null, 2)
-        ),
-        writeFile(
-          path.resolve('cypress', 'fixtures', 'cards.json'),
-          JSON.stringify(indexArray(cards), null, 2)
-        ),
-      ])
+  return getCards()
+    .then(cards =>
+      writeFile(
+        path.resolve('cypress', 'fixtures', 'cards.json'),
+        JSON.stringify(indexArray(cards), null, 2)
+      )
     )
     .then(() => {
       on(
