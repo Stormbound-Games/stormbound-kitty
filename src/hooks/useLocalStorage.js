794839314c835f69bf14ef885193501def1c6ec9
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-29 12:37:22 +0200

    Simplify the ‘useLocalStorage’ hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,65 +1,24 @@
 import React from 'react'
 
 const useLocalStorage = (key, initialValue) => {
-  const readValue = React.useCallback(() => {
-    if (typeof window === 'undefined') return initialValue
-
-    try {
-      const item = window.localStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      console.warn(`Error reading localStorage key “${key}”:`, error)
-      return initialValue
-    }
-  }, [key, initialValue])
-
-  // We could avoid a flash of initial value by passing `readValue` as the
-  // initial state getter, but that causes a React mismatch because the server
-  // pass uses the initial value, while the first client pass uses the stored
-  // value, thus failing reconciliation.
-  const [storedValue, setStoredValue] = React.useState(initialValue)
-
-  const setValue = React.useCallback(
-    value => {
-      if (typeof window == 'undefined') {
-        console.warn(
-          `Tried setting localStorage key “${key}” outside of the browser.`
-        )
-      }
-
-      try {
-        const newValue = value instanceof Function ? value(storedValue) : value
-
-        window.localStorage.setItem(key, JSON.stringify(newValue))
-
-        setStoredValue(newValue)
-
-        window.dispatchEvent(new Event('local-storage'))
-      } catch (error) {
-        console.warn(`Error setting localStorage key “${key}”:`, error)
-      }
-    },
-    [storedValue, key]
-  )
-
-  const handleStorageChange = React.useCallback(
-    () => setStoredValue(readValue()),
-    [readValue]
-  )
-
-  React.useEffect(() => handleStorageChange(), [handleStorageChange])
+  const [value, setValue] = React.useState(initialValue)
+  const didMountRef = React.useRef(false)
 
   React.useEffect(() => {
-    window.addEventListener('storage', handleStorageChange)
-    window.addEventListener('local-storage', handleStorageChange)
+    // Only update when the value has effectively changed, and not when the hook
+    // mounts the first time, otherwise it ends up *always* storing the initial
+    // value, overriding anything previously set.
+    if (didMountRef.current) {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } else {
+      didMountRef.current = true
 
-    return () => {
-      window.removeEventListener('storage', handleStorageChange)
-      window.removeEventListener('local-storage', handleStorageChange)
+      const item = window.localStorage.getItem(key)
+      if (typeof item !== 'undefined') setValue(JSON.parse(item))
     }
-  }, [handleStorageChange])
+  }, [key, value])
 
-  return [storedValue, setValue]
+  return [value, setValue]
 }
 
 export default useLocalStorage
