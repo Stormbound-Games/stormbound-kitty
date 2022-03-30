3789f2538f89340babe2e801ad5026f7cb8232f7
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-03-30 15:41:03 +0200

    Remove legacy code from user name

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import useLocalStorage from '~/hooks/useLocalStorage'
 
-const useUser = (initialValue = null) => {
-  const [name, setName] = useLocalStorage('sk.user_name', initialValue)
-  const [user, setUser] = useLocalStorage('sk.user', initialValue)
-
-  React.useEffect(() => {
-    if (name && !user) {
-      setUser({ name, slug: name.replace(/\s/g, '').toLowerCase() })
-      setName(null)
-    }
-    // eslint-disable-next-line
-  }, [name, user])
-
-  return [user, setUser]
-}
+const useUser = (initialValue = null) =>
+  useLocalStorage('sk.user', initialValue)
 
 export default useUser
