be3324d9738805bb298e57b6530df06dd76e149d
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-03 12:55:36 +0200

    Make the revalidation script Node-agnostic

--- bin/revalidate.js
+++ bin/revalidate.js
@@ -1,3 +1,8 @@
+// The script is executed with Node 17 in the GitHub Workflow, but might be
+// executed with Node 14 or Node 16 when ran locally.
+const fetch =
+  typeof global.fetch === 'function' ? global.fetch : require('node-fetch')
+
 const NODE_ENV = process.env.NODE_ENV || 'development'
 const NEXT_REVALIDATION_TOKEN = process.env.NEXT_REVALIDATION_TOKEN
 
@@ -7,6 +12,11 @@ const domain =
     : 'https://stormbound-kitty.com'
 
 const path = process.argv[2]
+
+if (!NEXT_REVALIDATION_TOKEN) {
+  console.error('Missing Next.js revalidation token.')
+  process.exit(1)
+}
 
 if (!path) {
   console.error('Missing path to revalidate.')
