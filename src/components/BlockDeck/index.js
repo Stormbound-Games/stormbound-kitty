36d7a603bcb54b1bb7eb19364c922441684a8ac8
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-04 19:15:00 +0200

    Fix date parsing in deck blocks

--- src/components/BlockDeck/index.js
+++ src/components/BlockDeck/index.js
@@ -3,9 +3,11 @@ import FeaturedDeck from '~/components/FeaturedDeck'
 import Spacing from '~/components/Spacing'
 
 export default React.memo(function BlockDeck(props) {
+  const date = props.value.date ? new Date(props.value.date) : null
+
   return (
     <Spacing bottom='LARGE'>
-      <FeaturedDeck {...props.value} />
+      <FeaturedDeck {...props.value} date={date} />
     </Spacing>
   )
 })
