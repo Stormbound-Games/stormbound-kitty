3ce11e8fb4d67d77a94594ba1801ad4adc723661
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-06 10:02:06 +0200

    Fix an issue with ‘useFluidSizing’ not working properly with elements within <details>

--- src/hooks/useFluidSizing.js
+++ src/hooks/useFluidSizing.js
@@ -9,7 +9,15 @@ const useFluidSizing = (ratio, defaultWidth = undefined) => {
   )
 
   React.useEffect(() => {
-    setFontSize(ref.current.offsetWidth * ratio + 'px')
+    // If the DOM element lives within a closed <details> element, its
+    // `offsetWidth` will be 0. To get its actual width, we can briefly open and
+    // close the <details> element.
+    const details = ref.current.closest('details:not([open])')
+    if (details) details.setAttribute('open', 'open')
+    const width = ref.current.offsetWidth
+    if (details) details.removeAttribute('open')
+
+    setFontSize(width * ratio + 'px')
   }, [ratio, ref, viewportWidth])
 
   return { fontSize, ref }
