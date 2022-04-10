701814721ccafb5b3448f5bf18fc4515e67ffe67
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-10 10:35:19 +0200

    Use ‘FeedDetailDisplay’ component in ‘CardFeedArtEntry’ component

--- src/components/CardFeedArtEntry/index.js
+++ src/components/CardFeedArtEntry/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { useFela } from 'react-fela'
 import Image from '~/components/Image'
+import FeedDetailDisplay from '~/components/FeedDetailDisplay'
 import FeedEntry from '~/components/FeedEntry'
 import Link from '~/components/Link'
 
 export default React.memo(function CardFeedArtEntry(props) {
-  const { css } = useFela()
   const { width, height } = props.dimensions
   const displayWidth = 450
   const displayHeight = Math.round((displayWidth / width) * height)
@@ -16,15 +15,13 @@ export default React.memo(function CardFeedArtEntry(props) {
     <FeedEntry icon='image' date={props.date}>
       <Link to={'/members/' + props.user.slug}>{props.user.name}</Link> has made
       some art about this card.
-      <details className={css({ maxWidth: displayWidth + 'px' })}>
-        <summary className={css({ marginBottom: 'var(--s-base)' })}>
-          + Toggle art display
-        </summary>
+      <FeedDetailDisplay label='artwork'>
         <Image
+          extend={{ marginTop: 0 }}
           src={props.image + dimensions}
           alt={'Artwork by' + props.user.name}
         />
-      </details>
+      </FeedDetailDisplay>
     </FeedEntry>
   )
 })
