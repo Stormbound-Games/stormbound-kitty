7595f89ee2eb9bb02062cc5a3df79600e7241dc5
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-11 15:44:56 +0200

    Rework the puzzles page

--- src/components/FeedPuzzleEntry/index.js
+++ src/components/FeedPuzzleEntry/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
-import { useFela } from 'react-fela'
 import Link from '~/components/Link'
 import FeedEntry from '~/components/FeedEntry'
-import BattleSimPuzzle from '~/components/BattleSimPuzzle'
-import styles from './styles'
 
 export default React.memo(function FeedPuzzleEntry(props) {
-  const { css } = useFela()
-
   return (
     <FeedEntry icon='sword' date={props.date}>
-      {props.user.name} has created a puzzle called{' '}
-      <Link to={'/simulators/battle/' + props.board}>{props.name}</Link>.
-      <div className={css(styles.container)}>
-        <BattleSimPuzzle {...props} />
-      </div>
+      {props.user.name} has created a {props.category.toLowerCase()} puzzle
+      called <Link to={'/puzzles/' + props.slug}>{props.name}</Link>.
     </FeedEntry>
   )
 })
--- src/components/PagePuzzles/index.js
+++ src/components/PagePuzzles/index.js
@@ -1,138 +1,130 @@
 import React from 'react'
 import Page from '~/components/Page'
 import Link from '~/components/Link'
-import EmptySearch from '~/components/EmptySearch'
 import Notice from '~/components/Notice'
-import Title from '~/components/Title'
-import PuzzlesFilters from '~/components/BattleSimPuzzlesFilters'
-import Row from '~/components/Row'
+import ListHeader from '~/components/ListHeader'
+import ListLayoutItem from '~/components/ListLayoutItem'
 import Teasers from '~/components/Teasers'
-import {
-  getCardForPuzzle,
-  getExcerptForPuzzle,
-} from '~/components/BattleSimPuzzle'
+import capitalize from '~/helpers/capitalize'
+import parseDate from '~/helpers/parseDate'
+import { formatDate } from '~/helpers/formatDate'
+import { CATEGORIES, RESTRICTIONS } from '~/constants/puzzles'
+
+const getCardForPuzzle = puzzle => ({
+  name: puzzle.name,
+  type: 'unit',
+  race: puzzle.category,
+  mana: '0',
+  rarity: 'common',
+  faction: 'neutral',
+  image: puzzle.image,
+  ability: puzzle.restrictions
+    .map(restriction => RESTRICTIONS[restriction].name)
+    .join(', '),
+})
+
+const Restrictions = props =>
+  props.restrictions.length > 0
+    ? props.restrictions
+        .slice(0)
+        .sort()
+        .map((restriction, index) => (
+          <React.Fragment key={restriction}>
+            <span title={RESTRICTIONS[restriction].description}>
+              {RESTRICTIONS[restriction].name}
+            </span>
+            {index !== props.restrictions.length - 1 && ', '}
+          </React.Fragment>
+        ))
+    : 'No restrictions'
+
+const getExcerptForPuzzle = puzzle => (
+  <>
+    {CATEGORIES[puzzle.category]}{' '}
+    <Restrictions restrictions={puzzle.restrictions} />.
+  </>
+)
 
 export default React.memo(function PagePuzzles(props) {
-  const [difficulty, setDifficulty] = React.useState('*')
-  const [category, setCategory] = React.useState('*')
-  const [restrictions, setRestrictions] = React.useState([])
-  const [name, setName] = React.useState('')
-
-  const resetFilters = React.useCallback(() => {
-    setDifficulty('*')
-    setCategory('*')
-    setRestrictions([])
-    setName('')
-  }, [])
-
-  const updateFilter = name => value => {
-    switch (name) {
-      case 'difficulty':
-        return setDifficulty(value)
-      case 'category':
-        return setCategory(value)
-      case 'restrictions':
-        return setRestrictions(value)
-      case 'name':
-        return setName(value)
-    }
-  }
-
-  const matchesName = React.useCallback(
-    puzzle =>
-      name === '' ||
-      puzzle.name.toLowerCase().replace('’', "'").includes(name.toLowerCase()),
-    [name]
-  )
-
-  const matchesCategory = React.useCallback(
-    puzzle => category === '*' || puzzle.category === category,
-    [category]
-  )
-
-  const matchesDifficulty = React.useCallback(
-    puzzle => difficulty === '*' || puzzle.difficulty === +difficulty,
-    [difficulty]
-  )
-
-  const matchesRestriction = React.useCallback(
-    puzzle =>
-      restrictions.length === 0 ||
-      restrictions.every(restriction =>
-        puzzle.restrictions.includes(restriction)
-      ),
-    [restrictions]
-  )
-
-  const puzzles = React.useMemo(
+  const [layout, setLayout] = React.useState('GRID')
+  const [order, setOrder] = React.useState('DATE')
+  const items = React.useMemo(
     () =>
-      props.puzzles
-        .filter(matchesName)
-        .filter(matchesDifficulty)
-        .filter(matchesRestriction)
-        .filter(matchesCategory)
-        .reverse(),
-    [
-      props.puzzles,
-      matchesName,
-      matchesDifficulty,
-      matchesRestriction,
-      matchesCategory,
-    ]
+      props.puzzles.slice(0).sort((a, b) => {
+        if (order === 'NAME') return a.name.localeCompare(b.name)
+        if (order === 'CATEGORY') return a.category.localeCompare(b.category)
+        if (order === 'AUTHOR') return a.user.name.localeCompare(b.user.name)
+        if (order === 'DATE') return parseDate(b.date) - parseDate(a.date)
+        return 0
+      }),
+    [props.puzzles, order]
   )
 
   return (
-    <Page
-      title='Puzzles'
-      description='Test your skills and resolve interesting and creative puzzles made by the Stormbound community'
-    >
-      <Row isDesktopOnly>
-        <Row.Column width='1/3'>
-          <Title>Filters</Title>
-          <PuzzlesFilters
-            name={name}
-            restrictions={restrictions}
-            difficulty={difficulty}
-            category={category}
-            updateFilter={updateFilter}
-          />
-        </Row.Column>
-        <Row.Column width='2/3'>
-          <Title>Puzzles</Title>
-          {puzzles.length > 0 ? (
-            <Teasers
-              columns={2}
-              items={puzzles.map(puzzle => ({
-                card: getCardForPuzzle(puzzle),
-                title: puzzle.name,
-                meta: (
-                  <>
-                    Made by{' '}
-                    <Link to={'/members/' + puzzle.user.slug}>
-                      {puzzle.user.name}
-                    </Link>
-                  </>
-                ),
-                to: `/puzzles/${puzzle.slug}`,
-                excerpt: getExcerptForPuzzle(puzzle),
-                'data-testid': `puzzle ${puzzle.name} ${puzzle.category} ${
-                  puzzle.difficulty
-                }/3 ${puzzle.restrictions.join(' ')}`,
-              }))}
-            />
-          ) : (
-            <EmptySearch title='No puzzles found' resetFilters={resetFilters} />
-          )}
+    <>
+      <Page
+        title='Puzzles'
+        description='Test your skills and resolve interesting and creative puzzles made by the Stormbound community'
+        isEditorialContent
+      >
+        <ListHeader
+          layout={layout}
+          setLayout={setLayout}
+          order={order}
+          setOrder={setOrder}
+          sorting={[
+            { title: 'Date', value: 'DATE' },
+            { title: 'Name', value: 'NAME' },
+            { title: 'Category', value: 'CATEGORY' },
+            { title: 'Author', value: 'AUTHOR' },
+          ]}
+        >
+          {items.length} {items.length === 1 ? 'puzzle' : 'puzzles'}
+        </ListHeader>
 
-          <Notice icon='sword'>
-            Design your own puzzles and{' '}
-            <Link to={{ pathname: '/faq', hash: '#adding-a-puzzle' }}>
-              have them added
-            </Link>{' '}
-            to the list!
-          </Notice>
-        </Row.Column>
-      </Row>
-    </Page>
+        {layout === 'GRID' ? (
+          <Teasers
+            items={items.map(puzzle => ({
+              card: getCardForPuzzle(puzzle),
+              title: puzzle.name,
+              meta: (
+                <>
+                  By{' '}
+                  <Link to={'/members/' + puzzle.user.slug}>
+                    {puzzle.user.name}
+                  </Link>{' '}
+                  in {formatDate(parseDate(puzzle.date))}
+                </>
+              ),
+              to: `/puzzles/${puzzle.slug}`,
+              excerpt: getExcerptForPuzzle(puzzle),
+              'data-testid': `puzzle ${puzzle.name} ${puzzle.category} ${
+                puzzle.difficulty
+              }/3 ${puzzle.restrictions.join(' ')}`,
+            }))}
+          />
+        ) : layout === 'LIST' ? (
+          items.map(puzzle => (
+            <ListLayoutItem
+              key={puzzle.slug}
+              title={puzzle.name}
+              author={puzzle.user}
+              date={puzzle.date}
+              path={`/puzzles/${puzzle.slug}`}
+              excerpt={getExcerptForPuzzle(puzzle)}
+              icon='sword'
+            />
+          ))
+        ) : null}
+      </Page>
+
+      <Notice icon='sword'>
+        Design your own puzzles and{' '}
+        <Link to={{ pathname: '/faq', hash: '#adding-a-puzzle' }}>
+          have them added
+        </Link>{' '}
+        to the list!
+      </Notice>
+    </>
   )
 })
