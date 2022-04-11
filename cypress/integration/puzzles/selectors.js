4924e8dbe4244fb780752d14604427129d641319
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-11 19:19:47 +0200

    Remove obsolete end-to-end tests

--- cypress/integration/puzzles/selectors.js
+++
@@ -1,9 +0,0 @@
-const selectors = {
-  CATEGORY_SELECT: '[data-testid="category-select"]',
-  NAME_INPUT: '[data-testid="name-input"]',
-  PUZZLE: '[data-testid*="puzzle"]',
-  RESTRICTION_TOGGLE: '[aria-controls="puzzles-filters-target"]',
-  RESTRICTION_CHECKBOXES: '[data-testid="restriction-checkbox"]',
-}
-
-export default selectors
