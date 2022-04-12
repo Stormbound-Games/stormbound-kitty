fed908eacfd4239bffd622041300db4e957cc5ae
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-12 10:27:44 +0200

    Remove the need for @sanity/image-url

--- src/components/BlockImage/index.js
+++ src/components/BlockImage/index.js
@@ -1,12 +1,6 @@
 import React from 'react'
-import { client } from '~/constants/sanity'
-import imageUrlBuilder from '@sanity/image-url'
 import Image from '~/components/Image'
 import PageEmbed from '~/components/PageEmbed'
-
-const builder = imageUrlBuilder(client)
-
-const getUrl = image => builder.image(image).fit('max').auto('format').url()
 
 export default React.memo(function BlockImage(props) {
   const extend = props.value.extend ? JSON.parse(props.value.extend) : undefined
@@ -15,7 +9,7 @@ export default React.memo(function BlockImage(props) {
   return (
     <Container>
       <Image
-        src={getUrl(props.value)}
+        src={props.value.src + '?fit=max'}
         alt={props.value.alt}
         extend={extend}
         lazy
