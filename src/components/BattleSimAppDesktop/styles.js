64bc76c6551d113e22443f46ed9a58d151023e0c
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-16 15:39:22 +0200

    Remove obsolete styles

--- src/components/BattleSimAppDesktop/styles.js
+++ src/components/BattleSimAppDesktop/styles.js
@@ -3,18 +3,9 @@
  *    size, which was originally 90% of the root font size (hence `0.9rem`).
  */
 const root = {
-  position: 'relative',
   fontSize: '0.9rem' /* 1 */,
 }
 
-const deck = {
-  position: 'absolute',
-  right: 0,
-  top: 0,
-  padding: 'var(--s-base)',
-  width: '25%',
-}
-
-const styles = { root, deck }
+const styles = { root }
 
 export default styles
