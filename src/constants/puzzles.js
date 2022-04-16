360215a6465edd4731e6519482a4f7cbce556661
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-16 11:59:55 +0200

    Remove levels-related puzzle restrictions

--- src/constants/puzzles.js
+++ src/constants/puzzles.js
@@ -1,12 +1,4 @@
 export const RESTRICTIONS = {
-  LEVEL_1: {
-    name: 'Level 1',
-    description: 'All cards are level 1.',
-  },
-  LEVEL_5: {
-    name: 'Level 5',
-    description: 'All cards are level 5.',
-  },
   RNG_FRIENDLY: {
     name: 'RNG-friendly',
     description: 'All RNG goes in your favor.',
@@ -15,9 +7,5 @@ export const RESTRICTIONS = {
     name: 'Anti-RNG',
     description:
       'Nothing goes right without 100% precision! Leave no room for chance.',
-  },
-  DETAILED: {
-    name: 'Detailed',
-    description: 'Cards have specific level; check them individually.',
   },
 }
