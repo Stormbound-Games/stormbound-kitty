f3addb3dce1757ea8bca614bc268926b25a0f051
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-26 13:37:57 +0200

    Fix the shortening endpoint failing beyond shortening quota

--- src/pages/api/shorten.js
+++ src/pages/api/shorten.js
@@ -5,6 +5,13 @@ const API_KEY = process.env.CUTTLY_API_KEY
 const API_URL = 'https://cutt.ly/api/api.php'
 const cache = new Map()
 
+const shorten = (url, withCustomDomain = false) =>
+  fetch(
+    API_URL +
+      `?key=${API_KEY}&short=${url}&noTitle=1&userDomain=` +
+      Number(withCustomDomain)
+  ).then(response => response.json())
+
 export default async function handler(request, response) {
   try {
     await applyRateLimit(request, response)
@@ -29,22 +36,40 @@ export default async function handler(request, response) {
     return response.status(200).json(cache.get(url))
   }
 
-  const res = await fetch(
-    API_URL + `?key=${API_KEY}&short=${url}&noTitle=1&userDomain=1`
-  )
-  const { url: data } = await res.json()
-
-  if (data.status !== 7) {
-    console.error(
-      'Failed to shorten link',
+  try {
+    let data = await shorten(
       url,
-      'with status code',
-      data.status
+      // Avoid Checkly’s automated checks taking custom links from the 1,000
+      // monthly quota.
+      // User-Agent: Checkly/1.0 (https://www.checklyhq.com)
+      !/checkly/i.test(request.headers['user-agent'])
     )
-    return response.status(400).send('Bad request')
+
+    // Not super elegant, but when we’ve reached the monthly quota of branded
+    // links with the short domain, the API returns the following plain text
+    // response, so we should query again bypassing the short domain.
+    // "Subscription has expired or you've reached your shortening limit"
+    if (typeof data === 'string' && data.includes('limit')) {
+      data = await shorten(url, false)
+    }
+
+    if (data.url.status !== 7) {
+      console.error(
+        'Failed to shorten link',
+        url,
+        'with status code',
+        data.url.status
+      )
+
+      return response.status(400).send('Bad request')
+    }
+
+    cache.set(url, data.url)
+
+    return response.status(200).json(data.url)
+  } catch (error) {
+    console.error(error)
+
+    return response.status(500).send('Internal server error')
   }
-
-  cache.set(url, data)
-
-  return response.status(200).json(data)
 }
