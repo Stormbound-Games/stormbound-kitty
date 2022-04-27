f2e1d6673c81cf9f9ff27cdb2da83001348cf867
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-27 14:36:31 +0200

    Improve 429 handling for card randomizer

--- src/components/RandomCardInfo/index.js
+++ src/components/RandomCardInfo/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CTA from '~/components/CTA'
+import Icon from '~/components/Icon'
 import Info from '~/components/Info'
 import Link from '~/components/Link'
 import { INITIAL_STATE } from '~/hooks/useCardBuilder'
@@ -9,7 +10,7 @@ import track from '~/helpers/track'
 
 export default React.memo(function RandomCardInfo(props) {
   const [isLoading, setIsLoading] = React.useState(false)
-  const [hasErrored, setHasErrored] = React.useState(false)
+  const [error, setError] = React.useState(false)
   const { setCardData } = props
 
   const createRandomCard = React.useCallback(async () => {
@@ -19,6 +20,13 @@ export default React.memo(function RandomCardInfo(props) {
       track('randomize_card')
 
       const response = await fetch('/api/randomize-card')
+
+      if (!response.ok) {
+        const error = new Error(response.statusText)
+        error.status = 429
+        throw error
+      }
+
       const card = await response.json()
 
       // Use the global setter instead of individual one as it’s more performant
@@ -44,10 +52,14 @@ export default React.memo(function RandomCardInfo(props) {
         fixedMovement: card.fixedMovement ?? INITIAL_STATE.fixedMovement,
       }))
 
-      setHasErrored(false)
+      setError(false)
     } catch (error) {
       setCardData({ ...INITIAL_STATE })
-      setHasErrored(true)
+      setError(
+        error.status === 429
+          ? 'You have performed too many requests. Please wait a moment before retrying.'
+          : 'An unexpected error occurred. Please try again.'
+      )
     } finally {
       setIsLoading(false)
     }
@@ -58,20 +70,29 @@ export default React.memo(function RandomCardInfo(props) {
       icon='wand'
       title='Random card'
       CTA={
-        <CTA onClick={createRandomCard} disabled={isLoading}>
+        <CTA
+          onClick={createRandomCard}
+          disabled={isLoading}
+          aria-describedby='randomize-card-error'
+          aria-invalid={Boolean(error)}
+        >
           {isLoading ? 'Loading…' : 'Randomize'}
         </CTA>
       }
     >
-      <p>
+      <p style={{ marginBottom: error ? 'var(--s-base)' : '0' }}>
         Don’t know where to start? Try the card randomizer from{' '}
         <Link to={`/members/parchment-engineer`}>ParchmentEngineer</Link>. It
         should generate a relatively balanced card for you to get started! From
         there, you can tweak it to suit your needs.
       </p>
-      {hasErrored ? (
-        <p>An unexpected error occurred. Please try again.</p>
-      ) : null}
+      <p id='randomize-card-error' style={{ color: 'var(--light-ironclad)' }}>
+        {error ? (
+          <>
+            <Icon icon='warning' /> {error}
+          </>
+        ) : null}
+      </p>
     </Info>
   )
 })
