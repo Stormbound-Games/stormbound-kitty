d7b185f2083612fe0e0eebd491a78d3c0bbd24f1
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-27 14:28:13 +0200

    Fix rate limiting not working properly

---
+++ cypress/integration/rateLimit/index.js
@@ -0,0 +1,19 @@
+import { LIMIT_PER_WINDOW } from '~/helpers/applyRateLimit'
+
+describe('Brute-force protection', () => {
+  it('should return the rate-limiting upper limit as a header', () => {
+    cy.request({ url: '/api/search', method: 'GET', failOnStatusCode: false })
+      .its('headers')
+      .its('x-ratelimit-limit')
+      .should('eq', String(LIMIT_PER_WINDOW))
+  })
+
+  it('should count down requests up to the limit', () => {
+    cy.request({ url: '/api/search', method: 'GET', failOnStatusCode: false })
+      .its('headers')
+      .its('x-ratelimit-remaining')
+      .should(count => {
+        expect(Number(count)).to.be.lessThan(LIMIT_PER_WINDOW)
+      })
+  })
+})
--- src/helpers/applyRateLimit/index.js
+++ src/helpers/applyRateLimit/index.js
@@ -1,27 +1,32 @@
 import rateLimit from 'express-rate-limit'
 import slowDown from 'express-slow-down'
-import applyMiddleware from '~/helpers/applyMiddleware'
 import getIP from '~/helpers/getIP'
 
+export const LIMIT_PER_WINDOW = 20
+const MIDDLEWARE_CONFIG = {
+  legacyHeaders: true,
+  windowMs: 60 * 1000,
+  keyGenerator: getIP,
+}
+
+// Taken from: https://nextjs.org/docs/api-routes/api-middlewares
+const applyMiddleware = middleware => (request, response) =>
+  new Promise((resolve, reject) => {
+    middleware(request, response, result =>
+      result instanceof Error ? reject(result) : resolve(result)
+    )
+  })
+
+const withSlowDown = applyMiddleware(
+  slowDown({ ...MIDDLEWARE_CONFIG, delayAfter: LIMIT_PER_WINDOW, delayMs: 500 })
+)
+const withRateLimit = applyMiddleware(
+  rateLimit({ ...MIDDLEWARE_CONFIG, max: LIMIT_PER_WINDOW })
+)
+
 async function applyRateLimit(request, response) {
-  const limitPerWindow = 20
-  const config = {
-    legacyHeaders: true,
-    windowMs: 60 * 1000,
-    keyGenerator: getIP,
-  }
-
-  await applyMiddleware(
-    request,
-    response,
-    slowDown({ ...config, delayAfter: limitPerWindow, delayMs: 500 })
-  )
-
-  await applyMiddleware(
-    request,
-    response,
-    rateLimit({ ...config, max: limitPerWindow })
-  )
+  await withSlowDown(request, response)
+  await withRateLimit(request, response)
 }
 
 export default applyRateLimit
