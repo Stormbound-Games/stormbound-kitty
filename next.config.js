da6a20bf477f857c53d733de28f649caab462743
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-30 11:21:03 +0200

    Remove obsolete HTTP header

--- next.config.js
+++ next.config.js
@@ -115,10 +115,6 @@ module.exports = withPlugins(plugins, {
             value: '1; mode=block',
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
