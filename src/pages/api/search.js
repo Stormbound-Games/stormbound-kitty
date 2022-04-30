4dce557d15a9510fd24a9467a0baccb356e3b021
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-04-30 11:30:37 +0200

    Cache search responses

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -35,6 +35,9 @@ export default async function handler(request, response) {
     return response.status(429).json({ message: 'Too many requests' })
   }
 
+  // Set up a 4 hours cache on the response.
+  response.setHeader('Cache-Control', 's-maxage=' + 60 * 60 * 4 + ', public')
+
   const term = (request.query.search || '').trim()
 
   if (term.length === 0) {
