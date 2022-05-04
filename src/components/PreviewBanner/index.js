b5e145154ed2f4c4ee5f94fbd4fb0e52507d71be
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-04 18:11:21 +0200

    Prevent turning off preview mode in the iframe

--- src/components/PreviewBanner/index.js
+++ src/components/PreviewBanner/index.js
@@ -3,6 +3,15 @@ import Link from '~/components/Link'
 import Notification from '~/components/Notification'
 
 export default React.memo(function PreviewBanner() {
+  const [withTurnOff, setWithTurnOff] = React.useState(false)
+
+  React.useEffect(() => {
+    // Only provide a way to turn off the preview mdoe when *not* rendered
+    // inside an iframe. This is to avoid being able to turn off the preview
+    // mode when iframed in the CMS.
+    setWithTurnOff(window.self === window.top)
+  }, [])
+
   return (
     <Notification
       as='div'
@@ -11,7 +20,11 @@ export default React.memo(function PreviewBanner() {
       icon='eye'
     >
       The content preview mode is currently enabled.{' '}
-      <Link href='/api/preview?type=clear'>Turn off content preview</Link>.
+      {withTurnOff && (
+        <>
+          <Link href='/api/preview?type=clear'>Turn off content preview</Link>.
+        </>
+      )}
     </Notification>
   )
 })
