805b6b89149920addbe58fc90b42bd2a3b72dc27
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-04 21:18:25 +0200

    Tweak the CDN usage

--- src/constants/sanity.js
+++ src/constants/sanity.js
@@ -8,7 +8,15 @@ export const client = sanityClient({
   projectId: PROJECT_ID,
   dataset: DATASET,
   apiVersion: API_VERSION,
-  useCdn: true,
+  // Using Sanity’s API CDN is generally good for performance, but does not play
+  // too nicely with on-demand revalidation. When the Sanity webhook instructs
+  // Next.js to rebuild a certain path, data queries to Sanity CDN may return
+  // stale data since they might be cached. This is a problem as it essentially
+  // negates the revalidation. That being said, we can safely use the API CDN a
+  // build time (during `next build`) since builds are performed to ship code
+  // and not to refresh data.
+  // See: https://www.sanity.io/docs/api-cdn
+  useCdn: Boolean(Number(process.env.CI)),
 })
 
 export const previewClient = process.env.SANITY_PREVIEW_TOKEN
