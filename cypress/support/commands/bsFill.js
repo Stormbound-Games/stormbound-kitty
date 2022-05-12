65f63c81064c9ccb41e4867a3933f50cede2e9b6
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-12 14:07:59 +0200

    Simplify the ‘bsFill’ command

--- cypress/support/commands/bsFill.js
+++ cypress/support/commands/bsFill.js
@@ -33,87 +33,54 @@ const bsFill = (
     }),
   })
 
-  return cy
-    .get(`[data-testid="cell-${cell}"]`, { log: false })
-    .then($cell => {
-      if (Boolean($cell.attr('aria-pressed')) !== true) {
-        return cy.wrap($cell, { log: false }).click({ log: false })
-      }
-    })
-    .get(s.CELL_FORM_CARD_SELECT, { log: false })
-    .type(card, { force: true, log: false })
-    .type('{enter}', { force: true, log: false })
+  cy.get(`[data-testid="cell-${cell}"]`, { log: false }).then($cell => {
+    if (String($cell.attr('aria-pressed')) !== 'true') {
+      cy.wrap($cell, { log: false }).click({ log: false })
+    }
+  })
 
-    .then(() => {
-      if (level) {
-        return cy
-          .get(s.CELL_FORM_LEVEL, { log: false })
-          .select(String(level), { log: false })
-      }
-    })
+  cy.get(s.CELL_FORM_CARD_SELECT, { log: false }).type(card + '{enter}', {
+    force: true,
+    log: false,
+  })
 
-    .then(() => {
-      if (player) {
-        return cy
-          .get(
-            player === 'RED' ? s.CELL_FORM_RED_RADIO : s.CELL_FORM_BLUE_RADIO,
-            { log: false }
-          )
-          .click({ force: true, log: false })
-      }
+  if (level) {
+    cy.get(s.CELL_FORM_LEVEL, { log: false }).select(String(level), {
+      log: false,
     })
+  }
 
-    .then(() => {
-      if (strength) {
-        cy.get(s.CELL_FORM_STRENGTH, { log: false })
-          .clear({ log: false })
-          .type(String(strength), { log: false })
-      }
-    })
+  if (player) {
+    cy.get(player === 'RED' ? s.CELL_FORM_RED_RADIO : s.CELL_FORM_BLUE_RADIO, {
+      log: false,
+    }).click({ log: false })
+  }
 
-    .then(() => {
-      if (poisoned) {
-        cy.get(s.CELL_FORM_POISON_CHECKBOX, { log: false }).click({
-          force: true,
-          log: false,
-        })
-      } else if (vitalized) {
-        cy.get(s.CELL_FORM_VITALITY_CHECKBOX, { log: false }).click({
-          force: true,
-          log: false,
-        })
-      }
-    })
+  if (strength) {
+    cy.get(s.CELL_FORM_STRENGTH, { log: false })
+      .clear({ log: false })
+      .type(String(strength), { log: false })
+  }
 
-    .then(() => {
-      if (frozen) {
-        cy.get(s.CELL_FORM_FROZEN_CHECKBOX, { log: false }).click({
-          force: true,
-          log: false,
-        })
-      }
-    })
+  if (poisoned) {
+    cy.get(s.CELL_FORM_POISON_CHECKBOX, { log: false }).click({ log: false })
+  } else if (vitalized) {
+    cy.get(s.CELL_FORM_VITALITY_CHECKBOX, { log: false }).click({ log: false })
+  }
 
-    .then(() => {
-      if (confused) {
-        cy.get(s.CELL_FORM_CONFUSED_CHECKBOX, { log: false }).click({
-          force: true,
-          log: false,
-        })
-      }
-    })
+  if (frozen) {
+    cy.get(s.CELL_FORM_FROZEN_CHECKBOX, { log: false }).click({ log: false })
+  }
 
-    .then(() => {
-      if (disabled) {
-        cy.get(s.CELL_FORM_DISABLED_CHECKBOX, { log: false }).click({
-          force: true,
-          log: false,
-        })
-      }
-    })
+  if (confused) {
+    cy.get(s.CELL_FORM_CONFUSED_CHECKBOX, { log: false }).click({ log: false })
+  }
 
-    .get(s.CELL_FORM_BTN, { log: false })
-    .click({ log: false })
+  if (disabled) {
+    cy.get(s.CELL_FORM_DISABLED_CHECKBOX, { log: false }).click({ log: false })
+  }
+
+  return cy.get(s.CELL_FORM_BTN, { log: false }).click({ log: false })
 }
 
 export default bsFill
