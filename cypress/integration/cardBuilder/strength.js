1894c582113999c352eb73634e3a18994e53684f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-12 12:43:57 +0200

    Consolidate more tests

--- cypress/integration/cardBuilder/strength.js
+++ cypress/integration/cardBuilder/strength.js
@@ -17,7 +17,11 @@ describe('Card Builder — Strength', () => {
   })
 
   it('should be possible to define the card strength', () => {
-    cy.get(s.STRENGTH_INPUT).clear().type('5').should('have.value', '5')
+    cy.get(s.STRENGTH_INPUT)
+      .should('be.visible')
+      .clear()
+      .type('5')
+      .should('have.value', '5')
     assertCardStrength(0, '5')
   })
 
