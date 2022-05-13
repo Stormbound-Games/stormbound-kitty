7a3883303c8c688a49a5205ddce38170843ee208
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-13 12:06:28 +0200

    Consolidate a test

--- cypress/integration/battleSim/players.js
+++ cypress/integration/battleSim/players.js
@@ -6,35 +6,43 @@ describe('Battle Simulator — Players', () => {
   })
 
   it('should be possible to update players health', () => {
-    cy.get(s.RED_HEALTH_INPUT)
-      .clear()
-      .type('9')
-      .get(s.RED_HEALTH)
-      .should('have.text', '9')
+    cy.url().then(currentUrl => {
+      cy.get(s.RED_HEALTH_INPUT)
+        .clear()
+        .type('9')
+        .get(s.RED_HEALTH)
+        .should('have.text', '9')
 
-      .get(s.BLUE_HEALTH_INPUT)
-      .clear()
-      .type('8')
-      .get(s.BLUE_HEALTH)
-      .should('have.text', '8')
+        .get(s.BLUE_HEALTH_INPUT)
+        .clear()
+        .type('8')
+        .get(s.BLUE_HEALTH)
+        .should('have.text', '8')
+
+        .url()
+        .should('not.eq', currentUrl)
+    })
   })
 
   it('should be possible to update players faction', () => {
-    cy.get(s.RED_FACTION_SELECT)
-      .select('swarm')
-      .get(s.RED_FACTION)
-      .then($faction => expect($faction).to.have.text('swarm'))
+    cy.url().then(currentUrl => {
+      cy.get(s.RED_FACTION_SELECT)
+        .select('swarm')
+        .get(s.RED_FACTION)
+        .then($faction => expect($faction).to.have.text('swarm'))
 
-      .get(s.BLUE_FACTION_SELECT)
-      .select('ironclad')
-      .get(s.BLUE_FACTION)
-      .then($faction => expect($faction).to.have.text('ironclad'))
+        .get(s.BLUE_FACTION_SELECT)
+        .select('ironclad')
+        .get(s.BLUE_FACTION)
+        .then($faction => expect($faction).to.have.text('ironclad'))
+
+        .url()
+        .should('not.eq', currentUrl)
+    })
   })
 
   it('should be preserved upon reload', () => {
-    cy.url()
-      .should('not.match', /battle$/)
-      .reload()
+    cy.reload()
       .get(s.RED_HEALTH)
       .should('have.text', '9')
       .get(s.BLUE_HEALTH)
