041377bcba683e3f56c170658d355bf28407d118
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-14 10:02:07 +0200

    Improve a deck builder test

--- cypress/integration/deckBuilder/levels.js
+++ cypress/integration/deckBuilder/levels.js
@@ -25,15 +25,12 @@ describe('Deck Builder — Levels', () => {
       .should('contain', 3)
   })
 
-  it('should adjust levels in card gallery to collection', () => {
-    cy.get(s.IMPORT_BTN)
-      .importFile('collection.import.csv')
-      .get(s.LEVEL_SELECT)
-      .should('have.value', '0')
-      .get('body')
-      .trigger('keydown', { keyCode: 51, which: 51, force: true })
-      .get(s.CARD)
-      .find(s.CARD_LEVEL)
-      .should('contain', 3)
+  it('should adjust levels/presence in card gallery to collection', () => {
+    cy.get(s.IMPORT_BTN).importFile('collection.import.csv')
+    cy.get(s.LEVEL_SELECT).should('have.value', '0')
+    cy.get(s.CARD).filter('#N89').should('have.attr', 'data-missing', 'true')
+    cy.get('body').trigger('keydown', { keyCode: 51, which: 51, force: true })
+    cy.get(s.CARD).find(s.CARD_LEVEL).should('contain', 3)
+    cy.get(s.CARD).filter('#N89').should('not.have.attr', 'data-missing')
   })
 })
