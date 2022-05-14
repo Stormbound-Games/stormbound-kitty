983a0911e0b97440d0949c61d9e57d8b2184460f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-14 09:27:46 +0200

    Avoid invalid DOM attributes in Icon component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { useFela } from 'react-fela'
 import styles from './styles'
 
-export default React.memo(function Icon(props) {
-  const { css } = useFela({ type: props.icon })
+export default React.memo(function Icon({ extend, icon, ...props }) {
+  const { css } = useFela({ type: icon })
+
+  if (!icon) return null
 
   return (
     <svg
       {...props}
-      className={css(styles.icon, props.extend)}
+      className={css(styles.icon, extend)}
       aria-hidden='true'
       focusable='false'
     >
-      <use xlinkHref={'#sprite_svg__icon-' + props.icon}></use>
+      <use xlinkHref={'#sprite_svg__icon-' + icon}></use>
     </svg>
   )
 })
