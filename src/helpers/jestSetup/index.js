71d416692c87ecbe93f7468d8a6e897b37ed55be
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-15 09:41:40 +0200

    Mock API calls with Jest

--- src/helpers/jestSetup/index.js
+++ src/helpers/jestSetup/index.js
@@ -5,3 +5,11 @@ if (typeof String.prototype.replaceAll !== 'function') {
     return this.replace(new RegExp(from, 'g'), to)
   }
 }
+
+jest.mock('~/api/misc/getAbbreviations', () =>
+  jest.fn(() => global.__ABBREVIATIONS__)
+)
+jest.mock('~/api/books/getBooks', () => jest.fn(() => global.__BOOKS__))
+jest.mock('~/api/brawls/getBrawls', () => jest.fn(() => global.__BRAWLS__))
+jest.mock('~/api/cards/getCards', () => jest.fn(() => global.__CARDS__))
+jest.mock('~/api/decks/getDecks', () => jest.fn(() => global.__DECKS__))
