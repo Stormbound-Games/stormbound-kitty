f5233fb2b41c8890f0ef344ab3628b2bb6a181d5
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-19 11:37:17 +0200

    Restrict some CMS sections to administrators

--- cms/structure.js
+++ cms/structure.js
@@ -12,27 +12,51 @@ const getCurrentUser = () => {
 
 getCurrentUser()
 
-const structure = () =>
-  S.list()
+// Document types that can be managed by moderators and community members.
+// Anything non listed underneath will be restricted to administrators only.
+const COMMUNITY_STRUCTURE = [
+  'artwork',
+  'podcast',
+  'deck',
+  'deckTag',
+  'guide',
+  'puzzle',
+  'story',
+  'SWCC',
+  'tournament',
+  'user',
+]
+
+const structure = () => {
+  const isAdmin = window._sanityUser?.role === 'administrator'
+
+  return S.list()
     .title('Content')
-    .items([
-      S.listItem()
-        .title('Site settings')
-        .icon(MdSettings)
-        .child(
-          S.document().schemaType('siteSettings').documentId('siteSettings')
-        ),
-      S.listItem()
-        .title('Equal tier list')
-        .icon(MdGrading)
-        .child(
-          S.document().schemaType('equalTierList').documentId('equalTierList')
-        ),
-      ...S.documentTypeListItems().filter(
-        listItem =>
-          !['siteSettings', 'equalTierList'].includes(listItem.getId())
-      ),
-    ])
+    .items(
+      [
+        isAdmin &&
+          S.listItem()
+            .title('Site settings')
+            .icon(MdSettings)
+            .child(
+              S.document().schemaType('siteSettings').documentId('siteSettings')
+            ),
+        S.listItem()
+          .title('Equal tier list')
+          .icon(MdGrading)
+          .child(
+            S.document().schemaType('equalTierList').documentId('equalTierList')
+          ),
+        ...S.documentTypeListItems().filter(listItem => {
+          const id = listItem.getId()
+
+          return isAdmin
+            ? !['siteSettings', 'equalTierList'].includes(id)
+            : COMMUNITY_STRUCTURE.includes(id)
+        }),
+      ].filter(Boolean)
+    )
+}
 
 export const getDefaultDocumentNode = () => {
   return S.document().views([
