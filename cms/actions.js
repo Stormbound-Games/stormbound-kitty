9b68e3ed6b53d618d45e06a53bec89e70ef04bc5
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-20 11:33:56 +0200

    Prevent deleting cards and books documents

--- cms/actions.js
+++ cms/actions.js
@@ -1,11 +1,20 @@
 import defaultResolve, {
+  DeleteAction,
   PublishAction,
   DiscardChangesAction,
 } from 'part:@sanity/base/document-actions'
 
 export default function resolveDocumentActions(props) {
-  if (props.type === 'siteSettings' || props.type === 'equalTierList') {
+  if (['siteSettings', 'equalTierList'].includes(props.type)) {
     return [PublishAction, DiscardChangesAction]
+  }
+
+  // Make sure cards and books documents cannot be deleted, as it would be
+  // pretty problematic for the site to function normally. If a new card/book
+  // entry is created by mistake, it can be deleted in the local studio by
+  // commenting out this condition beforehand.
+  if (['card', 'book'].includes(props.type)) {
+    return [...defaultResolve(props)].filter(action => action !== DeleteAction)
   }
 
   return [...defaultResolve(props)]
