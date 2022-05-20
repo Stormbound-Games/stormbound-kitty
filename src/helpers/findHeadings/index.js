0ddf78b94fca53aa6032a35ed148bab201c68cdd
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-20 16:30:28 +0200

    Simplify some filtering

--- src/helpers/findHeadings/index.js
+++ src/helpers/findHeadings/index.js
@@ -9,15 +9,14 @@ const filter = (ast, match) =>
   }, [])
 
 const findHeadings = (ast, isDeep = false) =>
-  filter(
-    ast,
-    isDeep ? node => /h\d/.test(node.style) : node => /h2/.test(node.style)
-  ).map(node => {
-    const text = getChildrenText(node)
-    const slug = speakingurl(text)
-    const level = Number(node.style.slice(1))
+  filter(ast, node => /h\d/.test(node.style))
+    .filter(node => isDeep || node.style === 'h2')
+    .map(node => {
+      const text = getChildrenText(node)
+      const slug = speakingurl(text)
+      const level = Number(node.style.slice(1))
 
-    return { ...node, text, slug, level, subheadings: [] }
-  })
+      return { ...node, text, slug, level, subheadings: [] }
+    })
 
 export default findHeadings
