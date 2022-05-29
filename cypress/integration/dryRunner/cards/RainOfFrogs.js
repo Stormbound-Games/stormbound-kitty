6d284a873c6af9a9836bd5241c5cc59256b75c18
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-29 11:01:08 +0200

    Skip an obsolete test

--- cypress/integration/dryRunner/cards/RainOfFrogs.js
+++ cypress/integration/dryRunner/cards/RainOfFrogs.js
@@ -3,7 +3,9 @@ import s from '../selectors'
 const DECK_ID = '5n15f85n25f45n35n235n45n55n65n625n635i5'
 
 describe('Dry-runner — Rain of Frogs', () => {
-  it('should not be possible to play a unit or a structure after Rain of Frogs fills the board', () => {
+  // Technically still valid but with RoF costing 3 mana instead of 2, the test
+  // doesn’t make much sense any more.
+  it.skip('should not be possible to play a unit or a structure after Rain of Frogs fills the board', () => {
     const HAND = ['N2', 'I5', 'N3', 'F8']
 
     cy.visit(`/deck/${DECK_ID}/dry-run?mode=MANUAL`)
