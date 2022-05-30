af61718f7af2975f2c97c193ea2522c8387ee552
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-05-30 10:18:20 +0200

    Consolidate a layout test

--- cypress/integration/battleSim/layout.js
+++ cypress/integration/battleSim/layout.js
@@ -1,41 +1,31 @@
 import s from './selectors'
 
 describe('Battle Simulator — Layout', () => {
-  beforeEach(() => {
-    cy.viewport('iphone-6')
-    cy.visit('/simulators/battle')
-    cy.get(s.BOARD).should('be.visible')
-  })
+  before(() => cy.visit('/simulators/battle'))
+  beforeEach(() => cy.viewport('iphone-6'))
 
-  it('should display the board', () => {
+  it('should be possible to open and close the cell panel', () => {
+    cy.get(s.BOARD).should('be.visible')
     cy.get(s.SETTINGS_PANEL).should('not.be.visible')
+    cy.get(s.CELL_PANEL).should('not.exist')
+
+    cy.get(s.CELL_A1).should('be.visible').click()
+    cy.get(s.CELL_PANEL).should('be.visible').find(s.PANEL_CLOSE_BTN).click()
     cy.get(s.CELL_PANEL).should('not.exist')
   })
 
-  it('should display the cell panel when clicking a cell', () => {
-    cy.get(s.CELL_A1).should('be.visible').click()
-    cy.get(s.CELL_PANEL).should('be.visible')
+  it('should be possible to open and close the settings panel', () => {
+    cy.get(s.SETTINGS_PANEL_BTN).click()
+    cy.get(s.SETTINGS_PANEL)
+      .should('be.visible')
+      .find(s.PANEL_CLOSE_BTN)
+      .click()
+    cy.get(s.SETTINGS_PANEL).should('not.be.visible')
   })
 
   it('should close the cell panel when filling a cell', () => {
+    cy.wait(1000)
     cy.bsFill('A1', { card: 'Zhev' })
     cy.get(s.CELL_PANEL).should('not.exist')
-  })
-
-  it('should be possible to open the settings panel', () => {
-    cy.get(s.SETTINGS_PANEL_BTN).click()
-    cy.get(s.SETTINGS_PANEL).should('be.visible')
-  })
-
-  it('should be possible to close the settings panel', () => {
-    cy.get(s.SETTINGS_PANEL_BTN).click()
-    cy.get(s.SETTINGS_PANEL).find(s.PANEL_CLOSE_BTN).click()
-    cy.get(s.SETTINGS_PANEL).should('not.be.visible')
-  })
-
-  it('should be possible to close the cell panel', () => {
-    cy.get(s.CELL_A1).click()
-    cy.get(s.CELL_PANEL).find(s.PANEL_CLOSE_BTN).click()
-    cy.get(s.CELL_PANEL).should('not.be.visible')
   })
 })
