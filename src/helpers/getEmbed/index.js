4208cba0e2f7b05e964ca77cac92f71580d8b850
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-09-20 17:53:23 +0200

    Update ‘.setAuthor()’ call to new signature

--- src/helpers/getEmbed/index.js
+++ src/helpers/getEmbed/index.js
@@ -6,11 +6,12 @@ const getEmbed = ({ withHeader = true } = {}) => {
   embed.setColor('#D7598B')
 
   if (withHeader) {
-    embed.setAuthor(
-      'Stormbound-Kitty',
-      'https://cdn.sanity.io/images/5hlpazgd/production/8fb3c0530531a501d9eb31690b684b40d5761c8a-500x364.png',
-      'https://stormbound-kitty.com'
-    )
+    embed.setAuthor({
+      name: 'Stormbound-Kitty',
+      iconURL:
+        'https://cdn.sanity.io/images/5hlpazgd/production/8fb3c0530531a501d9eb31690b684b40d5761c8a-500x364.png',
+      url: 'https://stormbound-kitty.com',
+    })
   }
 
   return embed
