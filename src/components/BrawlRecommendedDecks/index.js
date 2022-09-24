f9792f37506c365bf43ddc8c16a4172d63561226
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-09-24 15:45:18 +0200

    Remove obsolete query param

--- src/components/BrawlRecommendedDecks/index.js
+++ src/components/BrawlRecommendedDecks/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { useFela } from 'react-fela'
 import Link from '#components/Link'
-import { BrawlContext } from '#components/BrawlProvider'
 import Decks from '#components/Decks'
 import Icon from '#components/Icon'
 import Title from '#components/Title'
 
 export default React.memo(function BrawlRecommendedDecks(props) {
   const { css } = useFela()
-  const { info } = React.useContext(BrawlContext)
 
   if (props.decks.length === 0) return null
 
@@ -18,8 +16,7 @@ export default React.memo(function BrawlRecommendedDecks(props) {
       <Decks showUpgrades columns={props.columns} decks={props.decks} />
       <p className={css({ marginTop: '-1.25em' })}>
         <Icon icon='arrow-right' extend={{ transform: 'translateY(2px)' }} />{' '}
-        Check more{' '}
-        <Link to={'/decks?tags=BRAWL%2C' + info.id}>decks for this brawl</Link>
+        Check more <Link to='/decks'>decks for this brawl</Link>
       </p>
     </>
   )
