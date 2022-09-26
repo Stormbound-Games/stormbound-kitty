f381330ce5892a4f46c868f4a93f4b9c48201d67
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-09-26 11:00:26 +0200

    Fix aliases for the CMS

--- cms/webpack.sanity.js
+++ cms/webpack.sanity.js
@@ -1,7 +1,23 @@
 const path = require('path')
 
 module.exports = function (config) {
-  config.resolve.alias['~'] = path.resolve(__dirname, '..', 'src')
+  config.resolve.alias['#api'] = path.resolve(__dirname, '..', 'src/api')
+  config.resolve.alias['#components'] = path.resolve(
+    __dirname,
+    '..',
+    'src/components'
+  )
+  config.resolve.alias['#constants'] = path.resolve(
+    __dirname,
+    '..',
+    'src/constants'
+  )
+  config.resolve.alias['#helpers'] = path.resolve(
+    __dirname,
+    '..',
+    'src/helpers'
+  )
+  config.resolve.alias['#hooks'] = path.resolve(__dirname, '..', 'src/hooks')
   config.resolve.extensions.push('.json')
 
   return config
