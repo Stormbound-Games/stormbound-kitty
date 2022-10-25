69c5ebe9b2329bf07020cee01e4c8882c682e60f
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2022-10-25 16:07:00 +0200

    Move ‘getLiveTierList’ API function

--- src/pages/tier-list/ranked/index.js
+++ src/pages/tier-list/ranked/index.js
@@ -1,6 +1,6 @@
 import PageRankedList from '#components/PageRankedList'
 import getInitialListData from '#helpers/getInitialListData'
-import getLiveTierList from '#helpers/getLiveTierList'
+import getLiveTierList from '#api/lists/getLiveTierList'
 import getSiteSettings from '#api/misc/getSiteSettings'
 
 export async function getStaticProps({ preview: isPreview = false }) {
