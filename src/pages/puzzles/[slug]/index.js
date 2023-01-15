4bd945f7821c9251d66d90a2b7a254a702605bd9
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2023-01-15 22:18:44 +0100

    Improve performance a bit

--- src/pages/puzzles/[slug]/index.js
+++ src/pages/puzzles/[slug]/index.js
@@ -1,11 +1,12 @@
 import PagePuzzle from '#components/PagePuzzle'
-import getChangesFromCard from '#api/changes/getChangesFromCard'
+import getChanges from '#api/changes/getChanges'
 import getPuzzle from '#api/puzzles/getPuzzle'
 import getPuzzles from '#api/puzzles/getPuzzles'
 import getSiteSettings from '#api/misc/getSiteSettings'
 import indexArray from '#helpers/indexArray'
 import serialization from '#helpers/serialization'
 import parseDate from '#helpers/parseDate'
+import groupBy from '#helpers/groupBy'
 
 export async function getStaticPaths() {
   const puzzles = await getPuzzles()
@@ -14,11 +15,17 @@ export async function getStaticPaths() {
   return { paths, fallback: 'blocking' }
 }
 
-// Get the (preview) changes for the card and preserve only what’s necessary.
-const getChanges = async (id, date, isPreview) =>
-  (await getChangesFromCard({ id: id, isPreview }))
-    .map(change => ({ from: change.from, timestamp: change.timestamp }))
+const getCardChanges = (changes, id) =>
+  (changes[id] ?? []).map(({ from, timestamp }) => ({ from, timestamp }))
+
+const getAllChangesAfterDate = async (date, isPreview) => {
+  const allChanges = await getChanges({ isPreview })
+  const afterDate = allChanges
     .filter(change => change.from && change.timestamp >= date)
+    .sort((a, b) => b.timestamp - a.timestamp)
+
+  return groupBy(afterDate, 'id')
+}
 
 export async function getStaticProps({ params, preview: isPreview = false }) {
   const { slug } = params
@@ -32,27 +39,22 @@ export async function getStaticProps({ params, preview: isPreview = false }) {
 
   const sim = serialization.battle.deserialize(cardsIndex, puzzle.board)
   const date = parseDate(puzzle.date)
+  const changesById = await getAllChangesAfterDate(date, isPreview)
 
   // In order to render the versioned cards (from the time the puzzle was
   // issued), we need to resolve the version of every card in the hand and on
   // the board.
 
-  await Promise.all(
-    sim.cards.map(async card => {
-      if (card.id) card.versions = await getChanges(card.id, date, isPreview)
-    })
-  )
+  sim.cards.forEach(card => {
+    if (card.id) card.versions = getCardChanges(changesById, card.id)
+  })
 
-  await Promise.all(
-    sim.board.map(row => {
-      Promise.all(
-        row.map(async cell => {
-          if (cell.card?.id)
-            cell.card.versions = await getChanges(cell.card.id, date, isPreview)
-        })
-      )
+  sim.board.forEach(row => {
+    row.forEach(cell => {
+      if (cell.card?.id)
+        cell.card.versions = getCardChanges(changesById, cell.card.id)
     })
-  )
+  })
 
   // Enforce grid markers for puzzles as it’s important to understand the
   // solution.
