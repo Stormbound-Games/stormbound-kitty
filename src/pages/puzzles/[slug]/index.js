acea778cb550c9ac2c358848d4226600ea6249d0
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2023-01-15 14:41:50 +0100

    Query versions data for all cards on the board and in the hand

--- src/pages/puzzles/[slug]/index.js
+++ src/pages/puzzles/[slug]/index.js
@@ -1,9 +1,11 @@
 import PagePuzzle from '#components/PagePuzzle'
+import getChangesFromCard from '#api/changes/getChangesFromCard'
 import getPuzzle from '#api/puzzles/getPuzzle'
 import getPuzzles from '#api/puzzles/getPuzzles'
 import getSiteSettings from '#api/misc/getSiteSettings'
 import indexArray from '#helpers/indexArray'
 import serialization from '#helpers/serialization'
+import parseDate from '#helpers/parseDate'
 
 export async function getStaticPaths() {
   const puzzles = await getPuzzles()
@@ -11,6 +13,12 @@ export async function getStaticPaths() {
 
   return { paths, fallback: 'blocking' }
 }
+
+// Get the (preview) changes for the card and preserve only what’s necessary.
+const getChanges = async (id, date, isPreview) =>
+  (await getChangesFromCard({ id: id, isPreview }))
+    .map(change => ({ from: change.from, timestamp: change.timestamp }))
+    .filter(change => change.from && change.timestamp >= date)
 
 export async function getStaticProps({ params, preview: isPreview = false }) {
   const { slug } = params
@@ -23,6 +31,28 @@ export async function getStaticProps({ params, preview: isPreview = false }) {
   }
 
   const sim = serialization.battle.deserialize(cardsIndex, puzzle.board)
+  const date = parseDate(puzzle.date)
+
+  // In order to render the versioned cards (from the time the puzzle was
+  // issued), we need to resolve the version of every card in the hand and on
+  // the board.
+
+  await Promise.all(
+    sim.cards.map(async card => {
+      if (card.id) card.versions = await getChanges(card.id, date, isPreview)
+    })
+  )
+
+  await Promise.all(
+    sim.board.map(row => {
+      Promise.all(
+        row.map(async cell => {
+          if (cell.card?.id)
+            cell.card.versions = await getChanges(cell.card.id, date, isPreview)
+        })
+      )
+    })
+  )
 
   // Enforce grid markers for puzzles as it’s important to understand the
   // solution.
