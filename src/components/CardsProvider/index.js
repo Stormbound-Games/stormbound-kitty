5d5132f9bc5f634f9304a79b36bed01a431ead54
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2023-09-17 09:24:40 +0200

    Update the default value of the ‘CardsContext’

--- src/components/CardsProvider/index.js
+++ src/components/CardsProvider/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import indexArray from '#helpers/indexArray'
 
-export const CardsContext = React.createContext({ cards: [], index: {} })
+export const CardsContext = React.createContext({
+  cards: [],
+  cardsWithoutTokens: [],
+  index: {},
+  cardsIndexByName: {},
+  cardsIndexBySid: {},
+})
 
 export default React.memo(function CardsProvider({ children, cards = [] }) {
   const cardsIndex = React.useMemo(() => indexArray(cards), [cards])
   const cardsIndexByName = React.useMemo(
     () => indexArray(cards, 'name'),
-    [cards]
+    [cards],
   )
   const cardsIndexBySid = React.useMemo(() => indexArray(cards, 'sid'), [cards])
   const cardsWithoutTokens = React.useMemo(
     () => cards.filter(card => !card.token),
-    [cards]
+    [cards],
   )
 
   return (
