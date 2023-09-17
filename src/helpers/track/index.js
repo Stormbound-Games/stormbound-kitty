74bcd2ddf9dcaed65564362a952b7b140e7cc0ad
KittySparkles <53430374+KittySparkles@users.noreply.github.com>
2023-09-17 10:40:43 +0200

    Send custom events to Vercel Analytics

--- src/helpers/track/index.js
+++ src/helpers/track/index.js
@@ -1,11 +1,14 @@
+import va from '@vercel/analytics'
+
 const track = (eventName, props = {}) => {
   if (!window.plausible) {
     return console.log(
       `Failed attempt to track event ‘${eventName}’ with props`,
-      props
+      props,
     )
   }
 
+  va.track(eventName, props)
   window.plausible(eventName, { props })
 }
 
