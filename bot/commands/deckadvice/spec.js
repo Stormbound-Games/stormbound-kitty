e24536ea003cfe77c31a27e6c176ef7d27ffb4eb
Kitty Sparkles <53430374+KittySparkles@users.noreply.github.com>
2024-08-04 10:37:04 +0200

    Fix a unit test

--- bot/commands/deckadvice/spec.js
+++ bot/commands/deckadvice/spec.js
@@ -10,7 +10,7 @@ describe('Bot — /deckadvice', () => {
     expect(output.ephemeral).toBeTruthy()
     expect(embed.title).toBe('💎 Deck Advice')
     expect(embed.description).toBe(
-      'There was an error evaluating the given deck ID.'
+      'There was an error evaluating the given deck ID.',
     )
   })
 
@@ -22,13 +22,13 @@ describe('Bot — /deckadvice', () => {
     expect(output.ephemeral).toBeTruthy()
     expect(embed.title).toBe('💎 Deck Advice')
     expect(embed.description).toBe(
-      'There was an error evaluating some of the cards.'
+      'There was an error evaluating some of the cards.',
     )
   })
 
   it('should return advice for a given deck', async () => {
     const interaction = mockInteraction({
-      deck: '5n35n125n163w54n184w95w125w133n394w153w194w21',
+      deck: '5n35n124w95n163w54n185w124w155w133n393w191w23',
     })
     const output = await command.execute(interaction, client)
     const embed = output.embeds[0].data
@@ -36,7 +36,7 @@ describe('Bot — /deckadvice', () => {
     expect(output.ephemeral).toBeTruthy()
     expect(embed.title).toBe('💎 Deck Advice')
     expect(embed.url).toContain(
-      'https://stormbound-kitty.com/deck/5n35n125n163w54n184w95w125w133n394w153w194w21/detail'
+      'https://stormbound-kitty.com/deck/5n35n125n163w54n184w95w125w133n394w153w194w21/detail',
     )
     expect(embed.fields.length).toBeGreaterThan(0)
   })
